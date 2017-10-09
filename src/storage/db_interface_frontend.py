01a495dc4416127382018cc15f705da1fe10b72d
Jörg Stucke <joerg.stucke@fkie.fraunhofer.de>
2017-10-09 14:02:44 +0200

    refactoring, quotation changes

--- src/storage/db_interface_frontend.py
+++ src/storage/db_interface_frontend.py
@@ -22,23 +22,23 @@ class FrontEndDbInterface(MongoInterfaceCommon):
             firmware_list = self.firmwares.find()
         for q in firmware_list:
             if q:
-                if q["processed_analysis"]["unpacker"]["file_system_flag"]:
-                    unpacker = self.retrieve_analysis(deepcopy(q["processed_analysis"]))["unpacker"]["plugin_used"]
+                if q['processed_analysis']['unpacker']['file_system_flag']:
+                    unpacker = self.retrieve_analysis(deepcopy(q['processed_analysis']))['unpacker']['plugin_used']
                 else:
-                    unpacker = q["processed_analysis"]["unpacker"]["plugin_used"]
+                    unpacker = q['processed_analysis']['unpacker']['plugin_used']
                 list_of_firmware_data.append((q['_id'], self.get_hid(q['_id']), unpacker))
         return list_of_firmware_data
 
     def get_hid(self, uid, root_uid=None):
-        """
+        '''
         returns a human readable identifier (hid) for a given uid
         returns an empty string if uid is not in Database
-        """
+        '''
         hid = self._get_hid_firmware(uid)
         if hid is None:
             hid = self._get_hid_fo(uid, root_uid)
         if hid is None:
-            return ""
+            return ''
         else:
             return hid
 
@@ -52,13 +52,13 @@ class FrontEndDbInterface(MongoInterfaceCommon):
         result = []
         for db_entry in db_iterable:
             if db_entry is not None:
-                virtual_file_path = db_entry["virtual_file_path"]
+                virtual_file_path = db_entry['virtual_file_path']
                 result.append({
-                    "uid": db_entry["_id"],
-                    "files_included": db_entry["files_included"],
-                    "size": db_entry["size"],
-                    "mime-type": db_entry["processed_analysis"]["file_type"]["mime"],
-                    "virtual_file_paths": virtual_file_path[root_uid] if root_uid in virtual_file_path else get_value_of_first_key(virtual_file_path)
+                    'uid': db_entry['_id'],
+                    'files_included': db_entry['files_included'],
+                    'size': db_entry['size'],
+                    'mime-type': db_entry['processed_analysis']['file_type']['mime'],
+                    'virtual_file_paths': virtual_file_path[root_uid] if root_uid in virtual_file_path else get_value_of_first_key(virtual_file_path)
                 })
         return result
 
@@ -74,23 +74,23 @@ class FrontEndDbInterface(MongoInterfaceCommon):
         return list(attribute_list)
 
     def get_device_class_list(self):
-        return self.get_firmware_attribute_list("device_class")
+        return self.get_firmware_attribute_list('device_class')
 
     def get_vendor_list(self):
-        return self.get_firmware_attribute_list("vendor")
+        return self.get_firmware_attribute_list('vendor')
 
     def get_device_name_dict(self):
         device_name_dict = {}
         query = self.firmwares.find()
         for item in query:
-            if item["device_class"] not in device_name_dict.keys():
-                device_name_dict[item["device_class"]] = {item["vendor"]: [item["device_name"]]}
+            if item['device_class'] not in device_name_dict.keys():
+                device_name_dict[item['device_class']] = {item['vendor']: [item['device_name']]}
             else:
-                if item["vendor"] not in device_name_dict[item["device_class"]].keys():
-                    device_name_dict[item["device_class"]][item["vendor"]] = [item["device_name"]]
+                if item['vendor'] not in device_name_dict[item['device_class']].keys():
+                    device_name_dict[item['device_class']][item['vendor']] = [item['device_name']]
                 else:
-                    if item["device_name"] not in device_name_dict[item["device_class"]][item["vendor"]]:
-                        device_name_dict[item["device_class"]][item["vendor"]].append(item["device_name"])
+                    if item['device_name'] not in device_name_dict[item['device_class']][item['vendor']]:
+                        device_name_dict[item['device_class']][item['vendor']].append(item['device_name'])
         return device_name_dict
 
     @staticmethod
@@ -100,14 +100,14 @@ class FrontEndDbInterface(MongoInterfaceCommon):
         return FileObject.get_top_of_virtual_path(fo_dict['virtual_file_path'][root_uid][0])
 
     def _get_hid_firmware(self, uid):
-        fw = self.firmwares.find_one({"_id": uid}, {'vendor': 1, 'device_name': 1, 'version': 1, 'device_class': 1})
+        fw = self.firmwares.find_one({'_id': uid}, {'vendor': 1, 'device_name': 1, 'version': 1, 'device_class': 1})
         if fw is not None:
-            return "{} {} - {} ({})".format(fw['vendor'], fw['device_name'], fw['version'], fw["device_class"])
+            return '{} {} - {} ({})'.format(fw['vendor'], fw['device_name'], fw['version'], fw['device_class'])
         else:
             return None
 
     def _get_hid_fo(self, uid, root_uid):
-        fo = self.file_objects.find_one({"_id": uid}, {'virtual_file_path': 1})
+        fo = self.file_objects.find_one({'_id': uid}, {'virtual_file_path': 1})
         if fo is not None:
             return self._get_one_virtual_path_of_fo(fo, root_uid)
         else:
@@ -126,7 +126,7 @@ class FrontEndDbInterface(MongoInterfaceCommon):
                 search_dict = json.loads(search_dict)
 
             query = self.firmwares.find(search_dict, {'_id': 1}, skip=skip, limit=limit, sort=[('vendor', 1)])
-            result = [match["_id"] for match in query]
+            result = [match['_id'] for match in query]
 
             if len(result) < limit or limit == 0:
                 max_firmware_results = self.get_firmware_number(query=search_dict)
@@ -134,17 +134,17 @@ class FrontEndDbInterface(MongoInterfaceCommon):
                 limit_fo = limit - len(result) if limit > 0 else 0
                 if not only_fo_parent_firmware:
                     query = self.file_objects.find(search_dict, {'_id': 1}, skip=skip_fo, limit=limit_fo, sort=[('file_name', 1)])
-                    result.extend([match["_id"] for match in query])
+                    result.extend([match['_id'] for match in query])
                 else:  # only searching for parents of matching file objects
-                    query = self.file_objects.find(search_dict, {"virtual_file_path": 1})
-                    parent_uids = {uid for match in query for uid in match["virtual_file_path"].keys()}
-                    query_filter = {"$nor": [{"_id": {"$nin": list(parent_uids)}}, search_dict]}
+                    query = self.file_objects.find(search_dict, {'virtual_file_path': 1})
+                    parent_uids = {uid for match in query for uid in match['virtual_file_path'].keys()}
+                    query_filter = {'$nor': [{'_id': {'$nin': list(parent_uids)}}, search_dict]}
                     query = self.firmwares.find(query_filter, {'_id': 1}, skip=skip_fo, limit=limit_fo, sort=[('file_name', 1)])
-                    parents = [match["_id"] for match in query]
+                    parents = [match['_id'] for match in query]
                     result = remove_duplicates_from_list(result + parents)
 
         except Exception as e:
-            error_message = "could not process search request: {} {}".format(sys.exc_info()[0].__name__, e)
+            error_message = 'could not process search request: {} {}'.format(sys.exc_info()[0].__name__, e)
             logging.warning(error_message)
             return error_message
         return result
@@ -158,13 +158,13 @@ class FrontEndDbInterface(MongoInterfaceCommon):
 
     @staticmethod
     def _convert_result_list_to_dict(search_results):
-        return {entry["_id"]: entry for entry in search_results}
+        return {entry['_id']: entry for entry in search_results}
 
     # --- statistics
 
     def get_X_last_added_firmwares(self, limit_x=10):
         db_entries = self.firmwares.find(
-            {'submission_date': {"$gt": 1}},
+            {'submission_date': {'$gt': 1}},
             {'_id': 1, 'vendor': 1, 'device_name': 1, 'version': 1, 'device_class': 1, 'submission_date': 1},
             limit=limit_x, sort=[('submission_date', -1)]
         )
@@ -177,17 +177,17 @@ class FrontEndDbInterface(MongoInterfaceCommon):
         comments = []
         for collection in [self.firmwares, self.file_objects]:
             db_entries = collection.aggregate([
-                {"$match": {"comments": {"$not": {"$size": 0}}}},
-                {"$project": {"_id": 1, "comments": 1}},
-                {"$unwind": {"path": "$comments"}},
-                {"$sort": {'comments.time': -1}},
-                {"$limit": limit}
+                {'$match': {'comments': {'$not': {'$size': 0}}}},
+                {'$project': {'_id': 1, 'comments': 1}},
+                {'$unwind': {'path': '$comments'}},
+                {'$sort': {'comments.time': -1}},
+                {'$limit': limit}
             ])
             comments.extend([
-                {**entry["comments"], "uid": entry["_id"]}  # caution: >=python3.5 exclusive syntax
-                for entry in db_entries if entry["comments"]
+                {**entry['comments'], 'uid': entry['_id']}  # caution: >=python3.5 exclusive syntax
+                for entry in db_entries if entry['comments']
             ])
-        comments.sort(key=lambda x: x["time"], reverse=True)
+        comments.sort(key=lambda x: x['time'], reverse=True)
         return comments
 
     def get_number_of_firmwares_in_db(self):
@@ -196,15 +196,15 @@ class FrontEndDbInterface(MongoInterfaceCommon):
     def get_file_type_statistics(self):
         file_type_dict = {}
         for fo in merge_generators(
-                self.firmwares.find({}, {"processed_analysis.file_type.mime": 1, "virtual_file_path": 1}),
-                self.file_objects.find({}, {"processed_analysis.file_type.mime": 1, "virtual_file_path": 1})
+                self.firmwares.find({}, {'processed_analysis.file_type.mime': 1, 'virtual_file_path': 1}),
+                self.file_objects.find({}, {'processed_analysis.file_type.mime': 1, 'virtual_file_path': 1})
         ):
-            file_type = fo["processed_analysis"]["file_type"]["mime"]
+            file_type = fo['processed_analysis']['file_type']['mime']
             if file_type in file_type_dict:
                 # one file can appear multiple times in the same firmware or in different firmwares
-                file_type_dict[file_type] += len(fo["virtual_file_path"])
+                file_type_dict[file_type] += len(fo['virtual_file_path'])
             else:
-                file_type_dict[file_type] = len(fo["virtual_file_path"])
+                file_type_dict[file_type] = len(fo['virtual_file_path'])
         return dict_to_sorted_tuples(file_type_dict)
 
     # --- file tree
@@ -216,28 +216,28 @@ class FrontEndDbInterface(MongoInterfaceCommon):
                 node.add_child_node(n)
         else:  # at the end of a virtual path aka a 'real' file
             if whitelist:
-                has_children = any(f in fo_data["files_included"] for f in whitelist)
+                has_children = any(f in fo_data['files_included'] for f in whitelist)
             else:
-                has_children = fo_data["files_included"] != []
-            node = FileTreeNode(uid, virtual=False, name=fo_data["file_name"], size=fo_data["size"],
-                                mime_type=fo_data["processed_analysis"]["file_type"]["mime"], has_children=has_children)
+                has_children = fo_data['files_included'] != []
+            node = FileTreeNode(uid, virtual=False, name=fo_data['file_name'], size=fo_data['size'],
+                                mime_type=fo_data['processed_analysis']['file_type']['mime'], has_children=has_children)
         return node
 
     def generate_file_tree_node(self, uid, root_uid, current_virtual_path=None, fo_data=None, whitelist=None):
-        required_fields = {"virtual_file_path": 1, "files_included": 1, "file_name": 1, "size": 1, "processed_analysis.file_type.mime": 1, "_id": 1}
+        required_fields = {'virtual_file_path': 1, 'files_included': 1, 'file_name': 1, 'size': 1, 'processed_analysis.file_type.mime': 1, '_id': 1}
         if fo_data is None:
-            fo_data = self.get_specific_fields_of_db_entry({"_id": uid}, required_fields)
+            fo_data = self.get_specific_fields_of_db_entry({'_id': uid}, required_fields)
         try:
-            if root_uid not in fo_data["virtual_file_path"]:  # file tree for a file object (instead of a firmware)
-                fo_data["virtual_file_path"] = get_partial_virtual_path(fo_data["virtual_file_path"], root_uid)
+            if root_uid not in fo_data['virtual_file_path']:  # file tree for a file object (instead of a firmware)
+                fo_data['virtual_file_path'] = get_partial_virtual_path(fo_data['virtual_file_path'], root_uid)
             if current_virtual_path is None:
-                for entry in fo_data["virtual_file_path"][root_uid]:  # the same file may occur several times with different virtual paths
-                    current_virtual_path = entry.split("/")[1:]
+                for entry in fo_data['virtual_file_path'][root_uid]:  # the same file may occur several times with different virtual paths
+                    current_virtual_path = entry.split('/')[1:]
                     yield self._create_node_from_virtual_path(uid, root_uid, current_virtual_path, fo_data, whitelist)
             else:
                 yield self._create_node_from_virtual_path(uid, root_uid, current_virtual_path, fo_data, whitelist)
         except Exception:  # the requested data is not present in the DB aka the file has not been analyzed yet
-            yield FileTreeNode(uid=uid, not_analyzed=True, name="{} (not analyzed yet)".format(uid))
+            yield FileTreeNode(uid=uid, not_analyzed=True, name='{} (not analyzed yet)'.format(uid))
 
     def get_number_of_total_matches(self, query, only_parent_firmwares):
         if not only_parent_firmwares:
@@ -245,14 +245,14 @@ class FrontEndDbInterface(MongoInterfaceCommon):
         else:
             if type(query) == str:
                 query = json.loads(query)
-            fw_matches = {match["_id"] for match in self.firmwares.find(query)}
+            fw_matches = {match['_id'] for match in self.firmwares.find(query)}
             fo_matches = {parent for match in self.file_objects.find(query)
-                          for parent in match["virtual_file_path"].keys()} if query != {} else set()
+                          for parent in match['virtual_file_path'].keys()} if query != {} else set()
             return len(fw_matches.union(fo_matches))
 
     def create_analysis_structure(self):
         if self.client.varietyResults.file_objectsKeys.count() == 0:
-            return "Database statistics do not seem to be created yet."
+            return 'Database statistics do not seem to be created yet.'
 
         file_object_keys = self.client.varietyResults.file_objectsKeys.find()
         all_field_strings = list(
@@ -260,7 +260,7 @@ class FrontEndDbInterface(MongoInterfaceCommon):
             if key_item['_id']['key'].startswith('processed_analysis') and
             key_item['percentContaining'] >= float(self.config['data_storage']['structural_threshold'])
         )
-        stripped_field_strings = list(field[len("processed_analysis."):] for field in all_field_strings if field != "processed_analysis")
+        stripped_field_strings = list(field[len('processed_analysis.'):] for field in all_field_strings if field != 'processed_analysis')
 
         return visualize_complete_tree(stripped_field_strings)
 
--- src/web_interface/components/ajax_routes.py
+++ src/web_interface/components/ajax_routes.py
@@ -17,13 +17,13 @@ from web_interface.components.jinja_filter import FilterClass
 
 class AjaxRoutes(ComponentBase):
     def _init_component(self):
-        self._app.add_url_rule("/ajax_tree/<uid>", "ajax_tree/<uid>", self._ajax_get_tree_children)
-        self._app.add_url_rule("/ajax_root/<uid>", "ajax_root/<uid>", self._ajax_get_tree_root)
-        self._app.add_url_rule("/compare/ajax_tree/<compare_id>/<root_uid>/<uid>", "compare/ajax_tree/<compare_id>/<root_uid>/<uid>",
+        self._app.add_url_rule('/ajax_tree/<uid>', 'ajax_tree/<uid>', self._ajax_get_tree_children)
+        self._app.add_url_rule('/ajax_root/<uid>', 'ajax_root/<uid>', self._ajax_get_tree_root)
+        self._app.add_url_rule('/compare/ajax_tree/<compare_id>/<root_uid>/<uid>', 'compare/ajax_tree/<compare_id>/<root_uid>/<uid>',
                                self._ajax_get_tree_children)
-        self._app.add_url_rule("/compare/ajax_common_files/<compare_id>/<feature_id>/", "compare/ajax_common_files/<compare_id>/<feature_id>/",
+        self._app.add_url_rule('/compare/ajax_common_files/<compare_id>/<feature_id>/', 'compare/ajax_common_files/<compare_id>/<feature_id>/',
                                self._ajax_get_common_files_for_compare)
-        self._app.add_url_rule("/ajax_get_binary/<mime_type>/<uid>", "ajax_get_binary/<type>/<uid>", self._ajax_get_binary)
+        self._app.add_url_rule('/ajax_get_binary/<mime_type>/<uid>', 'ajax_get_binary/<type>/<uid>', self._ajax_get_binary)
 
     def _get_exclusive_files(self, compare_id, root_uid):
         if compare_id is None or root_uid is None:
@@ -31,7 +31,7 @@ class AjaxRoutes(ComponentBase):
         with ConnectTo(CompareDbInterface, self._config) as sc:
             result = sc.get_compare_result(compare_id)
         try:
-            exclusive_files = result["plugins"]["File_Coverage"]["exclusive_files"][root_uid]
+            exclusive_files = result['plugins']['File_Coverage']['exclusive_files'][root_uid]
         except KeyError:
             exclusive_files = []
         return exclusive_files
@@ -41,9 +41,9 @@ class AjaxRoutes(ComponentBase):
         children = []
         root = FileTreeNode(None)
         with ConnectTo(FrontEndDbInterface, self._config) as sc:
-            child_uids = sc.get_specific_fields_of_db_entry(uid, {"files_included": 1})["files_included"]
+            child_uids = sc.get_specific_fields_of_db_entry(uid, {'files_included': 1})['files_included']
             for child_uid in child_uids:
-                if exclusive_files is None or child_uid in exclusive_files:
+                if not exclusive_files or child_uid in exclusive_files:
                     for node in sc.generate_file_tree_node(child_uid, uid, whitelist=exclusive_files):
                         root.add_child_node(node)
         for child_node in root.get_list_of_child_nodes():
@@ -61,26 +61,26 @@ class AjaxRoutes(ComponentBase):
     @staticmethod
     def _get_jstree_node_contents(text, a_attr, li_attr, icon):
         return {
-            "text": text,
-            "a_attr": {"href": a_attr},
-            "li_attr": {"href": li_attr},
-            "icon": icon
+            'text': text,
+            'a_attr': {'href': a_attr},
+            'li_attr': {'href': li_attr},
+            'icon': icon
         }
 
     def _get_virtual_jstree_node_contents(self, node):
-        return self._get_jstree_node_contents("{}".format(node.name), "#", "#", "/static/file_icons/folder.png")
+        return self._get_jstree_node_contents('{}'.format(node.name), '#', '#', '/static/file_icons/folder.png')
 
     def _get_not_analyzed_jstree_node_contents(self, node):
         return self._get_jstree_node_contents(
-            "{}".format(node.name), "/analysis/{}".format(node.uid), "/analysis/{}".format(node.uid), "/static/file_icons/not_analyzed.png"
+            '{}'.format(node.name), '/analysis/{}'.format(node.uid), '/analysis/{}'.format(node.uid), '/static/file_icons/not_analyzed.png'
         )
 
     def _get_analyzed_jstree_node_contents(self, node):
         result = self._get_jstree_node_contents(
-            "<b>{}</b> (<span style='color:gray;'>{}</span>)".format(node.name, human_readable_file_size(node.size)),
-            "/analysis/{}".format(node.uid), "/analysis/{}".format(node.uid), get_correct_icon_for_mime(node.type)
+            '<b>{}</b> (<span style="color:gray;">{}</span>)'.format(node.name, human_readable_file_size(node.size)),
+            '/analysis/{}'.format(node.uid), '/analysis/{}'.format(node.uid), get_correct_icon_for_mime(node.type)
         )
-        result["data"] = {"uid": node.uid}
+        result['data'] = {'uid': node.uid}
         return result
 
     def _get_jstree_child_nodes(self, node):
@@ -96,11 +96,11 @@ class AjaxRoutes(ComponentBase):
             return result
 
     def _generate_jstree_node(self, node):
-        """
+        '''
         converts a file tree node to a json dict that can be rendered by jstree
         :param node: the file tree node
         :return: a json-compatible dict containing the jstree data
-        """
+        '''
         if node.virtual:
             result = self._get_virtual_jstree_node_contents(node)
         elif node.not_analyzed:
@@ -114,8 +114,8 @@ class AjaxRoutes(ComponentBase):
     def _ajax_get_common_files_for_compare(self, compare_id, feature_id):
         with ConnectTo(CompareDbInterface, self._config) as sc:
             result = sc.get_compare_result(compare_id)
-        feature, key = feature_id.split("___")
-        uid_list = result["plugins"]["File_Coverage"][feature][key]
+        feature, key = feature_id.split('___')
+        uid_list = result['plugins']['File_Coverage'][feature][key]
         # filters = FilterClass(None, None, self._config)
         FilterClass._config = self._config
         return self._get_nice_uid_list_html(uid_list)
@@ -125,20 +125,20 @@ class AjaxRoutes(ComponentBase):
             included_files = sc.get_data_for_nice_list(input_data, None)
         number_of_unanalyzed_files = len(input_data) - len(included_files)
         return render_template(
-            "generic_view/nice_fo_list.html",
+            'generic_view/nice_fo_list.html',
             fo_list=included_files,
             number_of_unanalyzed_files=number_of_unanalyzed_files,
             omit_collapse=True
         )
 
     def _ajax_get_binary(self, mime_type, uid):
-        mime_type = mime_type.replace("_", "/")
-        div = "<div style='display: block; border: 1px solid; border-color: #dddddd; padding: 5px; text-align: center'>"
+        mime_type = mime_type.replace('_', '/')
+        div = '<div style="display: block; border: 1px solid; border-color: #dddddd; padding: 5px; text-align: center">'
         with ConnectTo(InterComFrontEndBinding, self._config) as sc:
             binary = sc.get_binary_and_filename(uid)[0]
-        if "text/" in mime_type:
-            return "<pre style='white-space: pre-wrap'>{}</pre>".format(html.escape(bytes_to_str_filter(binary)))
-        elif "image/" in mime_type:
-            return "{}<img src='data:image/{} ;base64,{}' style='max-width:100%'></div>".format(div, mime_type[6:], encode_base64_filter(binary))
+        if 'text/' in mime_type:
+            return '<pre style="white-space: pre-wrap">{}</pre>'.format(html.escape(bytes_to_str_filter(binary)))
+        elif 'image/' in mime_type:
+            return '{}<img src="data:image/{} ;base64,{}" style="max-width:100%"></div>'.format(div, mime_type[6:], encode_base64_filter(binary))
         else:
             return None
