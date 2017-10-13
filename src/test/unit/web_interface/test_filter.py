5022331dcfa72765241fe595f354870ed7baf38d
Peter Weidenbach <peter.weidenbach@fkie.fraunhofer.de>
2017-10-13 13:24:42 +0200

    refactoring

--- src/test/unit/web_interface/test_filter.py
+++ src/test/unit/web_interface/test_filter.py
@@ -1,25 +1,17 @@
 import unittest
+import pytest
+from time import gmtime
 
 from web_interface.filter import replace_underscore_filter, byte_number_filter, get_all_uids_in_string, nice_list, uids_to_link, \
     list_to_line_break_string, nice_unix_time, nice_number_filter, sort_chart_list_by_value, \
-    sort_chart_list_by_name, text_highlighter, generic_nice_representation, list_to_line_break_string_no_sort
+    sort_chart_list_by_name, text_highlighter, generic_nice_representation, list_to_line_break_string_no_sort,\
+    encode_base64_filter
 
 
 class TestWebInterfaceFilter(unittest.TestCase):
 
-    def test_byte_number_filter(self):
-        self.assertEqual(byte_number_filter(1000), '1000.00 Byte', 'bytes output not correct')
-        self.assertEqual(byte_number_filter(1024), '1.00 KiB', 'KB output not correct')
-        self.assertEqual(byte_number_filter(1024 * 1024), '1.00 MiB', 'MB output not correct')
-        # verbose
-        self.assertEqual(byte_number_filter(1000, verbose=True), '1000.00 Byte (1,000 bytes)', 'verbose byte output not correct')
-
-    def test_replace_underscore(self):
-        a = 'test_string'
-        self.assertEqual(replace_underscore_filter(a), 'test string', 'output not correct')
-
     def test_get_all_uids_in_string(self):
-        test_string = '{"d41c0f1431b39b9db565b4e32a5437c61c77762a3f4401bac3bafa4887164117_24", "f7c927fb0c209035c7e6939bdd00eabdaada429f2ee9aeca41290412c8c79759_25" , "deaa23651f0a9cc247a20d0e0a78041a8e40b144e21b82081ecb519dd548eecf_24494080"}'
+        test_string = '{\'d41c0f1431b39b9db565b4e32a5437c61c77762a3f4401bac3bafa4887164117_24\', \'f7c927fb0c209035c7e6939bdd00eabdaada429f2ee9aeca41290412c8c79759_25\' , \'deaa23651f0a9cc247a20d0e0a78041a8e40b144e21b82081ecb519dd548eecf_24494080\'}'
         result = get_all_uids_in_string(test_string)
         self.assertEqual(len(result), 3, 'not all uids found')
         self.assertIn('d41c0f1431b39b9db565b4e32a5437c61c77762a3f4401bac3bafa4887164117_24', result, 'first uid not found')
@@ -56,12 +48,6 @@ class TestWebInterfaceFilter(unittest.TestCase):
         self.assertEqual(nice_unix_time(input_data), '2016-03-31 14:31:00', 'output not correct (float)')
         self.assertEqual(nice_unix_time('test'), 'test')
 
-    def test_nice_number(self):
-        self.assertEqual(nice_number_filter(123), '123')
-        self.assertEqual(nice_number_filter(1234), '1,234')
-        self.assertEqual(nice_number_filter(1234.1234), '1,234.12')
-        self.assertEqual(nice_number_filter(None), 'not available')
-
     def test_sort_chart_list_by_value(self):
         test_list = [['a', 1], ['b', 2]]
         result = sort_chart_list_by_value(test_list)
@@ -79,8 +65,47 @@ class TestWebInterfaceFilter(unittest.TestCase):
         self.assertEqual(text_highlighter('foo', green=['*']), '<span style="color:green;">foo</span>')
         self.assertEqual(text_highlighter('foo', red=['*']), '<span style="color:red;">foo</span>')
 
-    def test_generic_nice_representation(self):
-        self.assertEqual(generic_nice_representation(b'abc'), 'abc', 'byte rep not correct')
-        self.assertEqual(generic_nice_representation(1234), '1,234', 'int rep not correct')
-        self.assertIsInstance(generic_nice_representation([1, 3]), str, 'list not converted to string')
-        self.assertIsInstance(generic_nice_representation({'a': 1}), str, 'dict not converted to string')
+
+def test_replace_underscore():
+    assert replace_underscore_filter('a_b') == 'a b'
+
+
+def test_base64_filter():
+    assert encode_base64_filter(b'test') == 'dGVzdA=='
+
+
+@pytest.mark.parametrize('input_data, verbose, expected', [
+    (1000, False, '1000.00 Byte'),
+    (1024, False, '1.00 KiB'),
+    (1024 * 1024, False, '1.00 MiB'),
+    (1234.1234, False, '1.21 KiB'),
+    (1000, True, '1000.00 Byte (1,000 bytes)'),
+    (b'abc', False, 'not available'),
+    (None, False, 'not available')
+])
+def test_byte_number_filter(input_data, verbose, expected):
+    assert byte_number_filter(input_data, verbose) == expected
+
+
+@pytest.mark.parametrize('input_data, expected', [
+    (b'abc', b'abc'),
+    (123, '123'),
+    (1234, '1,234'),
+    (1234.1234, '1,234.12'),
+    (None, 'not available')
+])
+def test_nice_number(input_data, expected):
+    assert nice_number_filter(input_data) == expected
+
+
+@pytest.mark.parametrize('input_data, expected', [
+    (b'abc', 'abc'),
+    (1234, '1,234'),
+    ([1, 3], '<ul>\n\t<li>1</li>\n\t<li>3</li>\n</ul>\n'),
+    ({'a': 1}, 'a: 1<br />'),
+    (gmtime(0), '1970-01-01 - 00:00:00'),
+    ('a_b', 'a b'),
+    (gmtime, gmtime)
+])
+def test_generic_nice_representation(input_data, expected):
+    assert generic_nice_representation(input_data) == expected
