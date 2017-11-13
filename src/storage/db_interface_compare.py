0526438d4e64038ac063d252153aa69c3b690360
dorp <dorp@cs.uni-bonn.de>
2017-11-13 13:04:26 +0100

    5: Fixed quotation

--- src/storage/db_interface_compare.py
+++ src/storage/db_interface_compare.py
@@ -15,13 +15,13 @@ class CompareDbInterface(MongoInterfaceCommon):
 
     def add_compare_result(self, compare_result):
         compare_result['_id'] = self._calculate_compare_result_id(compare_result)
-        compare_result["submission_date"] = time()
+        compare_result['submission_date'] = time()
         try:
             self.compare_results.delete_one({'_id': compare_result['_id']})
         except Exception:
             pass
         self.compare_results.insert_one(compare_result)
-        logging.info("compare result added to db: {}".format(compare_result['_id']))
+        logging.info('compare result added to db: {}'.format(compare_result['_id']))
 
     def get_compare_result(self, compare_id):
         compare_id = unify_string_list(compare_id)
@@ -29,10 +29,10 @@ class CompareDbInterface(MongoInterfaceCommon):
         if err is None:
             compare_result = self.compare_results.find_one(compare_id)
             if compare_result:
-                logging.debug("got compare result from db: {}".format(compare_id))
+                logging.debug('got compare result from db: {}'.format(compare_id))
                 return compare_result
             else:
-                logging.debug("compare result not found in db: {}".format(compare_id))
+                logging.debug('compare result not found in db: {}'.format(compare_id))
                 return None
         else:
             return err
@@ -42,7 +42,7 @@ class CompareDbInterface(MongoInterfaceCommon):
         err = None
         for uid in uids:
             if not self.existence_quick_check(uid):
-                err = "{} not found in database".format(uid)
+                err = '{} not found in database'.format(uid)
         return err
 
     def compare_result_is_in_db(self, compare_id):
@@ -51,10 +51,10 @@ class CompareDbInterface(MongoInterfaceCommon):
 
     def delete_old_compare_result(self, compare_id):
         try:
-            self.compare_results.remove({"_id": unify_string_list(compare_id)})
-            logging.debug("old compare result deleted: {}".format(compare_id))
+            self.compare_results.remove({'_id': unify_string_list(compare_id)})
+            logging.debug('old compare result deleted: {}'.format(compare_id))
         except Exception as e:
-            logging.warning("Could not delete old compare result: {} {}".format(sys.exc_info()[0].__name__, e))
+            logging.warning('Could not delete old compare result: {} {}'.format(sys.exc_info()[0].__name__, e))
 
     @staticmethod
     def _calculate_compare_result_id(compare_result):
@@ -64,10 +64,10 @@ class CompareDbInterface(MongoInterfaceCommon):
         return comp_id
 
     def page_compare_results(self, skip=0, limit=0):
-        db_entries = self.compare_results.find({'submission_date': {"$gt": 1}}, {"general.hid": 1, "submission_date": 1}, skip=skip, limit=limit, sort=[("submission_date", -1)])
-        all_previous_results = [(item["_id"], item["general"]["hid"], item["submission_date"]) for item in db_entries]
+        db_entries = self.compare_results.find({'submission_date': {'$gt': 1}}, {'general.hid': 1, 'submission_date': 1}, skip=skip, limit=limit, sort=[('submission_date', -1)])
+        all_previous_results = [(item['_id'], item['general']['hid'], item['submission_date']) for item in db_entries]
         return [compare for compare in all_previous_results if not self.object_existence_quick_check(compare[0])]
 
     def get_total_number_of_results(self):
-        db_entries = self.compare_results.find({'submission_date': {"$gt": 1}}, {"_id": 1})
+        db_entries = self.compare_results.find({'submission_date': {'$gt': 1}}, {'_id': 1})
         return sum(1 for entry in db_entries if not self.object_existence_quick_check(entry['_id']))  # sum(1 for... calculates length of generator
