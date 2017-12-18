ce2d46b9c2f58ec393950dee414947f3428039ed
Peter Weidenbach <peter.weidenbach@fkie.fraunhofer.de>
2017-12-18 16:23:28 +0100

    avm tool installer added

--- src/bootstrap/bootstrap_common.sh
+++ src/bootstrap/bootstrap_common.sh
@@ -7,6 +7,7 @@ echo "####################################"
 sudo apt-get update
 sudo apt-get upgrade -y
 sudo apt-get autoremove -y
+sudo apt-get clean
 
 echo "####################################"
 echo "#  installing general dependencies #"
