f7a00c6355e14ce6b413157bdcc1120d6552d1d6
Jörg Stucke <joerg.stucke@fkie.fraunhofer.de>
2017-12-18 16:04:01 +0100

    added new comparison starting method and removed old start comparison page

--- src/web_interface/components/compare_routes.py
+++ src/web_interface/components/compare_routes.py
@@ -2,11 +2,11 @@
 
 import logging
 
-from flask import render_template, request, redirect, url_for
+from flask import render_template, request, redirect, url_for, session
 from flask_paginate import Pagination
 
-from helperFunctions.dataConversion import string_list_to_list, unify_string_list
-from helperFunctions.web_interface import ConnectTo, apply_filters_to_query
+from helperFunctions.dataConversion import string_list_to_list, unify_string_list, list_to_unified_string_list
+from helperFunctions.web_interface import ConnectTo
 from intercom.front_end_binding import InterComFrontEndBinding
 from storage.db_interface_compare import CompareDbInterface
 from web_interface.components.component_base import ComponentBase
@@ -14,7 +14,7 @@ from web_interface.components.component_base import ComponentBase
 
 class CompareRoutes(ComponentBase):
     def _init_component(self):
-        self._app.add_url_rule("/compare", "/compare/", self._app_show_start_compare, methods=["GET", "POST"])
+        self._app.add_url_rule("/compare", "/compare/", self._app_show_start_compare)
         self._app.add_url_rule("/database/browse_compare", "database/browse_compare", self._app_show_browse_compare)
         self._app.add_url_rule("/compare/<compare_id>", "/compare/<compare_id>", self._app_show_compare_result)
 
@@ -31,25 +31,25 @@ class CompareRoutes(ComponentBase):
         return render_template("compare/error.html", error=result.__str__())
 
     def _app_show_start_compare(self):
-        if request.method == "POST":
-            compare_id = request.form.get("uid_list")
-            redo = True if request.form.get("force") else None
-            unified_compare_id = unify_string_list(compare_id)
+        if 'uids_for_comparison' not in session or not isinstance(session['uids_for_comparison'], list) or len(session['uids_for_comparison']) < 2:
+            return render_template("compare/error.html", error='No UIDs found for comparison')
+        compare_id = list_to_unified_string_list(session['uids_for_comparison'])
+        session['uids_for_comparison'] = None
+        redo = True if request.args.get('force_recompare') else None
 
-            with ConnectTo(CompareDbInterface, self._config) as sc:
-                compare_exists = sc.compare_result_is_in_db(unified_compare_id)
-            if compare_exists and not redo:
-                return redirect(url_for("/compare/<compare_id>", compare_id=compare_id))
+        with ConnectTo(CompareDbInterface, self._config) as sc:
+            compare_exists = sc.compare_result_is_in_db(compare_id)
+        if compare_exists and not redo:
+            return redirect(url_for("/compare/<compare_id>", compare_id=compare_id))
 
-            with ConnectTo(CompareDbInterface, self._config) as sc:
-                err = sc.object_existence_quick_check(compare_id)
-            if err is not None:
-                return render_template("compare/error.html", error=err.__str__())
+        with ConnectTo(CompareDbInterface, self._config) as sc:
+            err = sc.object_existence_quick_check(compare_id)
+        if err is not None:
+            return render_template("compare/error.html", error=err.__str__())
 
-            with ConnectTo(InterComFrontEndBinding, self._config) as sc:
-                sc.add_compare_task(unified_compare_id, force=redo)
-            return render_template("compare/wait.html", compare_id=unified_compare_id)
-        return render_template("compare/selection.html")
+        with ConnectTo(InterComFrontEndBinding, self._config) as sc:
+            sc.add_compare_task(compare_id, force=redo)
+        return render_template("compare/wait.html", compare_id=compare_id)
 
     @staticmethod
     def _create_ida_download_if_existing(result, compare_id):
