a70fbfe20ce9e71db127b11ca52bfcb60b923c6a
Peter Weidenbach <peter.weidenbach@fkie.fraunhofer.de>
2017-12-19 13:50:22 +0100

    sqfs multiple tool try unpacking

--- src/plugins/unpacking/squashFS/code/squashFS.py
+++ src/plugins/unpacking/squashFS/code/squashFS.py
@@ -2,12 +2,17 @@
 This plugin unpacks SquashFS filesystem images
 '''
 from common_helper_process import execute_shell_command
-import logging
+from common_helper_files import get_files_in_dir
+import os
+
+THIS_FILES_DIR = os.path.dirname(os.path.abspath(__file__))
+BIN_DIR = os.path.join(THIS_FILES_DIR, '../bin')
+
 
 name = 'SquashFS'
 mime_patterns = ['filesystem/squashfs']
-version = '0.6'
-squash_unpacker = 'sasquatch'
+version = '0.7'
+squash_unpacker = ['sasquatch', BIN_DIR + '/unsquashfs4-avm-be', BIN_DIR + '/unsquashfs4-avm-le', BIN_DIR + '/unsquashfs4-avm-le']
 
 
 def unpack_function(file_path, tmp_dir):
@@ -15,8 +20,24 @@ def unpack_function(file_path, tmp_dir):
     file_path specifies the input file.
     tmp_dir should be used to store the extracted files.
     '''
-    output = execute_shell_command('fakeroot {} -d {}/faf_extracted {}'.format(squash_unpacker, tmp_dir, file_path))
-    return {'output': output}
+    unpack_result = dict()
+    for unpacker in squash_unpacker:
+        output = execute_shell_command('fakeroot {} -d {}/faf_extracted {}'.format(unpacker, tmp_dir, file_path))
+        if _unpack_success(tmp_dir):
+            unpack_result['unpacking_tool'] = _get_unpacker_name(unpacker)
+            unpack_result['output'] = output
+            break
+        else:
+            unpack_result[_get_unpacker_name(unpacker)] = output
+    return unpack_result
+
+
+def _get_unpacker_name(unpacker_path):
+    return unpacker_path.split('/')[-1]
+
+
+def _unpack_success(tmp_dir):
+    return len(get_files_in_dir(tmp_dir)) > 0
 
 
 # ----> Do not edit below this line <----
--- src/plugins/unpacking/squashFS/test/test_plugin_squashfs.py
+++ src/plugins/unpacking/squashFS/test/test_plugin_squashfs.py
@@ -1,8 +1,38 @@
 import os
+import pytest
+from tempfile import TemporaryDirectory
+
 from test.unit.unpacker.test_unpacker import TestUnpackerBase
+
+from ..code.squashFS import _get_unpacker_name, _unpack_success, unpack_function
 
 
 TEST_DATA_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'data')
+
+
+@pytest.mark.parametrize('unpacker_path, expected', [
+    ('/foo/bar/unpacker', 'unpacker'),
+    ('unpacker', 'unpacker'),
+])
+def test_get_unpacker_name(unpacker_path, expected):
+    assert _get_unpacker_name(unpacker_path) == expected
+
+
+@pytest.mark.parametrize('unpack_path, expected', [
+    ('/foo/bar/unpacker', False),
+    (TEST_DATA_DIR, True),
+])
+def test_unpack_success(unpack_path, expected):
+    assert _unpack_success(unpack_path) == expected
+
+
+def test_not_unpackable_file():
+    empty_test_file = os.path.join(TEST_DATA_DIR, 'empty')
+    unpack_dir = TemporaryDirectory(prefix='fact_test_')
+    result = unpack_function(empty_test_file, unpack_dir)
+    assert 'output' not in result.keys()
+    assert 'sasquatch' in result.keys()
+    assert 'unsquashfs4-avm-be' in result.keys()
 
 
 class TestSquashUnpacker(TestUnpackerBase):
