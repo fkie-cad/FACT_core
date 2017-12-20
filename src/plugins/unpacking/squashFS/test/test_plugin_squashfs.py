5643c08a43e0b60cce72b685b6247fcac5f0529c
Peter Weidenbach <peter.weidenbach@fkie.fraunhofer.de>
2017-12-20 11:40:25 +0100

    legacy renaming removed

--- src/plugins/unpacking/squashFS/test/test_plugin_squashfs.py
+++ src/plugins/unpacking/squashFS/test/test_plugin_squashfs.py
@@ -39,4 +39,4 @@ class TestSquashUnpacker(TestUnpackerBase):
         self.check_unpacker_selection('filesystem/squashfs', 'SquashFS')
 
     def test_extraction_sqfs(self):
-        self.check_unpacking_of_standard_unpack_set(os.path.join(TEST_DATA_DIR, 'sqfs.img'), additional_prefix_folder='faf_extracted')
+        self.check_unpacking_of_standard_unpack_set(os.path.join(TEST_DATA_DIR, 'sqfs.img'), additional_prefix_folder='fact_extracted')
