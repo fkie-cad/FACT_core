3fbb0f3dc22b75c0ce4d2a7bcdf94bdcf0558c64
Peter Weidenbach <peter.weidenbach@fkie.fraunhofer.de>
2018-01-09 10:33:30 +0100

    tags added to firmware object and test coverage improved

--- src/objects/firmware.py
+++ src/objects/firmware.py
@@ -1,11 +1,12 @@
 from objects.file import FileObject
 from helperFunctions.hash import get_md5
+from contextlib import suppress
 
 
 class Firmware(FileObject):
-    """
+    '''
     This objects represents a firmware
-    """
+    '''
 
     def __init__(self, binary=None, file_name=None, file_path=None, scheduled_analysis=None):
         super().__init__(binary=binary, file_name=file_name, file_path=file_path, scheduled_analysis=scheduled_analysis)
@@ -14,6 +15,7 @@ class Firmware(FileObject):
         self.device_class = None
         self.vendor = None
         self.release_date = None
+        self.tags = set()
         self._update_root_id_and_virtual_path()
 
     def set_device_name(self, device_name):
@@ -40,14 +42,21 @@ class Firmware(FileObject):
         self.root_uid = self.get_uid()
         self.virtual_file_path = {self.get_uid(): [self.get_uid()]}
 
+    def set_tag(self, tag):
+        self.tags.add(tag)
+
+    def remove_tag(self, tag):
+        with suppress(KeyError):
+            self.tags.remove(tag)
+
     def get_hid(self, root_uid=None):
         '''
         return a human readable identifier
         '''
-        return "{} {} - {}".format(self.vendor, self.device_name, self.version)
+        return '{} {} - {}'.format(self.vendor, self.device_name, self.version)
 
     def __str__(self):
-        return "Device Name: {}\nFW Version: {}\nProcessed Analysis: {}\nScheduled Analysis: {}".format(self.device_name, self.version, list(self.processed_analysis.keys()), self.scheduled_analysis)
+        return 'Device Name: {}\nFW Version: {}\nProcessed Analysis: {}\nScheduled Analysis: {}'.format(self.device_name, self.version, list(self.processed_analysis.keys()), self.scheduled_analysis)
 
     def __repr__(self):
         return self.__str__()
--- src/test/unit/objects/test_firmware.py
+++ src/test/unit/objects/test_firmware.py
@@ -1,36 +1,69 @@
 from common_helper_files import get_binary_from_file
-import unittest
+import pytest
 
 from helperFunctions.fileSystem import get_test_data_dir
 from objects.firmware import Firmware
 
 
-class TestFirmwareObject(unittest.TestCase):
+@pytest.mark.parametrize('input_data, expected_count', [
+    (['a'], 1),
+    (['a', 'b', 'a'], 2)
+])
+def test_add_tag(input_data, expected_count):
+    test_object = Firmware()
+    for item in input_data:
+        test_object.set_tag(item)
+    for item in input_data:
+        assert item in test_object.tags
+    assert len(test_object.tags) == expected_count
 
-    def test_create_firmware_container_raw(self):
-        test_object = Firmware()
-        self.assertEqual(test_object.size, None, 'correct size')
-        self.assertEqual(test_object.binary, None, 'correct binary')
 
-    def test_create_firmware_from_file(self):
-        test_object = Firmware()
-        test_object.create_from_file('{}/test_data_file.bin'.format(get_test_data_dir()))
-        self.assertEqual(test_object.device_name, None, 'correct device name')
-        self.assertEqual(test_object.size, 19, 'correct size')
-        self.assertEqual(test_object.binary, b'test string in file', 'correct binary data')
-        self.assertEqual(test_object.sha256, '268d870ffa2b21784e4dc955d8e8b8eb5f3bcddd6720a1e6d31d2cf84bd1bff8', 'correct sha256')
-        self.assertEqual(test_object.file_name, 'test_data_file.bin', 'correct file name')
+@pytest.mark.parametrize('tag_set, remove_items, expected_count', [
+    (set('ab'), ['a'], 1),
+    (set('ab'), ['a', 'b', 'a'], 0)
+])
+def test_remove_tag(tag_set, remove_items, expected_count):
+    test_fw = Firmware()
+    test_fw.tags = tag_set
+    for item in remove_items:
+        test_fw.remove_tag(item)
+    assert len(test_fw.tags) == expected_count
 
-    def test_set_binary(self):
-        binary = get_binary_from_file('{}/get_files_test/testfile1'.format(get_test_data_dir()))
-        md5 = 'e802ca22f6cd2d9357cf3da1d191879e'
-        firmware = Firmware()
-        firmware.set_binary(binary)
-        self.assertEqual(firmware.md5, md5, 'correct md5 sum')
 
-    def test_get_hid(self):
-        test_fw = Firmware(binary=b'foo')
-        test_fw.set_device_name('test_device')
-        test_fw.set_vendor('foo')
-        test_fw.set_firmware_version('1.0')
-        self.assertEqual(test_fw.get_hid(), 'foo test_device - 1.0', 'hid not correct')
+def test_create_firmware_container_raw():
+    test_object = Firmware()
+    assert test_object.size is None
+    assert test_object.binary is None
+
+
+def test_create_firmware_from_file():
+    test_object = Firmware()
+    test_object.create_from_file('{}/test_data_file.bin'.format(get_test_data_dir()))
+    assert test_object.device_name is None
+    assert test_object.size == 19
+    assert test_object.binary == b'test string in file'
+    assert test_object.sha256 == '268d870ffa2b21784e4dc955d8e8b8eb5f3bcddd6720a1e6d31d2cf84bd1bff8'
+    assert test_object.file_name == 'test_data_file.bin'
+
+
+def test_set_binary():
+    binary = get_binary_from_file('{}/get_files_test/testfile1'.format(get_test_data_dir()))
+    md5 = 'e802ca22f6cd2d9357cf3da1d191879e'
+    firmware = Firmware()
+    firmware.set_binary(binary)
+    assert firmware.md5 == md5
+
+
+def test_get_hid():
+    test_fw = Firmware(binary=b'foo')
+    test_fw.set_device_name('test_device')
+    test_fw.set_vendor('foo')
+    test_fw.set_firmware_version('1.0')
+    assert test_fw.get_hid() == 'foo test_device - 1.0'
+
+
+def test_repr_and_str():
+    test_fw = Firmware(scheduled_analysis=['test'])
+    assert 'Device Name: None' in test_fw.__str__()
+    assert 'test' in test_fw.__str__()
+    assert test_fw.__str__() == test_fw.__repr__()
