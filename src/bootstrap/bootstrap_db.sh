4e33ce048c8470b8efa17a078830fa521f40a93f
Peter Weidenbach <peter.weidenbach@fkie.fraunhofer.de>
2018-01-11 16:46:26 +0100

    mongo server upgrade

--- src/bootstrap/bootstrap_db.sh
+++ src/bootstrap/bootstrap_db.sh
@@ -11,10 +11,12 @@ cd $CURRENT_FILE_DIR
 echo "####################################"
 echo "#       installing MongoDB         #"
 echo "####################################"
-sudo -E apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 0C49F3730359A14518585931BC711F9BA15703C6
-echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
-sudo -E apt-get update
-sudo -E apt-get install -y mongodb-org
+sudo -E apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2930ADAE8CAF5059EE73BB4B58712A2291FA4AD5
+sudo rm /etc/apt/sources.list.d/mongodb-org-3.*
+echo "deb https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.6 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.6.list
+sudo -E apt update
+sudo -E apt install -y mongodb-org
+sudo -E apt upgrade -y
 
 
 echo "####################################"
