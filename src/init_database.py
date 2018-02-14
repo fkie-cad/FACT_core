b47e814f6a78051f8cb850093b08bbb74a689962
Peter Weidenbach <peter.weidenbach@fkie.fraunhofer.de>
2018-02-14 13:19:44 +0100

    refactoring

--- src/init_database.py
+++ src/init_database.py
@@ -17,47 +17,25 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 '''
 
-import argparse
-import configparser
 import logging
+import sys
 
 from storage.MongoMgr import MongoMgr
-from helperFunctions.config import get_config_dir
+from helperFunctions.fact_init import setup_argparser, load_config, setup_logging
 
 
 PROGRAM_NAME = "FACT Database Initializer"
-PROGRAM_VERSION = "0.1"
+PROGRAM_VERSION = "0.2"
 PROGRAM_DESCRIPTION = "Initialize authentication and users for FAF's Database"
 
 
-def _setup_argparser():
-    parser = argparse.ArgumentParser(description="{} - {}".format(PROGRAM_NAME, PROGRAM_DESCRIPTION))
-    parser.add_argument('-V', '--version', action='version', version="{} {}".format(PROGRAM_NAME, PROGRAM_VERSION))
-    parser.add_argument("-C", "--config_file", help="set path to config File", default="{}/main.cfg".format(get_config_dir()))
-    return parser.parse_args()
-
-
-def _load_config(args):
-    config = configparser.ConfigParser()
-    config.read(args.config_file)
-    return config
-
-
-def _setup_logging():
-    log_format = logging.Formatter(fmt="[%(asctime)s][%(module)s][%(levelname)s]: %(message)s", datefmt="%Y-%m-%d %H:%M:%S")
-    logger = logging.getLogger('')
-    logger.setLevel(logging.INFO)
-    console_logger = logging.StreamHandler()
-    console_logger.setFormatter(log_format)
-    logger.addHandler(console_logger)
-
-
 if __name__ == '__main__':
-    args = _setup_argparser()
-    config = _load_config(args)
-    _setup_logging()
+    args = setup_argparser()
+    config = load_config(args)
+    setup_logging()
 
     logging.info("Trying to start Mongo Server and initializing users...")
     mongo_manger = MongoMgr(config=config, auth=False)
     mongo_manger.init_users()
     mongo_manger.shutdown()
+    sys.exit()
