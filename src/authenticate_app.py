1501189665503f6bac79e9277fd28e25e26f4f04
dorp <dorp@cs.uni-bonn.de>
2018-02-16 15:17:33 +0100

    Added user management and support for api keys

--- src/authenticate_app.py
+++ src/authenticate_app.py
@@ -1,3 +1,4 @@
+import base64
 import os
 
 from flask_security import Security, SQLAlchemyUserDatastore, UserMixin, RoleMixin
@@ -5,17 +6,38 @@ from flask_sqlalchemy import SQLAlchemy
 
 
 def add_flask_security_to_app(app, config):
-    # Create app
+    _add_configuration_to_app(app, config)
+
+    db = SQLAlchemy(app)
+
+    user_datastore = create_db_interface(db)
+    security = Security(app, user_datastore)
+
+    _add_apikey_handler(security, user_datastore)
+
+    return user_datastore, security
+
+
+def _add_apikey_handler(security, user_datastore):
+    @security.login_manager.request_loader
+    def load_user_from_request(request):
+        api_key = request.headers.get('Authorization')
+        if api_key:
+            user = user_datastore.find_user(api_key=api_key)
+            if user:
+                return user
+        return None
+
+
+def _add_configuration_to_app(app, config):
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config['SECURITY_PASSWORD_SALT'] = os.urandom(24)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://'
+    app.config['SECURITY_PASSWORD_SALT'] = b'SUPERH\x45H'
+    app.config['SQLALCHEMY_DATABASE_URI'] = config.get('data_storage', 'user_database', fallback='sqlite:///')
     app.config['SECURITY_UNAUTHORIZED_VIEW'] = '/login'
     app.config['LOGIN_DISABLED'] = not config.getboolean('ExpertSettings', 'authentication')
 
-    # Create database connection object
-    db = SQLAlchemy(app)
 
-    # Define models
+def create_db_interface(db):
     roles_users = db.Table('roles_users', db.Column('user_id', db.Integer(), db.ForeignKey('user.id')), db.Column('role_id', db.Integer(), db.ForeignKey('role.id')))
 
     class Role(db.Model, RoleMixin):
@@ -25,25 +47,16 @@ def add_flask_security_to_app(app, config):
 
     class User(db.Model, UserMixin):
         id = db.Column(db.Integer, primary_key=True)
+        api_key = db.Column(db.String(255), default=_build_api_key(), unique=True)
         email = db.Column(db.String(255), unique=True)
         password = db.Column(db.String(255))
         active = db.Column(db.Boolean())
         confirmed_at = db.Column(db.DateTime())
-        roles = db.relationship('Role', secondary=roles_users,
-                                backref=db.backref('users', lazy='dynamic'))
+        roles = db.relationship('Role', secondary=roles_users, backref=db.backref('users', lazy='dynamic'))
 
-    # Setup Flask-Security
-    user_datastore = SQLAlchemyUserDatastore(db, User, Role)
-    security = Security(app, user_datastore)
+    return SQLAlchemyUserDatastore(db, User, Role)
 
-    # Create a user to test with
-    @app.before_first_request
-    def create_user():
-        db.create_all()
-        user_datastore.create_user(email='test', password='test')
-        user_datastore.activate_user(user_datastore.find_user(email='test'))
-        user_datastore.create_role(name='superuser')
-        user_datastore.add_role_to_user(user=user_datastore.find_user(email='test'), role='superuser')
-        db.session.commit()
 
-    return user_datastore, security
+def _build_api_key():
+    raw_key = os.urandom(32)
+    return base64.standard_b64encode(raw_key).decode()
