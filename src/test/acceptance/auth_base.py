6361fa15739f02b08f06e0bb4f460e64bbc10c01
dorp <dorp@cs.uni-bonn.de>
2018-02-19 14:09:18 +0100

    Added acceptance test for api key authentication

--- src/test/acceptance/auth_base.py
+++ src/test/acceptance/auth_base.py
@@ -1,8 +1,11 @@
 import os
+from collections import namedtuple
 
 from test.acceptance.base import TestAcceptanceBase
 from helperFunctions.fileSystem import get_test_data_dir
 
+ALL_ENDPOINTS_2018_02_19 = ['/database/database_binary_search_results.html',  '/database/advanced_search',  '/database/browse_compare',  '/database/binary_search',  '/database/quick_search',  '/database/browse',  '/database/search',  '/rest/file_object',  '/rest/firmware',  '/rest/compare',  '/rest/file_object/<uid>',  '/rest/firmware/<uid>',  '/rest/compare/<compare_id>',  '/rest/binary/<uid>',  '/system_health',  '/statistic',  '/compare',  '/upload',  '/about',  '/',  '/comparison/remove_all/<analysis_uid>',  '/comparison/remove/<analysis_uid>/<compare_uid>',  '/comparison/add/<uid>',  '/analysis/<uid>/ro/<root_uid>',  '/analysis/<uid>/<selected_analysis>/ro/<root_uid>',  '/compare/ajax_common_files/<compare_id>/<feature_id>/',  '/compare/ajax_tree/<compare_id>/<root_uid>/<uid>',  '/admin/delete_comment/<uid>/<timestamp>',  '/admin/re-do_analysis/<uid>',  '/admin/delete/<uid>',  '/base64-download/<uid>/<section>/<expression_id>',  '/ajax_get_binary/<mime_type>/<uid>',  '/update-analysis/<uid>',  '/tar-download/<uid>',  '/ida-download/<compare_id>',  '/ajax_tree/<uid>/<root_uid>',  '/ajax_root/<uid>',  '/analysis/<uid>/<selected_analysis>',  '/analysis/<uid>',  '/download/<uid>',  '/hex-dump/<uid>',  '/compare/<compare_id>',  '/comment/<uid>',  '/static/<filename>']
+MockUser = namedtuple('MockUser', ['name', 'password', 'key'])
 
 class TestAuthenticatedAcceptanceBase(TestAcceptanceBase):
     '''
@@ -17,4 +20,8 @@ class TestAuthenticatedAcceptanceBase(TestAcceptanceBase):
     def _set_config(cls):
         super()._set_config()
         cls.config.set('ExpertSettings', 'authentication', 'true')
-        cls.config.set('data_storage', 'user_database', os.path.join(get_test_data_dir(), 'user_test.db'))
+        cls.config.set('data_storage', 'user_database', ''.join(['sqlite:///', get_test_data_dir(), '/user_test.db']))
+
+        cls.guest = MockUser(name='t_guest', password='test', key='1okMSKUKlYxSvPn0sgfHM0SWd9zqNChyj5fbcIJgfKM=')
+        cls.guest_analyst = MockUser(name='t_guest_analyst', password='test', key='mDsgjAM2iE543PySnTpPZr0u8KeGTPGzPjKJVO4I4Ww=')
+        cls.superuser = MockUser(name='t_superuser', password='test', key='k2GKnNaA5UlENStVI4AEJKQ7BP9ZqO+21Cx746BjJDo=')
--- src/test/acceptance/test_authenticated_gui.py
+++ src/test/acceptance/test_authenticated_gui.py
@@ -1,20 +1,12 @@
-from test.common_helper import create_test_firmware
 from test.acceptance.auth_base import TestAuthenticatedAcceptanceBase
-from storage.db_interface_backend import BackEndDbInterface
 
 
 class TestAcceptanceNormalSearch(TestAuthenticatedAcceptanceBase):
 
     def setUp(self):
         super().setUp()
-        self._start_backend()
-        # self.db_backend_interface = BackEndDbInterface(self.config)
-        # self.test_fw = create_test_firmware(device_name='test_fw')
-        # self.db_backend_interface.add_firmware(self.test_fw)
 
     def tearDown(self):
-        # self.db_backend_interface.shutdown()
-        self._stop_backend()
         super().tearDown()
 
     def test_redirection(self):
@@ -24,3 +16,27 @@ class TestAcceptanceNormalSearch(TestAuthenticatedAcceptanceBase):
     def test_show_login_page(self):
         response = self.test_client.get('/', follow_redirects=True)
         self.assertIn(b'Remember Me', response.data, 'no authorization required')
+
+    def test_api_key_auth(self):
+        response = self.test_client.get('/', headers={'Authorization': self.guest.key}, follow_redirects=True)
+        self.assertNotIn(b'Remember Me', response.data, 'authorization not working')
+
+    def test_role_based_access(self):
+        self._start_backend()
+        response = self.test_client.get('/upload', headers={'Authorization': self.guest.key}, follow_redirects=True)
+        self.assertIn(b'Remember Me', response.data, 'upload should not be accessible for guest')
+
+        response = self.test_client.get('/upload', headers={'Authorization': self.guest_analyst.key}, follow_redirects=True)
+        self.assertIn(b'Remember Me', response.data, 'upload should not be accessible for guest_analyst')
+
+        response = self.test_client.get('/upload', headers={'Authorization': self.superuser.key}, follow_redirects=True)
+        self.assertNotIn(b'Remember Me', response.data, 'upload should not be accessible for guest')
+        self._stop_backend()
+
+    def test_login(self):
+        '''
+        As of now, not working in tests. Can not yet determine the reason. Maybe bad creation of request.
+        Does not apply to production code though.
+        Writing tests for this is postponed for now.
+        '''
+        pass
