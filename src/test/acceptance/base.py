558e3c9bf1dcf5aa7ecf441774993395ce075378
dorp <dorp@cs.uni-bonn.de>
2018-02-19 10:37:28 +0100

    Added first acceptance tests for authentication

--- src/test/acceptance/base.py
+++ src/test/acceptance/base.py
@@ -39,7 +39,7 @@ class TestAcceptanceBase(unittest.TestCase):
 
     def setUp(self):
         self.frontend = WebFrontEnd(config=self.config)
-        self.frontend.app.config['TESTING'] = True
+        self.frontend.app.config['TESTING'] = not self.config.getboolean('ExpertSettings', 'authentication')
         self.test_client = self.frontend.app.test_client()
 
         self.test_fw_a = self.TestFW('418a54d78550e8584291c96e5d6168133621f352bfc1d43cf84e81187fef4962_787',
