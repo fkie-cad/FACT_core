9215ade6da5079d04ccf56edf8f20625d35f3622
Jörg Stucke <joerg.stucke@fkie.fraunhofer.de>
2018-02-20 15:50:55 +0100

    fixed bug with wrong directory

--- src/web_interface/components/plugin_routes.py
+++ src/web_interface/components/plugin_routes.py
@@ -10,20 +10,28 @@ from web_interface.components.component_base import ComponentBase
 
 ROUTES_MODULE_NAME = 'routes'
 PLUGIN_CATEGORIES = ['analysis', 'compare', 'unpacking']
+PLUGIN_DIR = '{}/plugins'.format(get_src_dir())
 
 
 class PluginRoutes(ComponentBase):
     def _init_component(self):
-        plugin_list = []
-        for plugin_category in PLUGIN_CATEGORIES:
-            plugin_list.append((plugin_category, self._get_modules_in_path('plugins/{}'.format(plugin_category))))
+        plugin_list = self._find_plugins()
+        self._register_all_plugin_endpoints(plugin_list)
+
+    def _register_all_plugin_endpoints(self, plugin_list):
         for plugin_type, plugins in plugin_list:
             for plugin in plugins:
                 if self._module_has_routes(plugin, plugin_type):
                     self._import_module_routes(plugin, plugin_type)
 
+    def _find_plugins(self):
+        plugin_list = []
+        for plugin_category in PLUGIN_CATEGORIES:
+            plugin_list.append((plugin_category, self._get_modules_in_path('{}/{}'.format(PLUGIN_DIR, plugin_category))))
+        return plugin_list
+
     def _module_has_routes(self, plugin, plugin_type):
-        plugin_components = self._get_modules_in_path('{}/plugins/{}/{}'.format(get_src_dir(), plugin_type, plugin))
+        plugin_components = self._get_modules_in_path('{}/{}/{}'.format(PLUGIN_DIR, plugin_type, plugin))
         return ROUTES_MODULE_NAME in plugin_components
 
     def _import_module_routes(self, plugin, plugin_type):
