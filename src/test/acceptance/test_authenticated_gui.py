65926c54cc22e50b58235bc8939d7fd75190980d
dorp <dorp@cs.uni-bonn.de>
2018-02-22 10:36:00 +0100

    Fixed tests after removing *rembember me* checkbox

--- src/test/acceptance/test_authenticated_gui.py
+++ src/test/acceptance/test_authenticated_gui.py
@@ -1,9 +1,8 @@
-import pytest
-
 from test.acceptance.auth_base import TestAuthenticatedAcceptanceBase
 
 
-class TestAcceptanceNormalSearch(TestAuthenticatedAcceptanceBase):
+class TestAcceptanceAuthentication(TestAuthenticatedAcceptanceBase):
+    UNIQUE_LOGIN_STRING = b'<div><h1>Login</h1></div>'
 
     def test_redirection(self):
         response = self.test_client.get('/', follow_redirects=False)
@@ -11,22 +10,22 @@ class TestAcceptanceNormalSearch(TestAuthenticatedAcceptanceBase):
 
     def test_show_login_page(self):
         response = self.test_client.get('/', follow_redirects=True)
-        self.assertIn(b'Remember Me', response.data, 'no authorization required')
+        self.assertIn(self.UNIQUE_LOGIN_STRING, response.data, 'no authorization required')
 
     def test_api_key_auth(self):
         response = self.test_client.get('/', headers={'Authorization': self.guest.key}, follow_redirects=True)
-        self.assertNotIn(b'Remember Me', response.data, 'authorization not working')
+        self.assertNotIn(self.UNIQUE_LOGIN_STRING, response.data, 'authorization not working')
 
     def test_role_based_access(self):
         self._start_backend()
         response = self.test_client.get('/upload', headers={'Authorization': self.guest.key}, follow_redirects=True)
-        self.assertIn(b'Remember Me', response.data, 'upload should not be accessible for guest')
+        self.assertIn(self.UNIQUE_LOGIN_STRING, response.data, 'upload should not be accessible for guest')
 
         response = self.test_client.get('/upload', headers={'Authorization': self.guest_analyst.key}, follow_redirects=True)
-        self.assertIn(b'Remember Me', response.data, 'upload should not be accessible for guest_analyst')
+        self.assertIn(self.UNIQUE_LOGIN_STRING, response.data, 'upload should not be accessible for guest_analyst')
 
         response = self.test_client.get('/upload', headers={'Authorization': self.superuser.key}, follow_redirects=True)
-        self.assertNotIn(b'Remember Me', response.data, 'upload should not be accessible for guest')
+        self.assertNotIn(self.UNIQUE_LOGIN_STRING, response.data, 'upload should not be accessible for guest')
         self._stop_backend()
 
     def test_login(self):
@@ -51,4 +50,4 @@ class TestAcceptanceNormalSearch(TestAuthenticatedAcceptanceBase):
                 if endpoint.startswith('/static'):
                     pass  # static routes should be served without auth so that css and logos are shown in login screen
                 else:
-                    self.assertIn(b'Remember Me', response.data, 'no authorization required {}'.format(endpoint))
+                    self.assertIn(self.UNIQUE_LOGIN_STRING, response.data, 'no authorization required {}'.format(endpoint))
