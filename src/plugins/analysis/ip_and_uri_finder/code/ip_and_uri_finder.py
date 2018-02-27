ce508a8ff7b3d272af9e443138f8780164ae6ac2
0roman <roman.wagner18@gmx.de>
2018-02-27 16:26:11 +0100

    added geolocation to ipaddresses

--- src/plugins/analysis/ip_and_uri_finder/code/ip_and_uri_finder.py
+++ src/plugins/analysis/ip_and_uri_finder/code/ip_and_uri_finder.py
@@ -1,8 +1,13 @@
 import logging
-
+import os
+import geoip2.database
 from common_analysis_ip_and_uri_finder import CommonAnalysisIPAndURIFinder, ip_and_uri_finder_analysis
-
+from sys import exc_info
 from analysis.PluginBase import AnalysisBasePlugin
+from common_helper_files import get_dir_of_file
+
+
+geoip_database_path = os.path.join(get_dir_of_file(__file__), '../bin/GeoLite2-City/GeoLite2-City.mmdb')
 
 
 class AnalysisPlugin(AnalysisBasePlugin):
@@ -30,9 +35,34 @@ class AnalysisPlugin(AnalysisBasePlugin):
         logging.debug(result)
         for key in ['uris', 'ips_v4', 'ips_v6']:
             result[key] = self._remove_duplicates(result[key])
+        result = self.add_geo_uri_to_ip(result)
         file_object.processed_analysis[self.NAME] = result
         file_object.processed_analysis[self.NAME]['summary'] = self._get_summary(result)
         return file_object
+
+    def add_geo_uri_to_ip(self, result):
+        for key in ['ips_v4', 'ips_v6']:
+            result[key] = self.link_ips_with_geo_location(result[key])
+        return result
+
+    def find_geo_location(self, ip_address):
+        reader = geoip2.database.Reader(geoip_database_path)
+        try:
+            response = reader.city(ip_address)
+            latitude = response.location.latitude
+            longitude = response.location.longitude
+        except Exception as e:
+            logging.error("Geo Location of IP: {} {}".format(exc_info()[0].__name__, e))
+            return ""
+        return (latitude, longitude)
+
+    def link_ips_with_geo_location(self, ip_adresses):
+        linked_ip_geo_list = []
+        for ip in ip_adresses:
+            geo_location = str(self.find_geo_location(ip))
+            link = {"ip": ip, "geo_location": geo_location}
+            linked_ip_geo_list.append(link)
+        return linked_ip_geo_list
 
     @staticmethod
     def _get_summary(results):
--- src/plugins/analysis/ip_and_uri_finder/test/test_ip_and_uri_finder.py
+++ src/plugins/analysis/ip_and_uri_finder/test/test_ip_and_uri_finder.py
@@ -2,8 +2,26 @@ import tempfile
 
 from objects.file import FileObject
 from test.unit.analysis.analysis_plugin_test_class import AnalysisPluginTest
-
 from ..code.ip_and_uri_finder import AnalysisPlugin
+from unittest.mock import patch
+from collections import namedtuple
+
+
+MockResponse = namedtuple('MockResponse', ['location'])
+MockLocation = namedtuple('MockLocation', ['latitude', 'longitude'])
+
+
+class MockReader:
+    def __init__(self, database_path):
+        pass
+
+    def city(self, address):
+        if address == '128.101.101.101':
+            return MockResponse(location=MockLocation(latitude=44.9759, longitude=-93.2166))
+        if address == '1.2.3.4':
+            return MockResponse(location=MockLocation(latitude=47.913, longitude=-122.3042))
+        if address == '1.1.1.123':
+            return MockResponse(location=MockLocation(latitude=-37.7, longitude=145.1833))
 
 
 class TestAnalysisPluginIpAndUriFinder(AnalysisPluginTest):
@@ -15,6 +33,7 @@ class TestAnalysisPluginIpAndUriFinder(AnalysisPluginTest):
         config = self.init_basic_config()
         self.analysis_plugin = AnalysisPlugin(self, config=config)
 
+    @patch('geoip2.database.Reader', MockReader)
     def test_process_object_ips(self):
         tmp = tempfile.NamedTemporaryFile()
         with open(tmp.name, "w") as fp:
@@ -24,11 +43,12 @@ class TestAnalysisPluginIpAndUriFinder(AnalysisPluginTest):
         processed_object = self.analysis_plugin.process_object(tmp_fo)
         results = processed_object.processed_analysis[self.PLUGIN_NAME]
         tmp.close()
-        expected_results_v4 = {"1.2.3.4", "1.1.1.123", "255.255.255.255"}
-        expected_results_v6 = {"1234:1234:abcd:abcd:1234:1234:abcd:abcd", "2001:db8:0:0:8d3::"}
         self.assertEqual(results["uris"], [])
-        self.assertEqual(expected_results_v4, set(results["ips_v4"]))
-        self.assertEqual(expected_results_v6, set(results["ips_v6"]))
+        self.assertIn({"ip": "1.2.3.4", "geo_location": "(47.913, -122.3042)"}, results["ips_v4"])
+        self.assertIn({"ip": "1.1.1.123", "geo_location": "(-37.7, 145.1833)"}, results["ips_v4"])
+        self.assertIn({"ip": "255.255.255.255", "geo_location": ""}, results["ips_v4"])
+        self.assertIn({"ip": "1234:1234:abcd:abcd:1234:1234:abcd:abcd", "geo_location": ""}, results["ips_v6"])
+        self.assertIn({"ip": "2001:db8:0:0:8d3::", "geo_location": ""}, results["ips_v6"])
 
     def test_process_object_uris(self):
         tmp = tempfile.NamedTemporaryFile()
@@ -43,3 +63,26 @@ class TestAnalysisPluginIpAndUriFinder(AnalysisPluginTest):
         self.assertIn("https://www.test.de/test/", results["uris"])
         self.assertIn("ftp://ftp.is.co.za/rfc/rfc1808.txt", results["uris"])
         self.assertIn("telnet://192.0.2.16:80/", results["uris"])
+
+    def test_add_geouri_to_ip(self):
+        test_data = {'ips_v4': ['128.101.101.101', '255.255.255.255'],
+                     'ips_v6': ["1234:1234:abcd:abcd:1234:1234:abcd:abcd"],
+                     "uris": "http://www.google.de"}
+        results = self.analysis_plugin.add_geo_uri_to_ip(test_data)
+        self.assertEqual("http://www.google.de", results["uris"])
+        self.assertEqual([{"ip": "128.101.101.101", "geo_location": "(44.9759, -93.2166)"},
+                          {"ip": "255.255.255.255", "geo_location": ""}], results["ips_v4"])
+        self.assertEqual([{"ip": "1234:1234:abcd:abcd:1234:1234:abcd:abcd", "geo_location": ""}], results["ips_v6"])
+
+    @patch('geoip2.database.Reader', MockReader)
+    def test_find_geo_location(self):
+        self.assertEqual(self.analysis_plugin.find_geo_location('128.101.101.101'), (44.9759, -93.2166))
+        self.assertEqual(self.analysis_plugin.find_geo_location('127.101.101.101'), "")
+        self.assertEqual(self.analysis_plugin.find_geo_location('255.255.255.255'), "")
+
+    @patch('geoip2.database.Reader', MockReader)
+    def test_link_ips_with_geo_location(self):
+        ip_adresses = ["128.101.101.101", "255.255.255.255"]
+        expected_results = [{"ip": "128.101.101.101", "geo_location": "(44.9759, -93.2166)"},
+                            {"ip": "255.255.255.255", "geo_location": ""}]
+        self.assertEqual(self.analysis_plugin.link_ips_with_geo_location(ip_adresses), expected_results)
