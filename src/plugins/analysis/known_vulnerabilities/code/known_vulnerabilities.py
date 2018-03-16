7838792fe205f26cad3806d66e8bab2a0e1984fd
dorp <dorp@cs.uni-bonn.de>
2018-03-16 09:41:23 +0100

    Added more vulnerability definition methods

--- src/plugins/analysis/known_vulnerabilities/code/known_vulnerabilities.py
+++ src/plugins/analysis/known_vulnerabilities/code/known_vulnerabilities.py
@@ -1,4 +1,12 @@
+import json
+import os
+import sys
+
 from analysis.YaraPluginBase import YaraBasePlugin
+
+THIS_FILE = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(os.path.join(THIS_FILE, '..', 'internal'))
+from software_rules import rules
 
 
 class AnalysisPlugin(YaraBasePlugin):
@@ -9,26 +17,31 @@ class AnalysisPlugin(YaraBasePlugin):
     FILE = __file__
 
     def __init__(self, plugin_administrator, config=None, recursive=True):
+        self._software_rules = rules()
+        self._hash_rules = self._initialize_hash_rules()
+
         super().__init__(plugin_administrator, config=config, recursive=recursive, plugin_path=__file__)
-        self._software_rules = self._initialize_rules()
 
     def process_object(self, file_object):
         file_object = super().process_object(file_object)
 
         yara_results = file_object.processed_analysis.pop(self.NAME)
-        binary_vulnerabilities, summary = self._post_process_yara_results(yara_results)
+        binary_vulnerabilities, _ = self._post_process_yara_results(yara_results)
 
-        software_vulnerabilies = self.check_software_components(file_object.processed_analysis['software_components'])
+        software_vulnerabilies = self._check_software_components(file_object.processed_analysis['software_components'])
+
+        hash_vulnerabilities = self._check_hash_vulnerabilities(file_object.processed_analysis['file_hashes']['sha256'])
 
         file_object.processed_analysis[self.NAME] = dict()
-        for name, vulnerability in software_vulnerabilies + binary_vulnerabilities:
+        for name, vulnerability in software_vulnerabilies + binary_vulnerabilities + hash_vulnerabilities:
             file_object.processed_analysis[self.NAME][name] = vulnerability
 
-        file_object.processed_analysis[self.NAME]['summary'] = [item[0] for item in binary_vulnerabilities + software_vulnerabilies]
+        file_object.processed_analysis[self.NAME]['summary'] = [item[0] for item in binary_vulnerabilities + software_vulnerabilies + hash_vulnerabilities]
 
         return file_object
 
-    def _post_process_yara_results(self, yara_results):
+    @staticmethod
+    def _post_process_yara_results(yara_results):
         summary = yara_results.pop('summary')
         new_results = list()
         for result in yara_results:
@@ -36,7 +49,7 @@ class AnalysisPlugin(YaraBasePlugin):
             new_results.append((result, meta))
         return new_results, summary
 
-    def check_software_components(self, software_components_result):
+    def _check_software_components(self, software_components_result):
         found_vulnerabilities = list()
         for software_component in software_components_result.keys():
             for rule in self._software_rules:
@@ -47,53 +60,23 @@ class AnalysisPlugin(YaraBasePlugin):
                     for version in component['meta']['version']:
                         if version:
                             component_version = version
-                    if rule.is_vulnerable(component['rule'], version=component_version):
+                    if rule.is_vulnerable(component_version):
                         found_vulnerabilities.append((software_component, rule.get_dict()))
         return found_vulnerabilities
 
-    def _initialize_rules(self):
-        heartbleed_versions = ['1.0.1{}'.format(minor) for minor in 'abcde']
-        heartbleed = SoftwareRule(score='high', description='The SSL Hearbleed bug allowing buffer overread', reliability='90')
-        heartbleed.set_software('OpenSSL', versions=heartbleed_versions)
+    @staticmethod
+    def _initialize_hash_rules():
+        rule_file = os.path.join(THIS_FILE, '..', 'internal/hash_rules.json')
+        with open(rule_file, 'r') as fd:
+            rules = json.load(fd)
+        return rules
 
-        return [heartbleed, ]
+    def _check_hash_vulnerabilities(self, sha256_hash):
+        vulnerabilities = list()
 
-
-class BadRuleError(ValueError):
-    pass
-
-
-class SoftwareRule:
-    def __init__(self, score, description, reliability):
-        try:
-            self.reliability = int(reliability)
-            assert self.reliability in range(0, 101), 'reliability must be between 0 and 100'
-            self.reliability = str(reliability)
-
-            self.score = score
-            assert self.score in ['low', 'medium', 'high'], ''
-
-            self.description = description
-            assert isinstance(self.description, str), 'description must be a string'
-
-        except (AssertionError, ValueError, TypeError) as exception:
-            raise BadRuleError(str(exception))
-
-        self.software, self._versions = None, None
-
-    def set_software(self, name, versions=None):
-        self.software = name
-        self._versions = versions if versions else []
-
-    def is_vulnerable(self, software, version):
-        assert self.software, 'function must not be called before software is set. See .set_software'
-
-        if not self._versions:
-            return True
-
-        if version in self._versions:
-            return True
-        return False
-
-    def get_dict(self):
-        return dict(description=self.description, score=self.score, reliability=self.reliability)
+        for rule in self._hash_rules :
+            if sha256_hash == rule['sha256']:
+                rule.pop('sha256')
+                name = rule.pop('name')
+                vulnerabilities.append((name, rule))
+        return vulnerabilities
--- src/plugins/analysis/known_vulnerabilities/test/test_known_vulnerabilities.py
+++ src/plugins/analysis/known_vulnerabilities/test/test_known_vulnerabilities.py
@@ -6,7 +6,8 @@ import json
 from objects.file import FileObject
 from test.unit.analysis.analysis_plugin_test_class import AnalysisPluginTest
 
-from ..code.known_vulnerabilities import AnalysisPlugin, SoftwareRule, BadRuleError
+from ..code.known_vulnerabilities import AnalysisPlugin
+from ..internal.software_rules import SoftwareRule, BadRuleError
 
 
 TEST_DATA_DIR = os.path.join(get_dir_of_file(__file__), 'data')
@@ -20,9 +21,12 @@ class TestAnalysisPluginsKnownVulnerabilities(AnalysisPluginTest):
         super().setUp()
         config = self.init_basic_config()
         self.analysis_plugin = AnalysisPlugin(self, config=config)
+        with open(os.path.join(TEST_DATA_DIR, 'sc.json'), 'r') as json_file:
+            self._software_components_result = json.load(json_file)
 
     def test_process_object_yara(self):
         test_file = FileObject(file_path=os.path.join(TEST_DATA_DIR, 'testfile'))
+        test_file.processed_analysis['file_hashes'] = {'sha256': '1234'}
         test_file.processed_analysis['software_components'] = {}
 
         results = self.analysis_plugin.process_object(test_file).processed_analysis[self.PLUGIN_NAME]
@@ -33,8 +37,8 @@ class TestAnalysisPluginsKnownVulnerabilities(AnalysisPluginTest):
 
     def test_process_object_software(self):
         test_file = FileObject(file_path=os.path.join(TEST_DATA_DIR, 'empty'))
-        with open(os.path.join(TEST_DATA_DIR, 'sc.json'), 'r') as json_file:
-            test_file.processed_analysis['software_components'] = json.load(json_file)
+        test_file.processed_analysis['file_hashes'] = {'sha256': '1234'}
+        test_file.processed_analysis['software_components'] = self._software_components_result
 
         results = self.analysis_plugin.process_object(test_file).processed_analysis[self.PLUGIN_NAME]
 
@@ -42,20 +46,41 @@ class TestAnalysisPluginsKnownVulnerabilities(AnalysisPluginTest):
         self.assertTrue('OpenSSL' in results, 'test match not found')
         self.assertEqual(results['OpenSSL']['score'], 'high', 'incorrect or no score found in meta data')
 
+    def test_process_object_software_wrong_version(self):
+        test_file = FileObject(file_path=os.path.join(TEST_DATA_DIR, 'empty'))
+        test_file.processed_analysis['file_hashes'] = {'sha256': '1234'}
+        self._software_components_result['OpenSSL']['meta']['version'] = ['0.9.8', '1.0.0', '']
+        test_file.processed_analysis['software_components'] = self._software_components_result
+
+        results = self.analysis_plugin.process_object(test_file).processed_analysis[self.PLUGIN_NAME]
+
+        self.assertCountEqual(['summary'], list(results.keys()), 'no match should be found')
+
+    def test_process_object_hash(self):
+        test_file = FileObject(file_path=os.path.join(TEST_DATA_DIR, 'empty'))
+        test_file.processed_analysis['file_hashes'] = {'sha256': '7579d10e812905e134cf91ad8eef7b08f87f6f8c8e004ebefa441781fea0ec4a'}
+        test_file.processed_analysis['software_components'] = {}
+
+        results = self.analysis_plugin.process_object(test_file).processed_analysis[self.PLUGIN_NAME]
+
+        self.assertEqual(len(results), 2, 'incorrect number of software components found (summary + one result)')
+        self.assertTrue('Netgear_CGI' in results, 'test match not found')
+        self.assertEqual(results['Netgear_CGI']['score'], 'medium', 'incorrect or no score found in meta data')
+
 
 @pytest.mark.parametrize('reliability', ['no_integer', None, '200'])
 def test_bad_reliability(reliability):
     with pytest.raises(BadRuleError):
-        SoftwareRule(description='', score='high', reliability=reliability)
+        SoftwareRule(description='', score='high', reliability=reliability, software='name')
 
 
 @pytest.mark.parametrize('score', ['higher', None, 50])
 def test_bad_score(score):
     with pytest.raises(BadRuleError):
-        SoftwareRule(description='', score=score, reliability='50')
+        SoftwareRule(description='', score=score, reliability='50', software='name')
 
 
-@pytest.mark.parametrize('description', [None, ])
+@pytest.mark.parametrize('description', [None, 12, dict(prefix='any')])
 def test_bad_description(description):
     with pytest.raises(BadRuleError):
-        SoftwareRule(description=description, score='high', reliability='50')
+        SoftwareRule(description=description, score='high', reliability='50', software='name')
