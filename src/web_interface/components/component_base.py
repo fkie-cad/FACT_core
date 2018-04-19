3c000de1e9df4b9715757b2eca871344711f9297
Jörg Stucke <joerg.stucke@fkie.fraunhofer.de>
2018-04-19 10:56:33 +0200

    pre-review changes

--- src/web_interface/components/component_base.py
+++ src/web_interface/components/component_base.py
@@ -4,12 +4,10 @@ from abc import ABCMeta, abstractmethod
 
 
 class ComponentBase(metaclass=ABCMeta):
-    def __init__(self, app, config, api=None, user_db=None, user_db_interface=None):
+    def __init__(self, app, config, api=None):
         self._app = app
         self._config = config
         self._api = api
-        self._user_db = user_db
-        self._user_db_interface = user_db_interface
 
         self._init_component()
 
