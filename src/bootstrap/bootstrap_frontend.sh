775f0b9bacd8f68be040f0936715a730b6e20f66
Peter Weidenbach <peter.weidenbach@fkie.fraunhofer.de>
2018-05-18 13:38:27 +0200

    faf2fact

--- src/bootstrap/bootstrap_frontend.sh
+++ src/bootstrap/bootstrap_frontend.sh
@@ -87,9 +87,9 @@ then
 	echo "####################################"
 	sudo apt-get install -y nginx
 	echo "generating a new certificate..."
-	openssl genrsa -out faf.key 4096
-	openssl req -new -key faf.key -out faf.csr
-	openssl x509 -req -days 730 -in faf.csr -signkey faf.key -out faf.crt
+	openssl genrsa -out fact.key 4096
+	openssl req -new -key faf.key -out fact.csr
+	openssl x509 -req -days 730 -in fact.csr -signkey fact.key -out fact.crt
 	sudo mv faf.key faf.csr faf.crt /etc/nginx
 	sudo cp /etc/nginx/nginx.conf /etc/nginx/nginx.conf.bak
 	sudo rm /etc/nginx/nginx.conf
