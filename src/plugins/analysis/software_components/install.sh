3f12110a864a97f438460f6dcebe7972894e3628
dorp <dorp@cs.uni-bonn.de>
2018-05-24 07:50:03 +0200

    Fixed testing

--- src/plugins/analysis/software_components/install.sh
+++ src/plugins/analysis/software_components/install.sh
@@ -2,44 +2,10 @@
 
 import os
 import sys
-from contextlib import suppress
 
-
-def get_scanned_software(yara_signature_file):
-    with open(yara_signature_file, 'r') as file:
-        scanned_software = []
-        line = file.readline()
-        while line:
-            line = line.strip()
-            parts_of_line = line.split('=')
-            if parts_of_line[0].strip() == 'software_name':
-                software_name = parts_of_line[1].strip()
-                software_name = software_name.replace('"', '')
-                scanned_software.append(software_name)
-            line = file.readline()
-    return scanned_software
-
-
-def scan():
-    stashed_directory = os.getcwd()
-    os.chdir(os.path.dirname(os.path.abspath(__file__)))
-
-    scanned_software = get_scanned_software('./signatures/os.yara')
-    first = True
-
-    with suppress(FileExistsError):
-        os.mkdir('./bin')
-
-    with open('./bin/__init__.py', 'w') as init_file:
-        init_file.write('OS_LIST = [')
-        for entry in scanned_software:
-            if not first:
-                init_file.write(', ')
-            init_file.write('\'' + entry + '\'')
-            first = False
-        init_file.write(']\n')
-
-    os.chdir(stashed_directory)
+THIS_FILE = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(os.path.join(THIS_FILE, '.', 'internal'))
+from internal.extract_os_names import extract_names
 
 
 def main():
@@ -48,7 +14,7 @@ def main():
         'Installation of Software Components\n'
         '-----------------------------------'
     )
-    scan()
+    extract_names()
     return 0
 
 
