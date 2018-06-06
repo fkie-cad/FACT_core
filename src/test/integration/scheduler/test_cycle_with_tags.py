47fa43e9b19d73649f3ffc42c50923efe4f3e1b9
dorp <dorp@cs.uni-bonn.de>
2018-06-06 08:52:26 +0200

    fixed tag scheduler test

--- src/test/integration/scheduler/test_cycle_with_tags.py
+++ src/test/integration/scheduler/test_cycle_with_tags.py
@@ -28,13 +28,13 @@ class TestTagPropagation(unittest.TestCase):
         self._mongo_server = MongoMgr(config=self._config, auth=False)
         self.backend_interface = BackEndDbInterface(config=self._config)
 
-        self._analysis_scheduler = AnalysisScheduler(config=self._config, post_analysis=self.count_analysis_finished_event)
+        self._analysis_scheduler = AnalysisScheduler(config=self._config, pre_analysis=self.backend_interface.add_object, post_analysis=self.count_analysis_finished_event)
         self._tagging_scheduler = TaggingDaemon(analysis_scheduler=self._analysis_scheduler)
         self._unpack_scheduler = UnpackingScheduler(config=self._config, post_unpack=self._analysis_scheduler.add_task)
 
     def count_analysis_finished_event(self, fw_object):
-        self.backend_interface.add_object(fw_object)
-        if fw_object.uid == self.uid_of_key_file:
+        self.backend_interface.add_analysis(fw_object)
+        if fw_object.uid == self.uid_of_key_file and 'crypto_material' in fw_object.processed_analysis:
             self.analysis_finished_event.set()
 
     def _wait_for_empty_tag_queue(self):
