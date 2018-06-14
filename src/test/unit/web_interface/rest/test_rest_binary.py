e38a567c1c73c28f5e4fb869bb8f17a62a1aabc9
dorp <dorp@cs.uni-bonn.de>
2018-06-14 11:23:22 +0200

    added unit test for endpoint

--- src/test/unit/web_interface/rest/test_rest_binary.py
+++ src/test/unit/web_interface/rest/test_rest_binary.py
@@ -22,3 +22,10 @@ def test_successful_download(test_app):
     assert result['SHA256'] == TEST_FW.uid.split('_')[0]
     assert result['file_name'] == 'test.zip'
     assert isinstance(standard_b64decode(result['binary']), bytes)
+
+
+def test_successful_tar_download(test_app):
+    result = decode_response(test_app.get('/rest/binary/{}?tar=true'.format(TEST_FW.uid)))
+    # assert result['SHA256'] == TEST_FW.uid.split('_')[0]
+    assert result['file_name'] == 'test.zip.tar.gz'
+    assert isinstance(standard_b64decode(result['binary']), bytes)
