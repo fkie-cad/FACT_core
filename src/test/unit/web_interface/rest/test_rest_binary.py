de7de29c0e41e52b336270c7d176dd26828b4eb9
dorp <dorp@cs.uni-bonn.de>
2018-06-18 12:39:55 +0200

    applied requested changes

--- src/test/unit/web_interface/rest/test_rest_binary.py
+++ src/test/unit/web_interface/rest/test_rest_binary.py
@@ -26,6 +26,11 @@ def test_successful_download(test_app):
 
 def test_successful_tar_download(test_app):
     result = decode_response(test_app.get('/rest/binary/{}?tar=true'.format(TEST_FW.uid)))
-    # assert result['SHA256'] == TEST_FW.uid.split('_')[0]
     assert result['file_name'] == 'test.zip.tar.gz'
     assert isinstance(standard_b64decode(result['binary']), bytes)
+
+
+def test_bad_tar_flag(test_app):
+    result = decode_response(test_app.get('/rest/binary/{}?tar=True'.format(TEST_FW.uid)))
+    assert result['status'] == 1
+    assert 'Malformed tar parameter' in result['error_message']
