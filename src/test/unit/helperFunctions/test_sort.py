adfff520f4428ad4c8c9df2fc4f5be3f45aab7a6
Jörg Stucke <joerg.stucke@fkie.fraunhofer.de>
2018-06-28 15:06:48 +0200

    refactoring + added tests

--- src/test/unit/helperFunctions/test_sort.py
+++ src/test/unit/helperFunctions/test_sort.py
@@ -1,4 +1,4 @@
-from helperFunctions.sort import sort_nice_data_list_by_virtual_path
+from helperFunctions.sort import sort_nice_data_list_by_virtual_path, set_root_uids
 
 
 def test_sort_fo_list_by_virtual_path():
@@ -6,4 +6,15 @@ def test_sort_fo_list_by_virtual_path():
     b = {'virtual_file_paths': ['/b/foobar']}
     c = {'virtual_file_paths': ['/c/foobar']}
     test_list = [c, b, a]
-    assert sort_nice_data_list_by_virtual_path(test_list) == [a, b, c]
+    assert sort_nice_data_list_by_virtual_path(test_list, 'root_uid') == [a, b, c]
+
+
+def test_set_root_uids():
+    root_uid = 'root'
+    test_input = [
+        {'virtual_file_paths': ['/a/foobar'], 'root_uid': 'foo'},
+        {'virtual_file_paths': ['/b/foobar'], 'root_uid': 'bar'},
+        None,
+    ]
+    set_root_uids(test_input, root_uid)
+    assert any(item is None or item['root_uid'] == root_uid for item in test_input)
