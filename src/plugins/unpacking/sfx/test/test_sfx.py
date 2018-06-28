54a36d9aa6d8d95f32f10dcfa709823fb265f491
dorp <dorp@cs.uni-bonn.de>
2018-06-28 16:08:46 +0200

    added password support and binwalk ignore

--- src/plugins/unpacking/sfx/test/test_sfx.py
+++ src/plugins/unpacking/sfx/test/test_sfx.py
@@ -4,12 +4,12 @@ from test.unit.unpacker.test_unpacker import TestUnpackerBase
 TEST_DATA_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'data')
 
 
-class TestElfUnpacker(TestUnpackerBase):
+class TestSfxUnpacker(TestUnpackerBase):
 
     def test_unpacker_selection_generic(self):
-        self.check_unpacker_selection('application/x-executable', 'ELF')
+        self.check_unpacker_selection('application/x-executable', 'SFX')
 
-    def test_extraction_with_normal_elf(self):
+    def test_normal_elf_is_skipped(self):
         files, meta_data = self.unpacker.extract_files_from_file(os.path.join(TEST_DATA_DIR, 'test'), self.tmp_dir.name)
         assert not files, 'no file should be extracted'
         assert 'Will not be extracted' in meta_data['output']
