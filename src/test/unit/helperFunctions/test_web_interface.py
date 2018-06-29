4bec674ca0962c6d41d10df61020ebb9e5d38c00
Jörg Stucke <joerg.stucke@fkie.fraunhofer.de>
2018-06-29 14:03:11 +0200

    removed redundant helper functions

--- src/test/unit/helperFunctions/test_web_interface.py
+++ src/test/unit/helperFunctions/test_web_interface.py
@@ -41,21 +41,3 @@ class normal_user(UserMixin):
 def test_is_superuser(input_data, expected):
     proxied_object = LocalProxy(input_data)
     assert user_has_privilege(proxied_object) == expected
-
-
-@pytest.mark.parametrize('hue, alpha, saturation, lightness, expected', [
-    (0, 0, 0, 0, 'rgba(0, 0, 0, 0)'),
-    (0, 1, 0, 0, 'rgba(0, 0, 0, 255)'),
-    (0, 1, 0, 1, 'rgba(255, 255, 255, 255)'),
-    (0, 1, 1, 1, 'rgba(255, 0, 0, 255)'),
-])
-def test_get_rgba(hue, alpha, saturation, lightness, expected):
-    assert _get_rgba(hue, alpha, saturation, lightness) == expected
-
-
-def test_get_js_list_of_n_uniques_colors():
-    result = get_js_list_of_n_uniques_colors(3)
-    assert len(result) == 3
-    assert any(isinstance(item, str) for item in result)
-    assert any('rgba' in item for item in result)
-    assert any(len(item.split(',')) == 4 for item in result)
