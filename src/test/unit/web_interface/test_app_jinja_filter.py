e714d7ecb1e180837a2f6909bd4fe6ce922c4ca0
Jörg Stucke <joerg.stucke@fkie.fraunhofer.de>
2018-06-29 14:49:08 +0200

    removed unused filter

--- src/test/unit/web_interface/test_app_jinja_filter.py
+++ src/test/unit/web_interface/test_app_jinja_filter.py
@@ -1,6 +1,5 @@
 from flask import render_template_string
 
-from test.common_helper import TEST_FW
 from test.unit.web_interface.base import WebInterfaceTest
 from web_interface.components.jinja_filter import FilterClass
 
@@ -10,7 +9,6 @@ class TestAppShowAnalysis(WebInterfaceTest):
     def setUp(self):
         super().setUp()
         self.filter = FilterClass(self.frontend.app, '', self.config)
-        self.test_fw = TEST_FW
 
     def _get_template_filter_output(self, data, filter_name):
         with self.frontend.app.test_request_context():
@@ -25,10 +23,3 @@ class TestAppShowAnalysis(WebInterfaceTest):
 
         result = self._get_template_filter_output(test_string, 'replace_uid_with_file_name')
         assert '>test_name<' in result
-
-    def test_filter_get_object_binary(self):
-        result = self.filter._filter_get_object_binary(self.test_fw.uid)
-        assert result == self.test_fw.binary
-
-        result = self._get_template_filter_output(self.test_fw.uid, 'get_object_binary')
-        assert self.test_fw.binary.__str__() in result
