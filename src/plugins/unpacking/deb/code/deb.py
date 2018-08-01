d8e1fbe2a188c251ea126ead011c3730185ffeae
Peter Weidenbach <peter.weidenbach@fkie.fraunhofer.de>
2018-08-01 10:26:57 +0200

    review changes

--- src/plugins/unpacking/deb/code/deb.py
+++ src/plugins/unpacking/deb/code/deb.py
@@ -1,11 +1,7 @@
 '''
 This plugin unpacks debian packages
 '''
-import logging
-
 from common_helper_process import execute_shell_command
-
-from helperFunctions.process import program_is_callable
 
 name = 'Deb'
 mime_patterns = ['application/vnd.debian.binary-package']
@@ -13,12 +9,7 @@ version = '0.1'
 
 
 def unpack_function(file_path, tmp_dir):
-    if not program_is_callable("fakeroot"):
-        logging.error("fakeroot not working")
-    elif not program_is_callable("dpkg-deb"):
-        logging.error("dpkg-deb not working!")
-    else:
-        return {'output': execute_shell_command('fakeroot dpkg-deb -v -x {} {}'.format(file_path, tmp_dir))}
+    return {'output': execute_shell_command('fakeroot dpkg-deb -v -x {} {}'.format(file_path, tmp_dir))}
 
 
 # ----> Do not edit below this line <----
