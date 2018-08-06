4517ee9739bd024a2d88623db7745f393226da75
Jörg Stucke <joerg.stucke@fkie.fraunhofer.de>
2018-08-06 11:16:44 +0200

    added rest binary search endpoint

---
+++ src/web_interface/rest/rest_binary_search.py
@@ -0,0 +1,64 @@
+from flask_restful import Resource, request
+
+from helperFunctions.rest import success_message, error_message, convert_rest_request
+from helperFunctions.web_interface import ConnectTo
+from helperFunctions.yara_binary_search import is_valid_yara_rule_file
+from intercom.front_end_binding import InterComFrontEndBinding
+from web_interface.security.decorator import roles_accepted
+from web_interface.security.privileges import PRIVILEGES
+
+
+class RestBinarySearch(Resource):
+    URL = '/rest/binary_search'
+
+    def __init__(self, **kwargs):
+        self.config = kwargs.get('config', None)
+
+    @roles_accepted(*PRIVILEGES['pattern_search'])
+    def post(self):
+        '''
+        The request data should have the form
+        {"rule_file": rule_file, 'uid': firmware_uid}
+        The uid parameter is optional and can be specified if the user want's to search in the files of a single firmware.
+        '''
+        try:
+            data = convert_rest_request(request.data)
+        except TypeError as type_error:
+            return error_message(str(type_error), self.URL, request_data=request.data)
+
+        if 'rule_file' not in data:
+            return error_message('rule_file could not be found in the request data', self.URL)
+
+        if not is_valid_yara_rule_file(data['rule_file']):
+            return error_message('Error in YARA rule file', self.URL, request_data=request.data)
+
+        if data['uid'] and not self._firmware_is_in_db(data['uid']):
+            error_str = 'Firmware with UID {uid} not found in database'.format(uid=data['uid'])
+            return error_message(error_str, self.URL)
+
+        with ConnectTo(InterComFrontEndBinding, self.config) as intercom:
+            search_id = intercom.add_binary_search_request(data['rule_file'], data['uid'])
+
+        return success_message(
+            'Started binary search. Please use GET and the search_id to get the results',
+            self.URL,
+            request_data={'search_id': search_id}
+        )
+
+    @roles_accepted(*PRIVILEGES['pattern_search'])
+    def get(self, search_id=None):
+        '''
+        The search_id is needed to fetch the corresponding search result.
+        The result of the search request can only be fetched once. After this the search needs to be started again.
+        '''
+
+        if search_id is None:
+            return error_message('The search_id is needed to fetch the search result', self.URL)
+
+        with ConnectTo(InterComFrontEndBinding, self.config) as intercom:
+            result, _ = intercom.get_binary_search_result(search_id)
+
+        if not isinstance(result, str):
+            return error_message('The result is not ready yet or it has already been fetched', self.URL)
+
+        return success_message(result, self.URL)
