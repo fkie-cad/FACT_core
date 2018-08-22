4ad3c1fa2d6ddd7337ac22dde2c3105e9236f088
dorp <dorp@cs.uni-bonn.de>
2018-08-22 15:44:37 +0200

    two more wrapper

--- src/test/unit/helperFunctions/test_install.py
+++ src/test/unit/helperFunctions/test_install.py
@@ -1,8 +1,6 @@
 import pytest
 
-from helperFunctions.install import apt_install_packages, apt_remove_packages, pip_install_packages, \
-    pip_remove_packages, apt_update_sources, apt_upgrade_system, check_if_command_in_path, \
-    check_if_executable_in_bin_folder, InstallationError
+from helperFunctions.install import apt_autoremove_packages, apt_clean_system, apt_install_packages, apt_remove_packages, pip_install_packages, pip_remove_packages, apt_update_sources, apt_upgrade_system, check_if_command_in_path, check_if_executable_in_bin_folder, InstallationError
 
 
 def _patch_shell_command(patch, mock_output: str, mock_return_code: int):
@@ -29,6 +27,28 @@ def test_apt_upgrade_system_fails(monkeypatch):
     _patch_shell_command(monkeypatch, 'upgrade failed\n', 255)
     with pytest.raises(InstallationError):
         apt_upgrade_system()
+
+
+def test_apt_autoremove_packages(monkeypatch):
+    _patch_shell_command(monkeypatch, 'removal succeeded\n', 0)
+    assert 'succeeded' in apt_autoremove_packages()
+
+
+def test_apt_autoremove_packages_fails(monkeypatch):
+    _patch_shell_command(monkeypatch, 'removal failed\n', 255)
+    with pytest.raises(InstallationError):
+        apt_autoremove_packages()
+
+
+def test_apt_clean_system(monkeypatch):
+    _patch_shell_command(monkeypatch, 'clean successful\n', 0)
+    assert 'clean success' in apt_clean_system()
+
+
+def test_apt_clean_system_fails(monkeypatch):
+    _patch_shell_command(monkeypatch, 'clean failed\n', 255)
+    with pytest.raises(InstallationError):
+        apt_clean_system()
 
 
 def test_apt_install_packages(monkeypatch):
