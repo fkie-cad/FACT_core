a7e3b66f5e54f97a0dbbfda812fe4c567d28d017
Jörg Stucke <joerg.stucke@fkie.fraunhofer.de>
2018-08-24 14:07:38 +0200

    added config list helper function

--- src/test/unit/helperFunctions/test_config.py
+++ src/test/unit/helperFunctions/test_config.py
@@ -1,6 +1,9 @@
 import os
+
+import pytest
+
 from helperFunctions.fileSystem import get_test_data_dir
-from helperFunctions.config import get_config_dir, load_config
+from helperFunctions.config import get_config_dir, load_config, read_list_from_config
 
 
 def test_get_config_dir():
@@ -11,3 +14,19 @@ def test_load_config(monkeypatch):
     monkeypatch.setattr('helperFunctions.config.get_config_dir', lambda: '{}/helperFunctions'.format(get_test_data_dir()))
     test_config = load_config('test.cfg')
     assert test_config['test']['test'] == 'test_config', 'config not correct'
+
+
+@pytest.mark.parametrize('input_data, expected', [
+    ('', []),
+    ('item1', ['item1']),
+    ('item1, item2, item3', ['item1', 'item2', 'item3']),
+    ('item1,item2,item3', ['item1', 'item2', 'item3']),
+    (' item1 , item2 , item3 ', ['item1', 'item2', 'item3']),
+])
+def test_read_list_from_config(monkeypatch, input_data, expected):
+    monkeypatch.setattr('helperFunctions.config.get_config_dir', lambda: '{}/helperFunctions'.format(get_test_data_dir()))
+    test_config = load_config('test.cfg')
+    test_config.add_section('test_section')
+    test_config.set('test_section', 'test_option', input_data)
+    result = read_list_from_config(test_config, 'test_section', 'test_option')
+    assert result == expected
