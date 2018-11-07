f98c57b15d26bfad3953d92fcfb92a0873cb0cb2
dorpvom <dorp@cs.uni-bonn.de>
2018-11-07 12:53:25 +0100

    fixed issues with js parsing

--- src/plugins/analysis/linter/internal/js_linter.py
+++ src/plugins/analysis/linter/internal/js_linter.py
@@ -1,5 +1,4 @@
 import logging
-import re
 from pathlib import Path
 
 from common_helper_process import execute_shell_command
@@ -15,27 +14,29 @@ class JavaScriptLinter:
         linter_output = execute_shell_command('jshint --config={} --verbose {}'.format(CONFIG_FILE_PATH, file_path))
         return self._parse_linter_output(linter_output)
 
-    def _parse_linter_output(self, output):  # throws TypeError on bad line
+    def _parse_linter_output(self, linter_output):
         issues = []
-        for message in output.splitlines()[:-2]:
+        for issue in linter_output.splitlines()[:-2]:
             try:
-                remaining_line, issue_code = self._strip_file_path_and_extract_code(message)
+                remaining_line = self._strip_file_path(issue)
+                remaining_line, issue_code = self._extract_issue_code(remaining_line)
                 remaining_line, line_number, column = self._extract_line_and_column(remaining_line)
                 issues.append(dict(line=line_number, column=column, symbol=issue_code, message=remaining_line))
-            except IndexError:
-                logging.debug('js linter caused error on line: {}'.format(message))
+            except (IndexError, ValueError) as error:
+                logging.warning('jshint line was not correctly parsed: {}\n{}'.format(issue, str(error)))
 
-        return {'full': issues, 'summary': list(set(issue['symbol'] for issue in issues))}
+        return issues
 
     @staticmethod
-    def _strip_file_path_and_extract_code(line):
+    def _strip_file_path(line):
         colon_separated = line.split(':')
-        line_without_path = ':'.join(colon_separated[1:])
+        return ':'.join(colon_separated[1:])
 
-        split_by_message_end = line_without_path.split('. ')
-        line_without_path_and_code = '. '.join(split_by_message_end[:-1])
-
-        return line_without_path_and_code, split_by_message_end[-1].strip('() ')
+    @staticmethod
+    def _extract_issue_code(line):
+        bracket_separated = line.split('(')
+        code_part = bracket_separated[-1]
+        return '('.join(bracket_separated[:-1]), code_part.strip(' )')
 
     @staticmethod
     def _extract_line_and_column(line):
--- src/plugins/analysis/linter/test/test_js_linter.py
+++ src/plugins/analysis/linter/test/test_js_linter.py
@@ -2,24 +2,25 @@ import pytest
 
 from ..internal.js_linter import JavaScriptLinter
 
-MOCK_RESPONSE = '''/home/dorp/work/FACT_core/src/web_interface/static/Chart.js: line 10, col 14, Missing "use strict" statement. (E007)
-/home/dorp/work/FACT_core/src/web_interface/static/Chart.js: line 10, col 91, Missing semicolon. (W033)
-/home/dorp/work/FACT_core/src/web_interface/static/Chart.js: line 10, col 443, Expected '===' and instead saw '=='. (W116)
-/home/dorp/work/FACT_core/src/web_interface/static/Chart.js: line 10, col 565, Missing semicolon. (E058)
-/home/dorp/work/FACT_core/src/web_interface/static/Chart.js: line 10, col 565, Expected an identifier and instead saw '='. (E030)
-/home/dorp/work/FACT_core/src/web_interface/static/Chart.js: line 10, col 565, Expected an assignment or function call and instead saw an expression. (W030)
-/home/dorp/work/FACT_core/src/web_interface/static/Chart.js: line 10, col 725, 'i' was used before it was defined. (W003)
-/home/dorp/work/FACT_core/src/web_interface/static/Chart.js: line 10, col 837, 'exports' is defined but never used. (W098)
-/home/dorp/work/FACT_core/src/web_interface/static/Chart.js: line 54, col 38, Expected a conditional expression and instead saw an assignment. (W084)
-/home/dorp/work/FACT_core/src/web_interface/static/Chart.js: line 56, col 16, 'i' is already defined. (W004)
-/home/dorp/work/FACT_core/src/web_interface/static/Chart.js: line 153, col 15, Misleading line break before '+'; readers may interpret this as an expression boundary. (W014)
-/home/dorp/work/FACT_core/src/web_interface/static/Chart.js: line 231, col 1, The body of a for in should be wrapped in an if statement to filter unwanted properties from the prototype. (W089)
-/home/dorp/work/FACT_core/src/web_interface/static/Chart.js: line 414, col 24, Unexpected use of '<<'. (W016)
-/home/dorp/work/FACT_core/src/web_interface/static/Chart.js: line 1429, col 29, Functions declared within loops referencing an outer scoped variable may lead to confusing semantics. (conversions) (W083)
-/home/dorp/work/FACT_core/src/web_interface/static/Chart.js: line 4739, col 37, Empty block. (W035)
-/home/dorp/work/FACT_core/src/web_interface/static/Chart.js: line 10, col 128, 'define' is not defined. (W117)
+MOCK_RESPONSE = '''/any/path: line 1, col 29, Any Message. (conversions) (W083)
+/any/path: line 10, col 14, Missing "use strict" statement. (E007)
+/any/path: line 10, col 91, Missing semicolon. (W033)
+/any/path: line 10, col 128, 'define' is not defined. (W117)
+/any/path: line 10, col 443, Expected '===' and instead saw '=='. (W116)
+/any/path: line 10, col 565, Missing semicolon. (E058)
+/any/path: line 10, col 565, Expected an identifier and instead saw '='. (E030)
+/any/path: line 10, col 565, Expected an assignment or function call and instead saw an expression. (W030)
+/any/path: line 10, col 725, 'i' was used before it was defined. (W003)
+/any/path: line 10, col 837, 'exports' is defined but never used. (W098)
+/any/path: line 54, col 38, Expected a conditional expression and instead saw an assignment. (W084)
+/any/path: line 56, col 16, 'i' is already defined. (W004)
+/any/path: line 153, col 15, Misleading line break before '+'; readers may interpret this as an expression boundary. (W014)
+/any/path: line 231, col 1, The body of a for in should be wrapped in an if statement to filter unwanted properties from the prototype. (W089)
+/any/path: line 414, col 24, Unexpected use of '<<'. (W016)
+/any/path/: line 752, col 94, This line contains stuff: http://weblink/ (W125)
+/any/path: line 4739, col 37, Empty block. (W035)
 
-16 errors
+17 errors
 '''
 
 
@@ -31,11 +32,11 @@ def stub_linter():
 def test_do_analysis(stub_linter, monkeypatch):
     monkeypatch.setattr('plugins.analysis.linter.internal.js_linter.execute_shell_command', lambda command: MOCK_RESPONSE)
     result = stub_linter.do_analysis('any/path')
-    assert 'full' in result
-    assert len(result['full']) == 16
-    assert result['full'][0] == {
-        'message': 'Missing "use strict" statement',
-        'line': 10,
-        'column': 14,
-        'symbol': 'E007'
+    assert result
+    assert len(result) == 17
+    assert result[0] == {
+        'message': 'Any Message. (conversions)',
+        'line': 1,
+        'column': 29,
+        'symbol': 'W083'
     }
