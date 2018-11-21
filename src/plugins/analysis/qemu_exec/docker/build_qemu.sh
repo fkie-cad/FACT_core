0b4b5ac74ee33363070b5a38dd2dd671b796b704
Jörg Stucke <joerg.stucke@fkie.fraunhofer.de>
2018-11-21 10:48:47 +0100

    updated version of docker base image and qemu

--- src/plugins/analysis/qemu_exec/docker/build_qemu.sh
+++ src/plugins/analysis/qemu_exec/docker/build_qemu.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
+
+QEMU_VERSION=qemu-3.0.0
+
 cd /opt
 mkdir /opt/qemu-targets
-wget https://download.qemu.org/qemu-2.11.1.tar.xz
-tar -xvf qemu-2.11.1.tar.xz
-cd qemu-2.11.1
+wget https://download.qemu.org/${QEMU_VERSION}.tar.xz
+tar -xvf ${QEMU_VERSION}.tar.xz
+rm ${QEMU_VERSION}.tar.xz
+cd ${QEMU_VERSION}
 
 # possible targets: aarch64 alpha arm armeb cris i386 m68k microblaze microblazeel mips mips64 mips64el mipsel mipsn32 mipsn32el or32 ppc ppc64 ppc64abi32 ppc64le s390x sh4 sh4eb sparc sparc32plus sparc64 unicore32 x86_64
 CPU_TARGETS="aarch64 alpha arm armeb i386 m68k mips mips64 mips64el mipsel mipsn32 mipsn32el ppc ppc64 ppc64abi32 ppc64le s390x sh4 sh4eb sparc sparc32plus sparc64 x86_64"
