556f5f84591ce21f401023007a909a9ef49feb02
Peter Weidenbach <peter.weidenbach@fkie.fraunhofer.de>
2018-11-23 12:06:00 +0100

    minor bugfix in cwe checker

--- src/plugins/analysis/cwe_checker/install.sh
+++ src/plugins/analysis/cwe_checker/install.sh
@@ -8,6 +8,7 @@ echo " Installing cwe_checker Plugin "
 echo "------------------------------------"
 
 echo "Checking out cwe_checker"
+rm -fR internal
 git clone https://github.com/fkie-cad/cwe_checker.git internal
 
 echo "Cleaning up"
@@ -15,10 +16,7 @@ rm -rf internal/src/_build
 rm -f internal/src/cwe_checker.plugin 
 
 echo "Building docker container"
-cd internal && docker build --build-arg http_proxy=$http_proxy --build-arg https_proxy=$https_proxy --build-arg HTTP_PROXY=$http_proxy --build-arg HTTPS_PROXY=$https_proxy -t cwe-checker .
-
-# change cwd to current file's directory
-cd "$( dirname "${BASH_SOURCE[0]}" )" 
+(cd internal && docker build --build-arg http_proxy=$http_proxy --build-arg https_proxy=$https_proxy --build-arg HTTP_PROXY=$http_proxy --build-arg HTTPS_PROXY=$https_proxy -t cwe-checker .)
 
 echo "Installing Python dependencies."
 sudo -EH pip3 install sexpdata
