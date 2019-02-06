bcf227d7f7c553f33b193dbe0074e83dc7a46bca
dorpvom <dorp@cs.uni-bonn.de>
2019-02-06 10:31:09 +0100

    added installation

---
+++ src/plugins/unpacking/generic_fs/install.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+cd "$( dirname "${BASH_SOURCE[0]}" )"
+
+echo "------------------------------------"
+echo "           install kpartx           "
+echo "------------------------------------"
+
+sudo apt-get install -y kpartx
+
+exit 0
