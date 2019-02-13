ad2b7545a82a3fe0fa16da2344afd3bf143817fc
Jörg Stucke <joerg.stucke@fkie.fraunhofer.de>
2019-02-13 09:48:37 +0100

    refactoring test

--- src/plugins/unpacking/hp/test/test_pjl_helper.py
+++ src/plugins/unpacking/hp/test/test_pjl_helper.py
@@ -11,7 +11,12 @@ from ..internal.pjl_helper import (
     _is_upgrade, get_pjl_commands
 )
 
-TEST_DATA_UPGRADE_RAW = b'\x40\x50\x4A\x4C\x20\x55\x50\x47\x52\x41\x44\x45\x20\x53\x49\x5A\x45\x3D\x31\x31\x32\x0D\x0A\x03\x00\xA8\x01\x48\x50\x20\x43\x6F\x6C\x6F\x72\x20\x4C\x61\x73\x65\x72\x4A\x65\x74\x20\x43\x50\x34\x35\x32\x35\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x30\x37\x32\x32\x30\x32\x20\x20\x04\x53\xB1\x21\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x54\x65\x73\x74\x20\x46\x69\x6C\x65\x20\x31\x0D\x0A'
+TEST_DATA_UPGRADE_RAW = (b'\x40\x50\x4A\x4C\x20\x55\x50\x47\x52\x41\x44\x45\x20\x53\x49\x5A\x45\x3D\x31\x31\x32\x0D\x0A'
+                         b'\x03\x00\xA8\x01\x48\x50\x20\x43\x6F\x6C\x6F\x72\x20\x4C\x61\x73\x65\x72\x4A\x65\x74\x20\x43'
+                         b'\x50\x34\x35\x32\x35\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20'
+                         b'\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x30'
+                         b'\x37\x32\x32\x30\x32\x20\x20\x04\x53\xB1\x21\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00'
+                         b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x54\x65\x73\x74\x20\x46\x69\x6C\x65\x20\x31\x0D\x0A')
 TEST_COMMAND = {'raw': b'@PJL UPGRADE SIZE=112', 'begin_offset': 0, 'end_offset': 21, 'type': b'UPGRADE', 'value': b'SIZE=112'}
 
 
@@ -58,21 +63,20 @@ def test_get_binary_of_upgrade():
 
 @pytest.mark.parametrize('input_data, fingerprint', [
     (b'no finger print', None),
-    (b'RANDOM_DATA--=</Begin HP Signed File Fingerprint\\>=-- The Fingerprint --=</End HP Signed File Fingerprint\\>=--SOME_MORE_RANDOM_DATA', b'--=</Begin HP Signed File Fingerprint\\>=-- The Fingerprint --=</End HP Signed File Fingerprint\\>=--')
+    (b'RANDOM_DATA--=</Begin HP Signed File Fingerprint\\>=-- The Fingerprint --=</End HP Signed File Fingerprint\\>=--SOME_MORE_RANDOM_DATA',
+     b'--=</Begin HP Signed File Fingerprint\\>=-- The Fingerprint --=</End HP Signed File Fingerprint\\>=--')
 ])
 def test_get_file_fingerprint(input_data, fingerprint):
     assert _get_file_fingerprint(input_data) == fingerprint
 
 
 def test_extract_file_from_upgrade():
-    tmp_dir = TemporaryDirectory(prefix='fact_test_')
-    expected_path_of_dumped_file = Path(tmp_dir.name, 'HP_Color_LaserJet_CP4525.bin')
+    with TemporaryDirectory(prefix='fact_test_') as tmp_dir:
+        expected_path_of_dumped_file = Path(tmp_dir, 'HP_Color_LaserJet_CP4525.bin')
 
-    _extract_file_from_upgrade(TEST_DATA_UPGRADE_RAW, TEST_COMMAND, tmp_dir.name)
+        _extract_file_from_upgrade(TEST_DATA_UPGRADE_RAW, TEST_COMMAND, tmp_dir)
 
-    assert expected_path_of_dumped_file.exists()
+        assert expected_path_of_dumped_file.exists()
 
-    dumped_file_binary = get_binary_from_file(str(expected_path_of_dumped_file))
-    assert dumped_file_binary == b'\x00Test File 1'
-
-    tmp_dir.cleanup()
+        dumped_file_binary = get_binary_from_file(str(expected_path_of_dumped_file))
+        assert dumped_file_binary == b'\x00Test File 1'
