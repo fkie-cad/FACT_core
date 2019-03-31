5e388a9db62490c8549feb78c9874d281942681d
Max <>
2019-03-31 13:22:31 +0200

    connection and query working

--- src/plugins/analysis/deepsearch/code/db.py
+++ src/plugins/analysis/deepsearch/code/db.py
@@ -4,13 +4,32 @@ import pymongo
 class PPyMongoDB:
 
     def __init__(self, url, port, user, password):
-        self.connection = pymongo.MongoClient( \ 
-            "mongodb://" + user + ":" + password \
-            + "@" + url + ":" + port)
+        self.connection = pymongo.MongoClient( 
+            "mongodb://" + user + ":" + password 
+            + "@" + url + ":" + port + "/?authSource=admin&authMechanism=SCRAM-SHA-1")
+
+        print(self.connection.list_database_names())
+
+        self.admin_db = self.connection["fact_main"]
+
+        self.collection = self.admin_db["file_objects"]
+
+        print(self.collection.find({"processed_analysis.file_type.mime": "application/x-executable"}).count())
+             
         
+        
+            
+        
+
+        # print(self.admin_db)    
         return
 
 
 
 if __name__ == "__main__":
-    pass
+    client = PPyMongoDB("localhost", "27018",
+                        "fact_readonly", "RFaoFSr8b6BMSbzt" )
+        
+    
+    
+    pass      
