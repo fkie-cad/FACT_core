78f6e4335c1ae7e0a2ba115abeddf317000078fc
Max <>
2019-04-12 21:09:55 +0200

    update

--- src/plugins/analysis/deepsearch/code/deepsearch.py
+++ src/plugins/analysis/deepsearch/code/deepsearch.py
@@ -1,6 +1,9 @@
 from analysis.PluginBase import AnalysisBasePlugin
 
-import pymongo
+from helperFunctions.web_interface import ConnectTo
+
+from storage.db_interface_common import MongoInterfaceCommon
+
 
 class AnalysisPlugin(AnalysisBasePlugin):
     '''
@@ -24,16 +27,12 @@ class AnalysisPlugin(AnalysisBasePlugin):
 
     def process_object(self, file_object):
 
+        #result_object = file_object
+        #result_strings = file_object.processed_analysis['printable_strings']
+        #result_file_type = file_object.processed_analysis['file_type']
 
-        #db.getCollection('file_objects').find({ "processed_analysis.file_type.mime": "application/x-executable"  })
-
-
-
-        result_object = file_object
-        result_strings = file_object.processed_analysis['printable_strings']
-        result_file_type = file_object.processed_analysis['file_type']
-
-        file_object.processed_analysis[self.NAME] = dict()
+        #gives runtime error
+        #file_object.processed_analysis[self.NAME] = dict()
 
         #file_object.processed_analysis[self.NAME]['object'] = str(result_object)
         #file_object.processed_analysis[self.NAME]['file_type'] = str(result_file_type)
@@ -45,59 +44,60 @@ class AnalysisPlugin(AnalysisBasePlugin):
         #file_object.processed_analysis[self.NAME]['analysis_result_b'] = result_b
         #file_object.processed_analysis[self.NAME]['summary'] = ['{} - {}'.format(result_a, result_b)]
 
-        user_ro = "fact_readonly"
-        password_ro = "RFaoFSr8b6BMSbzt"
-        user_write = 'fact_admin'
-        password_write = '6fJEb5LkV2hRtWq0'
-        url = "localhost"
-        port = "27018"
+        '''
+       for ele in collection.find():
 
-        connection = pymongo.MongoClient(
-            "mongodb://" + user_write + ":" + password_write
-            + "@" + url + ":" + port + "/?authSource=admin&authMechanism=SCRAM-SHA-1")
+           if firmware_id in ele["parent_firmware_uids"]:
+               tmp = dict()
+               path = ele["virtual_file_path"]
+               path_str = list(path.values())[0]
+               path_str = path_str[0]
+               path_str = path_str[path_str.index("/"):]
+               #print(path_str)
 
-        admin_db = connection["fact_main"]
+               tmp["path"] = path_str
+               tmp["name"] = ele["file_name"]
 
-        collection = admin_db["file_objects"]
+               executables.append(tmp)
+
+           else:
+               continue
+        '''
+
+        analysisdictionary = {}
+
         fileid = file_object.uid
-
         firmware_id = ""
-        for element in collection.find({"_id": fileid}):
-            firmware_id = element["parent_firmware_uids"][0]
 
+        with ConnectTo(BinaryInterface, self.config) as interface:
+            for element in interface.firmwareIDofFileID(fileid):
+                firmware_id = element['parent_firmware_uids'][0]
 
-        executables = list()
+            analysisdictionary['contained_binaries'] = list()
 
-        for ele in collection.find({"processed_analysis.file_type.mime": {"$regex": "executable"}}):
+            for printstring in file_object.processed_analysis['printable_strings']['offsets']:
+                for executablefile in interface.executableFiles(firmware_id):
+                    if executablefile['parent_firmware_uids'] == firmware_id:
+                        if executablefile['file_name'] in printstring:
+                            #file_object.processed_analysis[self.NAME]['contained_binaries'].append(executablefile)
+                            analysisdictionary['contained_binaries'].append(executablefile)
 
-            if firmware_id in ele["parent_firmware_uids"]:
-                tmp = dict()
-                path = ele["virtual_file_path"]
-                path_str = list(path.values())[0]
-                path_str = path_str[0]
-                path_str = path_str[path_str.index("/"):]
-                #print(path_str)
-
-                tmp["path"] = path_str
-                tmp["name"] = ele["file_name"]
-
-                executables.append(tmp)
-
-            else:
-                continue
-
-        #db.getCollection('file_objects').find({"processed_analysis.deepsearch.contained_binaries": {"$gt": 1} })
-        file_object.processed_analysis[self.NAME]['contained_binaries'] = list()
-
-        for printstring in file_object.processed_analysis['printable_strings']["offsets"]:
-
-            for i in executables:
-
-                if i["name"] in printstring:
-                    file_object.processed_analysis[self.NAME]['contained_binaries'].append(i)
                     pass
 
-            pass
+            file_object.processed_analysis[self.NAME] = analysisdictionary
+
+
 
 
         return file_object
+
+
+class BinaryInterface(MongoInterfaceCommon):
+
+    READ_ONLY = True
+
+    def firmwareIDofFileID(self, file_id):
+        return self.file_objects.find({"processed_analysis._uid": file_id})
+
+    def executableFiles(self, firmware_id):
+        return self.file_objects.find({"processed_analysis.file_type.mime": {"$regex": "executable"}, "parent_firmware_uids": firmware_id})
