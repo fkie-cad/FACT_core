747dc7700911c083536307de51e312357636c9aa
dorpvom <dorp@cs.uni-bonn.de>
2019-06-24 10:36:20 +0200

    added empty context for query of empty database

--- src/plugins/analysis/tlsh/test/test_plugin_tlsh.py
+++ src/plugins/analysis/tlsh/test/test_plugin_tlsh.py
@@ -1,11 +1,12 @@
 import pytest
-
 from helperFunctions.config import get_config_for_testing
 from plugins.analysis.tlsh.code.tlsh import AnalysisPlugin
 from test.common_helper import create_test_file_object
 
 HASH_0 = '9A355C07B5A614FDC5A2847046EF92B7693174A642327DBF3C88D6303F42E746B1ABE1'
 HASH_1 = '0CC34B06B1B258BCC16689308A67D671AB747E5053223B3E3684F7342F56E6F1F0DAB1'
+
+# pylint: disable=redefined-outer-name
 
 
 class MockAdmin:
@@ -19,13 +20,22 @@ class MockContext:
 
     def __enter__(self):
         class ControlledInterface:
-            def tlsh_query_all_objects(self):
+            def tlsh_query_all_objects(self):  # pylint: disable=no-self-use
                 return [{'processed_analysis': {'file_hashes': {'tlsh': HASH_1}}, '_id': '5'}, ]
 
         return ControlledInterface()
 
     def __exit__(self, *args):
         pass
+
+
+class EmptyContext(MockContext):
+    def __enter__(self):
+        class EmptyInterface:
+            def tlsh_query_all_objects(self):  # pylint: disable=no-self-use
+                return []
+
+        return EmptyInterface()
 
 
 @pytest.fixture(scope='function')
@@ -55,8 +65,8 @@ def test_one_matching_file(stub_plugin, test_object, monkeypatch):
 
 def test_no_matching_file(test_object, stub_plugin, monkeypatch):
     monkeypatch.setattr('plugins.analysis.tlsh.code.tlsh.ConnectTo', MockContext)
-    NOT_MATCHING_HASH = '0CC34689821658B06B1B258BCC16689308A671AB3223B3E3684F8d695A658742F0DAB1'
-    test_object.processed_analysis['file_hashes'] = {'tlsh': NOT_MATCHING_HASH}
+    not_matching_hash = '0CC34689821658B06B1B258BCC16689308A671AB3223B3E3684F8d695A658742F0DAB1'
+    test_object.processed_analysis['file_hashes'] = {'tlsh': not_matching_hash}
     result = stub_plugin.process_object(test_object)
 
     assert result.processed_analysis[stub_plugin.NAME] == {}
@@ -78,17 +88,11 @@ def test_file_has_no_tlsh_hash(test_object, stub_plugin, monkeypatch):
     assert result.processed_analysis[stub_plugin.NAME] == {}
 
 
-def test_no_files_in_database():
-    pass
+def test_no_files_in_database(monkeypatch):
+    monkeypatch.setattr('plugins.analysis.tlsh.code.tlsh.ConnectTo', EmptyContext)
 
 
 def test_file_hashes_not_run(test_object, stub_plugin):
     with pytest.raises(KeyError):
         test_object.processed_analysis.pop('file_hashes')
         stub_plugin.process_object(test_object)
-
-
-'''
-if value < 150 expected {id, tlsh_value}
-else expected { }
-'''
