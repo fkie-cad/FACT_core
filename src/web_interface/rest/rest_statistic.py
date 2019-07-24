55b219920f0079a96254c23657f5a7631714df7e
dorpvom <dorp@cs.uni-bonn.de>
2019-07-24 10:35:35 +0200

    restructured plugin information

--- src/web_interface/rest/rest_statistic.py
+++ src/web_interface/rest/rest_statistic.py
@@ -1,6 +1,6 @@
 from flask_restful import Resource
 
-from helperFunctions.rest import success_message, error_message
+from helperFunctions.rest import error_message, success_message
 from helperFunctions.web_interface import ConnectTo
 from intercom.front_end_binding import InterComFrontEndBinding
 from storage.db_interface_statistic import StatisticDbViewer
@@ -23,13 +23,23 @@ class RestStatus(Resource):
                 status[component] = stats_db.get_statistic(component)
 
         with ConnectTo(InterComFrontEndBinding, self.config) as sc:
-            plugin_dict = sc.get_available_analysis_plugins()
+            plugins = sc.get_available_analysis_plugins()
 
         if not status:
             return error_message('Unknown Issue. Cannot Stat FACT.', self.URL, return_code=404)
 
         response = {
             'system_status': status,
-            'plugins': plugin_dict,
+            'plugins': self._condense_plugin_information(plugins),
         }
         return success_message(response, self.URL)
+
+    @staticmethod
+    def _condense_plugin_information(plugins):
+        plugins.pop('unpacker', None)
+
+        for name, information in plugins.items():
+            description, _, _, version = information
+            plugins[name] = dict(description=description, version=version)
+
+        return plugins
