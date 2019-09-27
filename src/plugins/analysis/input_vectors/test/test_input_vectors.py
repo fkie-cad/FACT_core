cd5a76e1cbfef90bf3198de882725466186e7d3a
Jörg Stucke <joerg.stucke@fkie.fraunhofer.de>
2019-09-27 11:13:46 +0200

    added tests

--- src/plugins/analysis/input_vectors/test/test_input_vectors.py
+++ src/plugins/analysis/input_vectors/test/test_input_vectors.py
@@ -1,7 +1,11 @@
-from test.unit.analysis.analysis_plugin_test_class import AnalysisPluginTest
+from pathlib import Path
+
 from objects.file import FileObject
+from test.unit.analysis.analysis_plugin_test_class import AnalysisPluginTest
 
 from ..code.input_vectors import AnalysisPlugin
+
+TEST_FILE_DIR = Path(__file__).parent / "data"
 
 
 class AnalysisPluginTestInputVectors(AnalysisPluginTest):
@@ -13,5 +17,19 @@ class AnalysisPluginTestInputVectors(AnalysisPluginTest):
         config = self.init_basic_config()
         self.analysis_plugin = AnalysisPlugin(self, config=config)
 
-    def tearDown(self):
-        super().tearDown()
+    def test_process_object_inputs(self):
+        result = self.assert_process_object("test_fgets.elf")
+        assert result["full"]["inputs"][0]["name"] == "fgets"
+
+    def test_process_object_domains(self):
+        result = self.assert_process_object("test_domain.elf")
+        assert result["full"]["domains"][0] == "http://foo.bar"
+
+    def assert_process_object(self, test_file_name: str) -> dict:
+        test_file = TEST_FILE_DIR / test_file_name
+        assert test_file.is_file(), "test file is missing"
+        fo = FileObject(file_path=str(test_file))
+        result = self.analysis_plugin.process_object(fo)
+        assert "input_vectors" in result.processed_analysis
+        assert result.processed_analysis["input_vectors"]["full"]
+        return result.processed_analysis["input_vectors"]
