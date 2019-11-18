d21b9ecb474f194b588ebbd0f1a5b949676eb052
Jörg Stucke <joerg.stucke@fkie.fraunhofer.de>
2019-11-18 15:58:30 +0100

    refactoring

--- src/test/unit/helperFunctions/test_merge_generators.py
+++ src/test/unit/helperFunctions/test_merge_generators.py
@@ -1,10 +1,8 @@
-import unittest
-
 from helperFunctions import merge_generators
 from helperFunctions.merge_generators import sum_up_lists
 
 
-class TestHelperFunctionsMergeGenerators(unittest.TestCase):
+class TestHelperFunctionsMergeGenerators:  # pylint: disable=no-self-use
 
     def test_merge_generators(self):
         generator_a = range(3, 10)
@@ -12,15 +10,15 @@ class TestHelperFunctionsMergeGenerators(unittest.TestCase):
         test_generated_list = list(generator_a) + list(generator_b)
         generator = merge_generators.merge_generators(generator_a, generator_b)
         generated_list = list(generator)
-        self.assertEqual(len(test_generated_list), len(generated_list))
-        self.assertTrue(all([item in generated_list for item in test_generated_list]))
-        self.assertTrue(all([item in test_generated_list for item in generator]))
+        assert len(test_generated_list) == len(generated_list)
+        assert all([item in generated_list for item in test_generated_list])
+        assert all([item in test_generated_list for item in generator])
 
     def test_sum_up_lists(self):
-        a = [['a', 1], ['b', 5]]
-        b = [['c', 3], ['b', 1]]
-        result = sum_up_lists(a, b)
-        self.assertEqual(len(result), 3, "number of entries not correct")
-        self.assertIn(['a', 1], result)
-        self.assertIn(['b', 6], result)
-        self.assertIn(['c', 3], result)
+        list_a = [['a', 1], ['b', 5]]
+        list_b = [['c', 3], ['b', 1]]
+        result = sum_up_lists(list_a, list_b)
+        assert len(result) == 3, 'number of entries not correct'
+        assert ['a', 1] in result
+        assert ['b', 6] in result
+        assert ['c', 3] in result
