648dab8ac903f5415f9df5d42c2fff05217a52c3
Jörg Stucke <joerg.stucke@fkie.fraunhofer.de>
2020-02-27 10:22:42 +0100

    moved docker call to helper function

--- src/plugins/analysis/software_components/internal/resolve_version_format_string.py
+++ src/plugins/analysis/software_components/internal/resolve_version_format_string.py
@@ -1,15 +1,11 @@
 import json
 import logging
 import re
-from contextlib import suppress
 from pathlib import Path
 from tempfile import TemporaryDirectory
 from typing import List
 
-import docker
-from docker.errors import DockerException
-from docker.types import Mount
-from requests.exceptions import ReadTimeout
+from helperFunctions.docker import run_docker_container
 
 CONTAINER_TARGET_PATH = '/work'
 DOCKER_IMAGE = 'fact/format_string_resolver'
@@ -24,7 +20,7 @@ def extract_data_from_ghidra(input_file_data: bytes, key_strings: List[str]) -> 
         ghidra_input_file = tmp_dir_path / 'ghidra_input'
         (tmp_dir_path / KEY_FILE).write_text(json.dumps(key_strings))
         ghidra_input_file.write_bytes(input_file_data)
-        docker_output = run_docker_container(tmp_dir)
+        docker_output = run_docker_container(DOCKER_IMAGE, TIMEOUT, mount=(CONTAINER_TARGET_PATH, tmp_dir), label='FSR')
         logging.debug(docker_output)
         try:
             output_file = (tmp_dir_path / DOCKER_OUTPUT_FILE).read_bytes()
@@ -36,22 +32,3 @@ def extract_data_from_ghidra(input_file_data: bytes, key_strings: List[str]) -> 
 
 def filter_implausible_results(version_list: List[str]):
     return [version for version in version_list if re.search(r'\d\.\d', version)]
-
-
-def run_docker_container(dir_path: str) -> str:
-    container = None
-    try:
-        volume = Mount(CONTAINER_TARGET_PATH, dir_path, read_only=False, type='bind')
-        client = docker.from_env()
-        container = client.containers.run(DOCKER_IMAGE, mounts=[volume], network_disabled=True, detach=True)
-        container.wait(timeout=TIMEOUT)
-        return container.logs().decode()
-    except ReadTimeout:
-        logging.warning('[FSR]: timeout while processing')
-    except (DockerException, IOError):
-        logging.warning('[FSR]: encountered process error while processing')
-    finally:
-        if container:
-            with suppress(DockerException):
-                container.stop()
-            container.remove()
