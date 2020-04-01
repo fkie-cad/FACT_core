4f96a65921af2b7897dbc0bea5ae0a670125a989
dorp <dorp@cs.uni-bonn.de>
2020-04-01 09:06:36 +0200

    added parameters to rest responses and tested it (+pylint)

--- src/test/unit/helperFunctions/test_object_conversion.py
+++ src/test/unit/helperFunctions/test_object_conversion.py
@@ -1,28 +1,34 @@
-import unittest
-from test.common_helper import create_test_firmware, create_test_file_object
 from helperFunctions.object_conversion import create_meta_dict
+from test.common_helper import create_test_file_object, create_test_firmware
 
 
-class TestHelperFunctionsObjectConversion(unittest.TestCase):
+def test_create_meta_dict_fw():
+    fw = create_test_firmware()
+    meta = create_meta_dict(fw)
 
-    def test_create_meta_dict_fw(self):
-        fw = create_test_firmware()
-        meta = create_meta_dict(fw)
-        self.assertEqual(meta['device_name'], 'test_router')
-        self.assertEqual(meta['device_class'], 'Router')
-        self.assertEqual(meta['vendor'], 'test_vendor')
-        self.assertEqual(meta['device_part'], '')
-        self.assertEqual(meta['version'], '0.1')
-        self.assertEqual(meta['release_date'], '1970-01-01')
-        self.assertEqual(meta['hid'], 'test_vendor test_router v. 0.1')
-        self.assertEqual(meta['size'], 787)
-        self.assertEqual(len(meta.keys()), 8)
+    assert meta['device_name'] == 'test_router'
+    assert meta['device_class'] == 'Router'
+    assert meta['vendor'] == 'test_vendor'
+    assert meta['device_part'] == ''
+    assert meta['version'] == '0.1'
+    assert meta['release_date'] == '1970-01-01'
+    assert meta['hid'] == 'test_vendor test_router v. 0.1'
+    assert meta['size'] == 787
 
-    def test_create_meta_dict_fo(self):
-        fo = create_test_file_object()
-        fo.list_of_all_included_files = []
-        meta = create_meta_dict(fo)
-        self.assertEqual(meta['firmwares_including_this_file'], ['d558c9339cb967341d701e3184f863d3928973fccdc1d96042583730b5c7b76a_62'])
-        self.assertEqual(meta['virtual_file_path'], ['d558c9339cb967341d701e3184f863d3928973fccdc1d96042583730b5c7b76a_62'])
-        self.assertEqual(meta['number_of_files'], 0)
-        self.assertEqual(len(meta.keys()), 5)
+    assert meta['number_of_included_files'] == 0
+    assert meta['included_files'] == []
+    assert meta['total_files_in_firmware'] == 'unknown'
+
+    assert len(meta.keys()) == 11
+
+
+def test_create_meta_dict_fo():
+    fo = create_test_file_object()
+    meta = create_meta_dict(fo)
+
+    assert meta['firmwares_including_this_file'] == ['d558c9339cb967341d701e3184f863d3928973fccdc1d96042583730b5c7b76a_62']
+    assert meta['virtual_file_path'] == ['d558c9339cb967341d701e3184f863d3928973fccdc1d96042583730b5c7b76a_62']
+
+    assert meta['number_of_included_files'] == 0
+
+    assert len(meta.keys()) == 7
