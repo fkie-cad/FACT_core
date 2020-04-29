48c36540252dd34f7ab86c6760181638271940e3
Jörg Stucke <joerg.stucke@fkie.fraunhofer.de>
2020-04-29 12:38:01 +0200

    added error passing out of plugin install scripts

--- src/plugins/analysis/linter/install.sh
+++ src/plugins/analysis/linter/install.sh
@@ -1,25 +1,22 @@
 #!/usr/bin/env bash
 
 # change cwd to current file's directory
-cd "$( dirname "${BASH_SOURCE[0]}" )" 
+cd "$( dirname "${BASH_SOURCE[0]}" )"  || exit 1
 
 echo "Installing shell linter..."
-sudo apt-get install -y shellcheck
+sudo apt-get install -y shellcheck || exit 1
 
-echo " Installing lua linter ..."
-sudo apt-get install -y luarocks
-sudo luarocks install luafilesystem
-sudo luarocks install argparse
-sudo luarocks install luacheck
+echo " Installing lua linter..."
+sudo apt-get install -y luarocks || exit 1
+sudo luarocks install luafilesystem || exit 1
+sudo luarocks install argparse || exit 1
+sudo luarocks install luacheck || exit 1
 
 echo " Installing python linter..."
-sudo -EH pip3 install --upgrade pylint
+sudo -EH pip3 install --upgrade pylint || exit 1
 
 echo " Installing javascript linter..."
-sudo apt-get install -y libssl1.0-dev
-sudo apt-get install -y nodejs-dev
-sudo apt-get install -y node-gyp
-sudo apt-get install -y npm
-sudo npm install -g jshint
+sudo apt-get install -y libssl1.0-dev nodejs-dev node-gyp npm || exit 1
+sudo npm install -g jshint || exit 1
 
 exit 0
