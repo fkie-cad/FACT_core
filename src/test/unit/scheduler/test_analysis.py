ccafb8336cfd7707d20275b394e310bf36318899
Jörg Stucke <joerg.stucke@fkie.fraunhofer.de>
2020-05-15 13:26:37 +0200

    sped up scheduler test

--- src/test/unit/scheduler/test_analysis.py
+++ src/test/unit/scheduler/test_analysis.py
@@ -2,7 +2,7 @@
 import gc
 import os
 from multiprocessing import Queue
-from unittest import TestCase, mock
+from unittest import mock
 
 import pytest
 
@@ -13,9 +13,9 @@ from test.common_helper import DatabaseMock, MockFileObject, fake_exit, get_conf
 from test.mock import mock_patch, mock_spy
 
 
-class AnalysisSchedulerTest(TestCase):
+class AnalysisSchedulerTest:
 
-    def setUp(self):
+    def setup(self):
         self.mocked_interface = DatabaseMock()
         self.enter_patch = mock.patch(target='helperFunctions.database.ConnectTo.__enter__', new=lambda _: self.mocked_interface)
         self.enter_patch.start()
@@ -29,7 +29,7 @@ class AnalysisSchedulerTest(TestCase):
         self.tmp_queue = Queue()
         self.sched = AnalysisScheduler(config=config, pre_analysis=lambda *_: None, post_analysis=self.dummy_callback, db_interface=self.mocked_interface)
 
-    def tearDown(self):
+    def teardown(self):
         self.sched.shutdown()
         self.tmp_queue.close()
 
@@ -43,19 +43,8 @@ class AnalysisSchedulerTest(TestCase):
 
 
 class TestScheduleInitialAnalysis(AnalysisSchedulerTest):
-
     def test_plugin_registration(self):
-        self.assertIn('dummy_plugin_for_testing_only', self.sched.analysis_plugins, 'Dummy plugin not found')
-
-    def test_schedule_firmware_init_no_analysis_selected(self):
-        self.sched.shutdown()
-        self.sched.process_queue = Queue()
-        test_fw = Firmware(binary=b'test')
-        self.sched.start_analysis_of_object(test_fw)
-        test_fw = self.sched.process_queue.get(timeout=5)
-        self.assertEqual(len(test_fw.scheduled_analysis), len(MANDATORY_PLUGINS), 'Mandatory Plugins not selected')
-        for item in MANDATORY_PLUGINS:
-            self.assertIn(item, test_fw.scheduled_analysis)
+        assert 'dummy_plugin_for_testing_only' in self.sched.analysis_plugins, 'Dummy plugin not found'
 
     def test_whole_run_analysis_selected(self):
         test_fw = Firmware(file_path=os.path.join(get_test_data_dir(), 'get_files_test/testfile1'))
@@ -63,45 +52,11 @@ class TestScheduleInitialAnalysis(AnalysisSchedulerTest):
         self.sched.start_analysis_of_object(test_fw)
         for _ in range(3):  # 3 plugins have to run
             test_fw = self.tmp_queue.get(timeout=10)
-        self.assertEqual(len(test_fw.processed_analysis), 3, 'analysis not done')
-        self.assertEqual(test_fw.processed_analysis['dummy_plugin_for_testing_only']['1'], 'first result', 'result not correct')
-        self.assertEqual(test_fw.processed_analysis['dummy_plugin_for_testing_only']['summary'], ['first result', 'second result'])
-        self.assertIn('file_hashes', test_fw.processed_analysis.keys(), 'Mandatory plug-in not executed')
-        self.assertIn('file_type', test_fw.processed_analysis.keys(), 'Mandatory plug-in not executed')
-
-    def test_expected_plugins_are_found(self):
-        result = self.sched.get_plugin_dict()
-
-        self.assertIn('file_hashes', result.keys(), 'file hashes plugin not found')
-        self.assertIn('file_type', result.keys(), 'file type plugin not found')
-
-        self.assertNotIn('dummy_plug_in_for_testing_only', result.keys(), 'dummy plug-in not removed')
-
-    def test_get_plugin_dict_description(self):
-        result = self.sched.get_plugin_dict()
-        self.assertEqual(result['file_type'][0], self.sched.analysis_plugins['file_type'].DESCRIPTION, 'description not correct')
-
-    def test_get_plugin_dict_flags(self):
-        result = self.sched.get_plugin_dict()
-
-        self.assertTrue(result['file_hashes'][1], 'mandatory flag not set')
-        self.assertTrue(result['unpacker'][1], 'unpacker plugin not marked as mandatory')
-
-        self.assertTrue(result['file_hashes'][2]['default'], 'default flag not set')
-        self.assertFalse(result['file_type'][2]['default'], 'default flag set but should not')
-
-    def test_get_plugin_dict_version(self):
-        result = self.sched.get_plugin_dict()
-        self.assertEqual(self.sched.analysis_plugins['file_type'].VERSION, result['file_type'][3], 'version not correct')
-        self.assertEqual(self.sched.analysis_plugins['file_hashes'].VERSION, result['file_hashes'][3], 'version not correct')
-
-    def test_process_next_analysis_unknown_plugin(self):
-        test_fw = Firmware(file_path=os.path.join(get_test_data_dir(), 'get_files_test/testfile1'))
-        test_fw.scheduled_analysis = ['unknown_plugin']
-
-        with mock_spy(self.sched, '_start_or_skip_analysis') as spy:
-            self.sched.process_next_analysis(test_fw)
-            assert not spy.was_called(), 'unknown plugin should simply be skipped'
+        assert len(test_fw.processed_analysis) == 3, 'analysis not done'
+        assert test_fw.processed_analysis['dummy_plugin_for_testing_only']['1'] == 'first result', 'result not correct'
+        assert test_fw.processed_analysis['dummy_plugin_for_testing_only']['summary'] == ['first result', 'second result']
+        assert 'file_hashes' in test_fw.processed_analysis.keys(), 'Mandatory plug-in not executed'
+        assert 'file_type' in test_fw.processed_analysis.keys(), 'Mandatory plug-in not executed'
 
     def test_skip_analysis_because_whitelist(self):
         self.sched.config.set('dummy_plugin_for_testing_only', 'mime_whitelist', 'foo, bar')
@@ -114,20 +69,87 @@ class TestScheduleInitialAnalysis(AnalysisSchedulerTest):
         assert 'skipped' in test_fw.processed_analysis['dummy_plugin_for_testing_only']
 
 
+def mock_load_plugins(self):
+    self.analysis_plugins = {
+        'dummy_plugin_for_testing_only': PluginMock(whitelist='foo, bar'),
+        'file_type': PluginMock(), 'file_hashes': PluginMock()
+    }
+
+
+class TestScheduleInitialAnalysisNoPlugins(AnalysisSchedulerTest):
+    def setup(self):
+        self.load_plugins_patch = mock.patch(target='scheduler.Analysis.AnalysisScheduler.load_plugins', new=mock_load_plugins)
+        self.load_plugins_patch.start()
+        super().setup()
+
+    def teardown(self):
+        self.load_plugins_patch.stop()
+        super().teardown()
+
+    def test_schedule_firmware_init_no_analysis_selected(self):
+        self.sched.shutdown()
+        self.sched.process_queue = Queue()
+        test_fw = Firmware(binary=b'test')
+        self.sched.start_analysis_of_object(test_fw)
+        test_fw = self.sched.process_queue.get(timeout=5)
+        assert len(test_fw.scheduled_analysis) == len(MANDATORY_PLUGINS), 'Mandatory Plugins not selected'
+        for item in MANDATORY_PLUGINS:
+            assert item in test_fw.scheduled_analysis
+
+    def test_expected_plugins_are_found(self):
+        result = self.sched.get_plugin_dict()
+
+        assert 'file_hashes' in result.keys(), 'file hashes plugin not found'
+        assert 'file_type' in result.keys(), 'file type plugin not found'
+
+        assert 'dummy_plug_in_for_testing_only' not in result.keys(), 'dummy plug-in not removed'
+
+    def test_get_plugin_dict_description(self):
+        result = self.sched.get_plugin_dict()
+        assert result['file_type'][0] == self.sched.analysis_plugins['file_type'].DESCRIPTION, 'description not correct'
+
+    def test_get_plugin_dict_flags(self):
+        result = self.sched.get_plugin_dict()
+
+        assert result['file_hashes'][1], 'mandatory flag not set'
+        assert result['unpacker'][1], 'unpacker plugin not marked as mandatory'
+
+        assert result['file_hashes'][2]['default'], 'default flag not set'
+        assert not result['file_type'][2]['default'], 'default flag set but should not'
+
+    def test_get_plugin_dict_version(self):
+        result = self.sched.get_plugin_dict()
+        assert self.sched.analysis_plugins['file_type'].VERSION == result['file_type'][3], 'version not correct'
+        assert self.sched.analysis_plugins['file_hashes'].VERSION == result['file_hashes'][3], 'version not correct'
+
+    def test_process_next_analysis_unknown_plugin(self):
+        test_fw = Firmware(file_path=os.path.join(get_test_data_dir(), 'get_files_test/testfile1'))
+        test_fw.scheduled_analysis = ['unknown_plugin']
+
+        with mock_spy(self.sched, '_start_or_skip_analysis') as spy:
+            self.sched.process_next_analysis(test_fw)
+            assert not spy.was_called(), 'unknown plugin should simply be skipped'
+
+
+class PluginMock:
+    DESCRIPTION = 'foo'
+    VERSION = '1.2.3'
+
+    def __init__(self, blacklist=None, whitelist=None, dependencies=()):
+        self.DEPENDENCIES = dependencies
+        if blacklist:
+            self.MIME_BLACKLIST = blacklist
+        if whitelist:
+            self.MIME_WHITELIST = whitelist
+
+    def shutdown(self):
+        pass
+
+
 class TestAnalysisSchedulerBlacklist:
 
     test_plugin = 'test_plugin'
     file_object = MockFileObject()
-
-    class PluginMock:
-        def __init__(self, blacklist=None, whitelist=None):
-            if blacklist:
-                self.MIME_BLACKLIST = blacklist
-            if whitelist:
-                self.MIME_WHITELIST = whitelist
-
-        def shutdown(self):
-            pass
 
     @classmethod
     def setup_class(cls):
@@ -142,12 +164,12 @@ class TestAnalysisSchedulerBlacklist:
         self.sched.config = get_config_for_testing()
 
     def test_get_blacklist_and_whitelist_from_plugin(self):
-        self.sched.analysis_plugins['test_plugin'] = self.PluginMock(['foo'], ['bar'])
+        self.sched.analysis_plugins['test_plugin'] = PluginMock(['foo'], ['bar'])
         blacklist, whitelist = self.sched._get_blacklist_and_whitelist_from_plugin('test_plugin')
         assert (blacklist, whitelist) == (['foo'], ['bar'])
 
     def test_get_blacklist_and_whitelist_from_plugin__missing_in_plugin(self):
-        self.sched.analysis_plugins['test_plugin'] = self.PluginMock(['foo'])
+        self.sched.analysis_plugins['test_plugin'] = PluginMock(['foo'])
         blacklist, whitelist = self.sched._get_blacklist_and_whitelist_from_plugin('test_plugin')
         assert whitelist == []
         assert isinstance(blacklist, list)
@@ -160,47 +182,47 @@ class TestAnalysisSchedulerBlacklist:
 
     def test_get_blacklist_and_whitelist__in_config_and_plugin(self):
         self._add_test_plugin_to_config()
-        self.sched.analysis_plugins['test_plugin'] = self.PluginMock(['foo'], ['bar'])
+        self.sched.analysis_plugins['test_plugin'] = PluginMock(['foo'], ['bar'])
         blacklist, whitelist = self.sched._get_blacklist_and_whitelist('test_plugin')
         assert blacklist == ['type1', 'type2']
         assert whitelist == []
 
     def test_get_blacklist_and_whitelist__plugin_only(self):
-        self.sched.analysis_plugins['test_plugin'] = self.PluginMock(['foo'], ['bar'])
+        self.sched.analysis_plugins['test_plugin'] = PluginMock(['foo'], ['bar'])
         blacklist, whitelist = self.sched._get_blacklist_and_whitelist('test_plugin')
         assert (blacklist, whitelist) == (['foo'], ['bar'])
 
     def test_next_analysis_is_blacklisted__blacklisted(self):
-        self.sched.analysis_plugins[self.test_plugin] = self.PluginMock(blacklist=['blacklisted_type'])
+        self.sched.analysis_plugins[self.test_plugin] = PluginMock(blacklist=['blacklisted_type'])
         self.file_object.processed_analysis['file_type']['mime'] = 'blacklisted_type'
         blacklisted = self.sched._next_analysis_is_blacklisted(self.test_plugin, self.file_object)
         assert blacklisted is True
 
     def test_next_analysis_is_blacklisted__not_blacklisted(self):
-        self.sched.analysis_plugins[self.test_plugin] = self.PluginMock(blacklist=[])
+        self.sched.analysis_plugins[self.test_plugin] = PluginMock(blacklist=[])
         self.file_object.processed_analysis['file_type']['mime'] = 'not_blacklisted_type'
         blacklisted = self.sched._next_analysis_is_blacklisted(self.test_plugin, self.file_object)
         assert blacklisted is False
 
     def test_next_analysis_is_blacklisted__whitelisted(self):
-        self.sched.analysis_plugins[self.test_plugin] = self.PluginMock(whitelist=['whitelisted_type'])
+        self.sched.analysis_plugins[self.test_plugin] = PluginMock(whitelist=['whitelisted_type'])
         self.file_object.processed_analysis['file_type']['mime'] = 'whitelisted_type'
         blacklisted = self.sched._next_analysis_is_blacklisted(self.test_plugin, self.file_object)
         assert blacklisted is False
 
     def test_next_analysis_is_blacklisted__not_whitelisted(self):
-        self.sched.analysis_plugins[self.test_plugin] = self.PluginMock(whitelist=['some_other_type'])
+        self.sched.analysis_plugins[self.test_plugin] = PluginMock(whitelist=['some_other_type'])
         self.file_object.processed_analysis['file_type']['mime'] = 'not_whitelisted_type'
         blacklisted = self.sched._next_analysis_is_blacklisted(self.test_plugin, self.file_object)
         assert blacklisted is True
 
     def test_next_analysis_is_blacklisted__whitelist_precedes_blacklist(self):
-        self.sched.analysis_plugins[self.test_plugin] = self.PluginMock(blacklist=['test_type'], whitelist=['test_type'])
+        self.sched.analysis_plugins[self.test_plugin] = PluginMock(blacklist=['test_type'], whitelist=['test_type'])
         self.file_object.processed_analysis['file_type']['mime'] = 'test_type'
         blacklisted = self.sched._next_analysis_is_blacklisted(self.test_plugin, self.file_object)
         assert blacklisted is False
 
-        self.sched.analysis_plugins[self.test_plugin] = self.PluginMock(blacklist=[], whitelist=['some_other_type'])
+        self.sched.analysis_plugins[self.test_plugin] = PluginMock(blacklist=[], whitelist=['some_other_type'])
         self.file_object.processed_analysis['file_type']['mime'] = 'test_type'
         blacklisted = self.sched._next_analysis_is_blacklisted(self.test_plugin, self.file_object)
         assert blacklisted is True
