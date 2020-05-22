db88ff752fd8b97c88bb2d8a0b73a38aef9d63bb
Qwerty <22414069+Qwerty0x64@users.noreply.github.com>
2020-05-22 15:42:00 +0200

    update repo mongodb

--- src/install/pre_install_fedora.sh
+++ src/install/pre_install_fedora.sh
@@ -39,6 +39,16 @@ elif [ "${CODENAME}" = "ThirtyTwo" ]; then
 fi
 
 sudo systemctl start docker
+sudo systemctl enable docker
+
+echo "Adding mongodb repo" >> /etc/yum.repos.d/mongodb.repo
+echo "[mongodb-org-4.2]" >> /etc/yum.repos.d/mongodb.repo
+echo "name=MongoDB Repository" >> /etc/yum.repos.d/mongodb.repo
+echo "baseurl=https://repo.mongodb.org/yum/redhat/8/mongodb-org/4.2/x86_64/" >> /etc/yum.repos.d/mongodb.repo
+echo "gpgcheck=1" >> /etc/yum.repos.d/mongodb.repo
+echo "enabled=1" >> /etc/yum.repos.d/mongodb.repo
+echo "gpgkey=https://www.mongodb.org/static/pgp/server-4.2.asc" >> /etc/yum.repos.d/mongodb.repo
+
 
 # add fact-user to docker group
 if [ ! "$(getent group docker)" ]
