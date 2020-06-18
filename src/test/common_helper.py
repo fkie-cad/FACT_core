c52fb69b7fd5fa8592d619fd5e452d74e0b71276
dorp <dorp@cs.uni-bonn.de>
2020-06-18 09:11:16 +0200

    fixed tests

--- src/test/common_helper.py
+++ src/test/common_helper.py
@@ -291,7 +291,7 @@ class DatabaseMock:  # pylint: disable=too-many-public-methods
             return {'date_histogram_data': [['July 2014', 1]]}
         if identifier == 'backend':
             return {
-                'system':{'cpu_percentage': 13.37},
+                'system': {'cpu_percentage': 13.37},
                 'analysis': {'current_analyses': [None, None]}
             }
         return None
