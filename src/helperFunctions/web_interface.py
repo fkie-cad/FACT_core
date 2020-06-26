ecb60d78f3dc99153c432c7a2f108ac29cb190cc
Nicholas Starke <starke.nicholas@gmail.com>
2020-06-26 15:18:43 -0500

    Making radare web service host/ip configurable
    This PR makes the radare2 host url configurable via the main.cfg
    file. The default value is set to 'localhost', but should be set
    to the IP or hostname of where ever the radare service is running
    in distributed configurations.

--- src/helperFunctions/web_interface.py
+++ src/helperFunctions/web_interface.py
@@ -60,9 +60,10 @@ def get_template_as_string(view_name):
 
 
 def get_radare_endpoint(config):
+    radare2_host = config['ExpertSettings']['radare2_host']
     if config.getboolean('ExpertSettings', 'nginx'):
-        return 'https://localhost/radare'
-    return 'http://localhost:8000'
+        return 'https://' + radare2_host + '/radare'
+    return 'http://' + radare2_host + ':8000'
 
 
 def password_is_legal(pw: str) -> bool:
