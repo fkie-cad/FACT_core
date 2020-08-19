4147751b2cedbfd4fdae5f029ec39f09529a70a2
Jörg Stucke <joerg.stucke@fkie.fraunhofer.de>
2020-08-19 16:56:12 +0200

    sphinx docs init

---
+++ docs/conf.py
@@ -0,0 +1,53 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Path setup --------------------------------------------------------------
+
+import sys
+from pathlib import Path
+
+FACT_SRC = Path(__file__).parent.parent / 'src'
+sys.path.insert(0, str(FACT_SRC))
+
+
+# -- Project information -----------------------------------------------------
+
+project = 'FACT'
+copyright = '2020, jstucke'
+author = 'jstucke'
+
+
+# -- General configuration ---------------------------------------------------
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx_autodoc_typehints',
+]
+
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ['_templates']
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+# -- Options for HTML output -------------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+html_theme = 'sphinx_rtd_theme'
+# html_theme = 'alabaster'
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ['_static']
---
+++ docs/index.rst
@@ -0,0 +1,28 @@
+.. image:: ../src/web_interface/static/FACT_smaller.png
+   :width: 625
+   :alt: FACT Logo
+
+
+Contents
+============
+
+.. toctree::
+   :maxdepth: 1
+
+   main
+
+
+.. toctree::
+   :hidden:
+   :maxdepth: 2
+   :caption: Modules:
+
+   modules/helperFunctions
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
