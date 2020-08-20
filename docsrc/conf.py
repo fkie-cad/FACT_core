abc16c3029bdc86a0a0294b0647677f8a9931b5c
Jörg Stucke <joerg.stucke@fkie.fraunhofer.de>
2020-08-20 16:22:14 +0200

    added logo and changed color

--- docsrc/conf.py
+++ docsrc/conf.py
@@ -52,3 +52,9 @@ html_theme = 'sphinx_rtd_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+html_logo = "../src/web_interface/static/FACT_smaller.png"
+
+
+def setup(app):
+    app.add_css_file("css/custom.css")
