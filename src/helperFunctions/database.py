85c9ce6b3023fe9b6db20fe53c2766f86a4807ee
Jörg Stucke <joerg.stucke@fkie.fraunhofer.de>
2020-08-28 13:27:30 +0200

    added db helper docs

--- src/helperFunctions/database.py
+++ src/helperFunctions/database.py
@@ -1,15 +1,30 @@
 import logging
 import re
 import sys
+from configparser import ConfigParser
 from typing import Any, Generic, Type, TypeVar
 
 DatabaseInterface = TypeVar('DatabaseInterface')
 
 
 class ConnectTo(Generic[DatabaseInterface]):
-    def __init__(self, connected_interface: Type[DatabaseInterface], config):
+    '''
+    Open a database connection using the interface passed to the constructor. Intended to be used as a context manager.
+
+    :param connected_interface: A database interface from the `storage` module (e.g. `FrontEndDbInterface`)
+    :param config: A FACT configuration.
+
+    :Example:
+
+        .. code-block:: python
+
+           with ConnectTo(FrontEndDbInterface, self.config) as connection:
+                query = connection.firmwares.find({})
+    '''
+    def __init__(self, connected_interface: Type[DatabaseInterface], config: ConfigParser):
         self.interface = connected_interface
         self.config = config
+        self.connection = None
 
     def __enter__(self) -> DatabaseInterface:
         self.connection = self.interface(self.config)
