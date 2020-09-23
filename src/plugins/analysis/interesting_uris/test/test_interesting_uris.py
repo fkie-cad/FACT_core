7bb9e2dd5709af07d0a610f815e8b13eceb9ec13
any333 <anna-mariya.mateyna@gmx.de>
2020-09-23 16:36:06 +0200

    Add tests for interesting_uris plugin

--- src/plugins/analysis/interesting_uris/test/test_interesting_uris.py
+++ src/plugins/analysis/interesting_uris/test/test_interesting_uris.py
@@ -13,16 +13,16 @@ from test.common_helper import create_test_file_object
 def test_blacklist_ip_and_uris(input_list, blacklist, expected_output):
     assert AnalysisPlugin.blacklist_ip_and_uris(blacklist, input_list) == expected_output
 
-"""
-@pytest.mark.parametrize('input_list, blacklist, expected_output', [
+
+@pytest.mark.parametrize('input_list, whitelist, expected_output', [
     ([], ['abc', 'def'], []),
-    (['abcd', 'bcde'], [], ['abcd', 'bcde']),
-    (['abcd', 'bcde', 'cdef', 'efgh'], ['abc', 'def'], ['bcde', 'efgh']),
-    (['abcdefgh'], ['abc', 'def'], []),
+    (['abcd', 'bcde'], [], []),
+    (['abcd', 'bcde', 'cdef', 'efgh'], ['abcd', 'cdef'], ['abcd', 'cdef']),
+    (['abcf', 'bcfg', 'abci', 'bdhi'], ['abc', 'hi'], ['abcf', 'abci', 'bdhi']),
+    (['abcdefgh'], ['abc', 'def'], ['abcdefgh']),
 ])
-def test_whitelist_ip_and_uris(input_list, whitelist, expected_output):
-    assert AnalysisPlugin.blacklist_ip_and_uris(whitelist, input_list) == expected_output
-"""
+def test_white_ip_and_uris(input_list, whitelist, expected_output):
+    assert sorted(AnalysisPlugin.whitelist_ip_and_uris(whitelist, input_list)) == expected_output
 
 
 class TestAnalysisPluginInterestingUris(AnalysisPluginTest):
@@ -40,3 +40,8 @@ class TestAnalysisPluginInterestingUris(AnalysisPluginTest):
         self.analysis_plugin.process_object(fo)
         assert self.PLUGIN_NAME in fo.processed_analysis
         assert fo.processed_analysis[self.PLUGIN_NAME]['summary'] == ['www.interesting.receive.org']
+
+    def test_remove_ip_v4_v6_addresses(self):
+        assert self.analysis_plugin.remove_ip_v4_v6_addresses(['2001:db8::1', '127.0.255.250']) == []
+        assert self.analysis_plugin.remove_ip_v4_v6_addresses(['abcd', '127.0.255.250', 'bcde']) == ['abcd', 'bcde']
+        assert self.analysis_plugin.remove_ip_v4_v6_addresses(['abcd', '2001:db8::1', 'efgh']) == ['abcd', 'efgh']
