a7788b692667dece4a0d33e9db744a200c31554a
any333 <anna-mariya.mateyna@gmx.de>
2020-09-30 19:33:12 +0200

    Added a test for the crack_mosquitto_ hash func. Some tests need to be fixed

--- src/plugins/analysis/users_and_passwords/test/test_plugin_password_file_analyzer.py
+++ src/plugins/analysis/users_and_passwords/test/test_plugin_password_file_analyzer.py
@@ -55,3 +55,10 @@ class TestAnalysisPluginPasswordFileAnalyzer(AnalysisPluginTest):
         result_dict = {'user': dict()}
         assert self.analysis_plugin._crack_hash(passwd_entry, result_dict, 'user') is False  # pylint: disable=protected-access
         assert 'ERROR' in result_dict['user']
+
+
+def test_crack_mosquitto_hash():
+    passwd_entry = 'apson:$dynamic_82$7dc9fdfcb9f75ba73aa13de8f1b5a82cfae64d4b3e368c1c2986b9d961553f9a94e63cda76ac12abc17e438000af6a8e0be4a4e4b9479a2b2b8b812ac7bfee0e$HEX$3e1fae467d6f990fa903b29a'
+    result_entry = {}
+    assert AnalysisPlugin._crack_mosquitto_hash(passwd_entry, result_entry) is True  # pylint: disable=protected-access
+    assert 'password' in result_entry
