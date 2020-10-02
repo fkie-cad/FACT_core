041d643f2eda692978a8149d7595c77a7d7c2d08
any333 <anna-mariya.mateyna@gmx.de>
2020-10-02 21:27:46 +0200

    Reverted to old _add_found_password_tag func and made changes for backward compatibility

--- src/plugins/analysis/users_and_passwords/code/password_file_analyzer.py
+++ src/plugins/analysis/users_and_passwords/code/password_file_analyzer.py
@@ -1,10 +1,9 @@
-
 import logging
 import os
 import re
 from contextlib import suppress
 from pathlib import Path
-from tempfile import NamedTemporaryFile
+from tempfile import NamedTemporaryFile, TemporaryDirectory
 from base64 import b64decode
 
 from common_helper_process import execute_shell_command
@@ -21,7 +20,7 @@ class AnalysisPlugin(AnalysisBasePlugin):
     NAME = 'users_and_passwords'
     DEPENDENCIES = []
     MIME_BLACKLIST = ['audio', 'filesystem', 'image', 'video']
-    DESCRIPTION = 'search for UNIX and httpd password files, parse them and try to crack the passwords'
+    DESCRIPTION = 'search for UNIX, httpd, and mosquitto password files, parse them and try to crack the passwords'
     VERSION = '0.4.5'
 
     wordlist_path = os.path.join(get_src_dir(), 'bin/passwords.txt')
@@ -54,30 +53,27 @@ class AnalysisPlugin(AnalysisBasePlugin):
         for passwd_regex in [br'[a-zA-Z][a-zA-Z0-9_-]{2,15}\:\$6\$[a-zA-Z0-9+/=]+\$[a-zA-Z0-9+/]{86}==']:
             passwd_entries = re.findall(passwd_regex, file_object.binary)
             if passwd_entries:
-                result = self._generate_mosquitto_entry(passwd_entries, file_object.uid)
+                result = self._generate_mosquitto_entry(passwd_entries)
                 file_object.processed_analysis[self.NAME].update(result)
                 file_object.processed_analysis[self.NAME]['summary'] += list(result.keys())
                 self._add_found_password_tag(file_object, result)
 
     def _add_found_password_tag(self, file_object, result):
         for password_entry in result:
-            for password_type in result[password_entry]:
-                if 'passwd' in result[password_entry] or 'mosquitto' in result[password_entry]:
-                    if 'password' in result[password_entry][password_type]:
-                        self.add_analysis_tag(
-                            file_object,
-                            '{}_{}'.format(password_entry, result[password_entry][password_type]['password']),
-                            'Password: {}:{}'.format(password_entry, result[password_entry][password_type]['password']),
-                            TagColor.RED,
-                            True
-                        )
+            if 'password' in result[password_entry]:
+                self.add_analysis_tag(
+                    file_object,
+                    '{}_{}'.format(password_entry, result[password_entry]['password']),
+                    'Password: {}:{}'.format(password_entry, result[password_entry]['password']),
+                    TagColor.RED,
+                    True
+                )
 
     def _generate_analysis_entry(self, passwd_entries, uid: str):
         result = {}
         for entry in [e.split(b':') for e in passwd_entries]:
             key = entry[0].decode(encoding='utf_8', errors='replace')
-            result.setdefault(key, {})
-            result_entry = result[key]['passwd'] = {}
+            result_entry = result['{}:unix'.format(key)] = {}
             result_entry['entry'] = b':'.join(entry).decode(encoding='utf_8', errors='replace')
             try:
                 if entry[1][0] == ord('$'):
@@ -88,16 +84,16 @@ class AnalysisPlugin(AnalysisBasePlugin):
                 logging.warning('Unsupported Format: {}'.format(uid), exc_info=True)
         return result
 
-    def _generate_mosquitto_entry(self, passwd_entries, uid: str):
+    def _generate_mosquitto_entry(self, passwd_entries):
         result = {}
         for entry in [m.split(b'$') for m in passwd_entries]:
             user = entry[0].decode(encoding='utf_8', errors='replace')[:-1]
             salt_hash = entry[2].decode(encoding='utf_8', errors='replace')
             passwd_hash = entry[3].decode(encoding='utf_8', errors='replace')
-            passwd_entry = '{}$dynamic_82${}$HEX${}'.format(user, b64decode(passwd_hash).hex(), b64decode(salt_hash).hex())
-            result.setdefault(user, {})
-            result_entry = result[user]['mosquitto'] = {}
+            passwd_entry = '{}:$dynamic_82${}$HEX${}'.format(user, b64decode(passwd_hash).hex(), b64decode(salt_hash).hex())
+            result_entry = result['{}:mosquitto'.format(user)] = {}
             result_entry['entry'] = b'$'.join(entry).decode(encoding='utf_8', errors='replace')
+            result_entry['password-hash'] = passwd_hash
             cracked_pw = self._crack_mosquitto_hash(passwd_entry, result_entry)
             result_entry['cracked'] = bool(cracked_pw)
         return result
