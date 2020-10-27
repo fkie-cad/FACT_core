2597315c75301a79e2b8758af0f8f5a748da2576
any333 <anna-mariya.mateyna@gmx.de>
2020-10-27 17:00:27 +0100

    Deleted uncompatible tests

--- src/plugins/analysis/linter/test/test_source_code_analysis.py
+++ src/plugins/analysis/linter/test/test_source_code_analysis.py
@@ -32,18 +32,6 @@ def stub_plugin(test_config, monkeypatch):
     return AnalysisPlugin(MockAdmin(), test_config, offline_testing=True)
 
 
-@pytest.mark.parametrize('mime_and_type', [
-    ('Python script, ASCII text executable', 'python'),
-    ('python3 script, ASCII text executable', 'python'),
-    ('Bourne-Again shell script, UTF-8 Unicode text executable', 'shell')
-])
-def test_determine_script_type_file_type(mime_and_type, stub_plugin, test_object):
-    mime, script_type = mime_and_type
-    test_object.processed_analysis['file_type'] = {'full': mime}
-
-    assert stub_plugin._determine_script_type(test_object) == script_type
-
-
 @pytest.mark.parametrize('shebang_and_type', [
     (b'#!/usr/bin/env python3', 'python'),
     (b'#!/usr/bin/python', 'python'),
@@ -53,17 +41,6 @@ def test_determine_script_type_file_type(mime_and_type, stub_plugin, test_object
 def test_determine_script_type_shebang(shebang_and_type, stub_plugin, test_object):
     shebang, script_type = shebang_and_type
     test_object.binary = shebang + b'\n' + test_object.binary
-
-    assert stub_plugin._determine_script_type(test_object) == script_type
-
-
-@pytest.mark.parametrize('ending_and_type', [
-    ('.py', 'python'),
-    ('.sh', 'shell')
-])
-def test_determine_script_type_ending(ending_and_type, stub_plugin, test_object):
-    ending, script_type = ending_and_type
-    test_object.file_name = test_object.file_name + ending
 
     assert stub_plugin._determine_script_type(test_object) == script_type
 
@@ -93,4 +70,5 @@ def test_process_object_no_issues(stub_plugin, test_object, monkeypatch):
                         lambda self, file_path: list())
     stub_plugin.process_object(test_object)
     result = test_object.processed_analysis[stub_plugin.NAME]
+    print(result)
     assert 'full' not in result
