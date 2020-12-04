e412fd3c0a3159b25e6e76aae671e49cc1ebdd9f
Jörg Stucke <joerg.stucke@fkie.fraunhofer.de>
2020-12-04 14:56:42 +0100

    pep8 action test

---
+++ src/pep8_test.py
@@ -0,0 +1,19 @@
+from helperFunctions import *
+import os, sys
+
+x = 4
+
+
+def foo(x):
+    return (os.urandom(10))
+
+class myClass:
+
+    @staticmethod
+
+    def  bar( y):
+        try:
+            return (y*y+ \
+               x)
+        except:
+            pass
