e35bd996ed5a038bff8301bb716c073abcb4a8c5
Jörg Stucke <joerg.stucke@fkie.fraunhofer.de>
2021-01-12 11:47:24 +0100

    fixed tests

--- src/test/unit/helperFunctions/test_file_system.py
+++ src/test/unit/helperFunctions/test_file_system.py
@@ -1,64 +1,52 @@
-import os
-import unittest
+# pylint: disable=redefined-outer-name,unused-argument
 
-from helperFunctions.fileSystem import (
-    _get_relative_path, file_is_empty, get_absolute_path, get_object_path_excluding_fact_dirs, get_parent_dir,
-    get_src_dir, get_template_dir
-)
+import os
+from pathlib import Path
+
+import pytest
+
+from helperFunctions.fileSystem import file_is_empty, get_relative_object_path, get_src_dir, get_template_dir
 from test.common_helper import get_test_data_dir
 
+TEST_DATA_DIR = Path(get_test_data_dir())
 
-class TestFileSystemHelpers(unittest.TestCase):
 
-    def setUp(self):
-        self.current_cwd = os.getcwd()
+@pytest.fixture()
+def restore_cwd():
+    current_cwd = os.getcwd()
+    yield
+    os.chdir(current_cwd)
 
-    def tearDown(self):
-        os.chdir(self.current_cwd)
 
-    def test_get_parent_dir(self):
-        self.assertEqual(get_parent_dir('/foo/bar/test'), '/foo/bar', 'parent directory')
+@pytest.mark.parametrize('working_directory', [os.getcwd(), '/'])
+def test_get_src_dir_cwd(restore_cwd, working_directory):
+    real_src_dir = get_src_dir()
+    os.chdir(working_directory)
+    assert os.path.exists('{}/helperFunctions/fileSystem.py'.format(real_src_dir)), 'fileSystem.py found in correct place'
+    assert get_src_dir() == real_src_dir, 'same source dir before and after chdir'
 
-    def check_correct_src_dir(self, working_directory):
-        real_src_dir = get_src_dir()
-        os.chdir(working_directory)
-        self.assertTrue(os.path.exists('{}/helperFunctions/fileSystem.py'.format(real_src_dir)), 'fileSystem.py found in correct place')
-        self.assertEqual(get_src_dir(), real_src_dir, 'same source dir before and after chdir')
 
-    def test_get_src_dir_cwd(self):
-        self.check_correct_src_dir(os.getcwd())
+def test_get_template_dir():
+    template_dir = get_template_dir()
+    assert template_dir.is_dir(), 'template dir not found'
+    file_suffixes_in_template_dir = [f.suffix for f in template_dir.iterdir()]
+    assert '.html' in file_suffixes_in_template_dir
 
-    def test_get_src_dir_root(self):
-        self.check_correct_src_dir('/')
 
-    def test_get_template_dir(self):
-        template_dir = get_template_dir()
-        self.assertTrue(os.path.isdir(template_dir), 'template dir not found')
-        file_suffixes_in_template_dir = [os.path.basename(f).split('.')[-1] for f in os.listdir(template_dir)]
-        self.assertTrue('html' in file_suffixes_in_template_dir)
+@pytest.mark.parametrize('base, offset, result, message', [
+    (Path('/foo/bar/com'), Path('/foo/'), '/bar/com', 'simple case with /'),
+    (Path('/foo/bar/com'), Path('/foo'), '/bar/com', 'simple case without /'),
+    (Path('/foo/bar/com'), Path('/bar'), '/foo/bar/com', 'non-matching root'),
+    (Path('/foo/fact_extracted/bar/com'), Path('/foo'), '/bar/com', 'including extracted'),
+])
+def test_get_relative_object_path(base, offset, result, message):
+    assert get_relative_object_path(base, offset) == result, message
 
-    def test_get_absolute_path(self):
-        abs_path = '/foo/bar'
-        self.assertEqual(get_absolute_path(abs_path), '/foo/bar', 'absolute path of absolute path not correct')
-        rel_path = 'foo/bar'
-        self.assertEqual(get_absolute_path(rel_path, base_dir='/the'), '/the/foo/bar', 'absolute path of relative path not correct')
 
-    def test_get_chroot_path(self):
-        a = _get_relative_path('/foo/bar/com', '/foo/')
-        self.assertEqual(a, '/bar/com', 'simple case with /')
-        b = _get_relative_path('/foo/bar/com', '/foo')
-        self.assertEqual(b, '/bar/com', 'simple case without /')
-        c = _get_relative_path('/foo/bar/com', '/bar')
-        self.assertEqual(c, '/foo/bar/com', 'none matching root')
+def test_file_is_zero():
+    assert file_is_empty(TEST_DATA_DIR / 'zero_byte'), 'file is empty but stated differently'
+    assert not file_is_empty(TEST_DATA_DIR / 'get_files_test' / 'testfile1'), 'file not empty but stated differently'
 
-    def test_get_chroot_excluding_extracted_prefix_dir(self):
-        d = get_object_path_excluding_fact_dirs('/foo/fact_extracted/bar/com', '/foo')
-        self.assertEqual(d, '/bar/com', 'including extracted')
 
-    def test_file_is_zero(self):
-        self.assertTrue(file_is_empty('{}/zero_byte'.format(get_test_data_dir())), 'file is empty but stated differently')
-        self.assertFalse(file_is_empty('{}/get_files_test/testfile1'.format(get_test_data_dir())), 'file not empty but stated differently')
-        self.assertFalse(file_is_empty(os.path.join(get_test_data_dir(), 'broken_link')), 'Broken link is not empty')
-
-    def test_file_is_zero_broken_link(self):
-        self.assertFalse(file_is_empty(os.path.join(get_test_data_dir(), 'broken_link')), 'Broken link is not empty')
+def test_file_is_zero_broken_link():
+    assert not file_is_empty(TEST_DATA_DIR / 'broken_link'), 'Broken link is not empty'
