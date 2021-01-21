cbee151a49c8a5fe501cb668c21814cdc8fefb8e
Jörg Stucke <joerg.stucke@fkie.fraunhofer.de>
2021-01-21 09:26:24 +0100

    lief install bugfix

--- src/install/common.py
+++ src/install/common.py
@@ -62,7 +62,7 @@ def main(distribution):  # pylint: disable=too-many-statements
     pip3_install_packages('pytest==3.5.1', 'pytest-cov', 'pytest-flake8', 'pylint', 'python-magic', 'xmltodict', 'yara-python==3.7.0', 'appdirs')
     pip3_install_packages('ssdeep')
 
-    pip3_install_packages('lief')
+    pip3_install_packages('lief==0.10.1')
 
     pip3_install_packages('requests')
 
