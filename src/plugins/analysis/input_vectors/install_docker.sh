41ede5f3ad92e8cea6fc4443461f8ce73d8e30bc
Marten Ringwelski <git@maringuu.de>
2021-06-14 18:19:22 +0200

    input_vectors: Split install.sh

--- src/plugins/analysis/input_vectors/install_docker.sh
+++ src/plugins/analysis/input_vectors/install_docker.sh
@@ -1,15 +1,9 @@
 #!/usr/bin/env bash
 
-# change cwd to current file's directory
+# change cwd to this file's directory
 cd "$( dirname "${BASH_SOURCE[0]}" )" || exit 1
-
-echo "------------------------------------"
-echo " Installing input_vectors Plugin "
-echo "------------------------------------"
 
 docker pull fkiecad/radare-web-gui:latest || exit 1
 
 echo "Building docker container"
 docker build -t input-vectors . || exit 1
-
-exit 0
