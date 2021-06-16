2c52fb7cc303c10fdb11ef4b3686a339e7f71601
Marten Ringwelski <git@maringuu.de>
2021-06-16 17:31:44 +0200

    Add Dockerfile

---
+++ docker/entrypoint.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+
+case "$1" in
+    "start")
+        shift 1
+        # TODO this path could be configurable
+        # It is not really necessary to make it configurable since there is no
+        # usecase to change the database path inside the docker container
+        if [ -e /media/data/fact_fw_data/REINITIALIZE_DB ]; then
+            python3 /opt/FACT_core/src/init_database.py
+        fi
+        exec /opt/FACT_core/start_all_installed_fact_components "$@"
+    ;;
+    "pull-containers")
+        # We cant to this in the Dockerfile, because the docker socket is not shared to there
+        exec /opt/FACT_core/src/install.py \
+            --backend-docker-images \
+            --frontend-docker-images
+    ;;
+    *)
+        printf "See https://github.com/fkie-cad/FACT_core/blob/master/docker/README.md for how to start this container\n"
+        exit 0
+esac
