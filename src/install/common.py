937dcd74f8e3a74d4c9c1da536ab057fce5e89d9
Marten Ringwelski <git@maringuu.de>
2021-07-06 17:33:24 +0200

    installer/common: Extract pip dependencies to requirements_common.txt

--- src/install/common.py
+++ src/install/common.py
@@ -6,7 +6,7 @@ from common_helper_process import execute_shell_command_get_return_code
 
 from helperFunctions.install import (
     InstallationError, OperateInDirectory, apt_install_packages, apt_remove_packages, apt_update_sources,
-    dnf_install_packages, dnf_remove_packages, dnf_update_sources, install_github_project, pip3_install_packages
+    dnf_install_packages, dnf_remove_packages, dnf_update_sources, install_github_project
 )
 
 BIN_DIR = Path(__file__).parent.parent / 'bin'
@@ -43,7 +43,6 @@ def main(distribution):  # pylint: disable=too-many-statements
         dnf_install_packages('gcc', 'gcc-c++', 'make', 'kernel-devel')
     else:
         apt_install_packages('python3', 'python3-dev', 'build-essential', 'automake', 'autoconf', 'libtool', 'git', 'unzip')
-        pip3_install_packages('testresources')
 
     # get a bug free recent pip version
     if distribution == 'fedora':
@@ -52,43 +51,22 @@ def main(distribution):  # pylint: disable=too-many-statements
         apt_remove_packages('python3-pip', 'python3-setuptools', 'python3-wheel')
 
     install_pip('python3')
-    pip3_install_packages('setuptools')
 
     # install general python dependencies
     if distribution == 'fedora':
         dnf_install_packages('file-devel')
         dnf_install_packages('libffi-devel')
         dnf_install_packages('python3-tlsh')
-        dnf_install_packages('python3-ssdeep')
     else:
         apt_install_packages('libmagic-dev')
         apt_install_packages('libfuzzy-dev')
         apt_install_packages('python3-tlsh')
-        pip3_install_packages('ssdeep')
-
-    pip3_install_packages('git+https://github.com/fkie-cad/fact_helper_file.git')
-    pip3_install_packages('psutil', 'pytest', 'pytest-cov', 'pylint', 'python-magic', 'xmltodict', 'yara-python==3.7.0', 'appdirs', 'flaky')
-
-    pip3_install_packages('lief')
-
-    pip3_install_packages('requests')
-
-    # install python MongoDB bindings
-    pip3_install_packages('pymongo', 'pyyaml')
 
     # VarietyJS (is executed by update_statistic.py)
     if (BIN_DIR / 'spec').exists():
         logging.warning('variety spec not overwritten')
     else:
         install_github_project('variety/variety', ['git checkout 2f4d815', 'mv -f variety.js ../../bin', 'mv -f spec ../../bin'])
-
-    #  installing common code modules
-    pip3_install_packages('hurry.filesize')
-    pip3_install_packages('git+https://github.com/fkie-cad/common_helper_files.git')
-    pip3_install_packages('git+https://github.com/fkie-cad/common_helper_mongo.git')
-    pip3_install_packages('git+https://github.com/mass-project/common_helper_encoder.git')
-    pip3_install_packages('git+https://github.com/fkie-cad/common_helper_filter.git')
-    pip3_install_packages('git+https://github.com/fkie-cad/common_helper_process.git')
 
     with OperateInDirectory('../../'):
         with suppress(FileNotFoundError):
