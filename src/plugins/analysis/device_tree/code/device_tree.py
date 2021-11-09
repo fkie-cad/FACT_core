282394002406b18b8eb3db1b6b2ee4085a2b4e31
Mic27M <mmuench27@gmail.com>
2021-11-09 16:20:49 +0100

    test + renamed

---
+++ src/plugins/analysis/device_tree/code/device_tree.py
@@ -0,0 +1,39 @@
+from common_helper_process import execute_shell_command_get_return_code
+
+from analysis.PluginBase import AnalysisBasePlugin
+
+
+class AnalysisPlugin(AnalysisBasePlugin):
+    '''
+    Device Tree Plug-in
+    '''
+    NAME = 'device_tree'
+    DESCRIPTION = 'get the device tree in text from from the device tree blob'
+    DEPENDENCIES = []
+    VERSION = '0.1'
+    MIME_WHITELIST = ['linux/device-tree']
+    # TODO installation
+
+    def __init__(self, plugin_administrator, config=None, recursive=True):
+        self.config = config
+
+        super().__init__(plugin_administrator, config=config,
+                         recursive=recursive, plugin_path=__file__)
+
+    def process_object(self, file_object):
+
+        device_tree = self.execute_device_tree_compiler(file_object.file_path)
+
+        # store the results
+        file_object.processed_analysis[self.NAME] = dict()
+        file_object.processed_analysis[self.NAME]['device_tree'] = device_tree
+
+        file_object.processed_analysis[self.NAME]['summary'] = ['device tree found']
+
+        return file_object
+
+    def execute_device_tree_compiler(self, file_path):
+        dtc_result, return_code = execute_shell_command_get_return_code(f'dtc -I dtb -O dts {file_path}')
+        if return_code != 0:
+            raise ValueError(f'The Device Tree Compiler exited with non-zero return code {return_code} after working on {file_path}')
+        return dtc_result
---
+++ src/plugins/analysis/device_tree/test/test_device_tree.py
@@ -0,0 +1,37 @@
+from pathlib import Path
+
+from test.unit.analysis.analysis_plugin_test_class import AnalysisPluginTest
+from objects.file import FileObject
+from ..code.device_tree import AnalysisPlugin
+
+TEST_DATA = Path(__file__).parent.parent / 'test/data'
+
+
+class test_device_tree(AnalysisPluginTest):
+
+    PLUGIN_NAME = 'device_tree'
+
+    def setUp(self):
+        super().setUp()
+        config = self.init_basic_config()
+
+        self.analysis_plugin = AnalysisPlugin(self, config=config)
+        self.test_file = TEST_DATA / 'device_tree.dtb'
+
+    def tearDown(self):
+        super().tearDown()
+        # additional tearDown can go here
+
+    def test_process_object(self):
+        test_object = FileObject()
+        test_object.file_path = str(self.test_file)
+        result = self.analysis_plugin.process_object(test_object)
+
+        assert result.processed_analysis[self.PLUGIN_NAME]['summary'] == ['device tree found']
+
+    def test_execute_device_tree_compiler(self):
+        test_object = self.test_file
+        result = self.analysis_plugin.execute_device_tree_compiler(test_object)
+        asserted_in_result = '#address-cells = <0x01>;'
+
+        assert asserted_in_result in result
