bc09e38281a9bf222b8553ec0212678ed3dab9fc
Marten Ringwelski <git@maringuu.de>
2021-12-14 09:01:30 +0100

    run_docker_container: Simplify; Typehints

--- src/helperFunctions/docker.py
+++ src/helperFunctions/docker.py
@@ -1,13 +1,14 @@
 import logging
 from contextlib import suppress
+from typing import Tuple
 
 import docker
 from docker.errors import APIError, DockerException, ImageNotFound
 from requests.exceptions import ReadTimeout
 
 
-def run_docker_container(image: str, logging_label: str = 'Docker', timeout: int = 300,  stderr=True, **kwargs):
-    """
+def run_docker_container(image: str, logging_label: str = 'Docker', timeout: int = 300,  stderr=True, **kwargs) -> Tuple[str, int]:
+    '''
     This is a convinience function that runs a docker container and returns the output and exit code of the command.
     All remaining keyword args are passed to `docker.containers.run`.
 
@@ -21,7 +22,7 @@ def run_docker_container(image: str, logging_label: str = 'Docker', timeout: int
     :raises docker.errors.ImageNotFound: If the docker image was not found
     :raises requests.exceptions.ReadTimeout: If the timeout was reached
     :raises docker.errors.APIError: If the communication with docker fails
-    """
+    '''
     # TODO verify that bind mounts in kwargs["mounts"] only contain files in docker-mount-base-dir
     # If they don't just copy them to docker-mount-base-dir and change the Mount's
 
@@ -31,26 +32,16 @@ def run_docker_container(image: str, logging_label: str = 'Docker', timeout: int
     try:
         container = client.containers.run(image, **kwargs)
     except (ImageNotFound, APIError):
-        logging.warning(f'[{logging_label}]: encountered process error while processing')
-        with suppress(DockerException):
-            container.stop()
-            container.remove()
-
+        logging.warning(f'[{logging_label}]: encountered docker error while processing')
         raise
 
     try:
         response = container.wait(timeout=timeout)
         exit_code = response['StatusCode']
+        output = container.logs(stderr=stderr).decode()
     except ReadTimeout:
         logging.warning(f'[{logging_label}]: timeout while processing')
-        with suppress(DockerException):
-            container.stop()
-            container.remove()
-
         raise
-
-    try:
-        output = container.logs(stderr=stderr).decode()
     except APIError:
         logging.warning(f'[{logging_label}]: encountered docker error while processing')
         raise
