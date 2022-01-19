5dca5de3ae9e860809d5fb5fc72b0905cb108d0d
Jörg Stucke <joerg.stucke@fkie.fraunhofer.de>
2022-01-19 13:49:59 +0100

    removed old db integration tests and replaced them with the (existing) postgres tests

--- src/test/integration/statistic/test_update.py
+++ src/test/integration/statistic/test_update.py
@@ -7,7 +7,7 @@ import pytest
 from statistic.update import StatsUpdater
 from storage_postgresql.db_interface_stats import StatsUpdateDbInterface
 from test.common_helper import create_test_file_object, create_test_firmware, get_config_for_testing
-from test.integration.storage_postgresql.helper import (
+from test.integration.storage.helper import (
     create_fw_with_parent_and_child, generate_analysis_entry, insert_test_fo, insert_test_fw
 )
 
