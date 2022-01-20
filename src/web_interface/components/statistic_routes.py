7daed191ac1811f2148411faeb623ea96d54f946
Jörg Stucke <joerg.stucke@fkie.fraunhofer.de>
2022-01-20 15:48:25 +0100

    live stats bugfix

--- src/web_interface/components/statistic_routes.py
+++ src/web_interface/components/statistic_routes.py
@@ -2,12 +2,16 @@ from flask import render_template, request
 
 from helperFunctions.database import ConnectTo
 from helperFunctions.web_interface import apply_filters_to_query
+from statistic.update import StatsUpdater
 from web_interface.components.component_base import GET, AppRoute, ComponentBase
 from web_interface.security.decorator import roles_accepted
 from web_interface.security.privileges import PRIVILEGES
 
 
 class StatisticRoutes(ComponentBase):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.stats_updater = StatsUpdater(stats_db=self.db.stats_updater)
 
     @roles_accepted(*PRIVILEGES['status'])
     @AppRoute('/statistic', GET)
@@ -54,20 +58,20 @@ class StatisticRoutes(ComponentBase):
         return stats_dict
 
     def _get_live_stats(self, filter_query):
-        self.db.stats_updater.set_match(filter_query)
+        self.stats_updater.set_match(filter_query)
         stats_dict = {
-            'firmware_meta_stats': self.db.stats_updater.get_firmware_meta_stats(),
-            'file_type_stats': self.db.stats_updater.get_file_type_stats(),
-            'malware_stats': self.db.stats_updater.get_malware_stats(),
-            'crypto_material_stats': self.db.stats_updater.get_crypto_material_stats(),
-            'unpacker_stats': self.db.stats_updater.get_unpacking_stats(),
-            'ip_and_uri_stats': self.db.stats_updater.get_ip_stats(),
-            'architecture_stats': self.db.stats_updater.get_architecture_stats(),
-            'release_date_stats': self.db.stats_updater.get_time_stats(),
-            'general_stats': self.db.stats_updater.get_general_stats(),
-            'exploit_mitigations_stats': self.db.stats_updater.get_exploit_mitigations_stats(),
-            'known_vulnerabilities_stats': self.db.stats_updater.get_known_vulnerabilities_stats(),
-            'software_stats': self.db.stats_updater.get_software_components_stats(),
-            'elf_executable_stats': self.db.stats_updater.get_executable_stats(),
+            'firmware_meta_stats': self.stats_updater.get_firmware_meta_stats(),
+            'file_type_stats': self.stats_updater.get_file_type_stats(),
+            'malware_stats': self.stats_updater.get_malware_stats(),
+            'crypto_material_stats': self.stats_updater.get_crypto_material_stats(),
+            'unpacker_stats': self.stats_updater.get_unpacking_stats(),
+            'ip_and_uri_stats': self.stats_updater.get_ip_stats(),
+            'architecture_stats': self.stats_updater.get_architecture_stats(),
+            'release_date_stats': self.stats_updater.get_time_stats(),
+            'general_stats': self.stats_updater.get_general_stats(),
+            'exploit_mitigations_stats': self.stats_updater.get_exploit_mitigations_stats(),
+            'known_vulnerabilities_stats': self.stats_updater.get_known_vulnerabilities_stats(),
+            'software_stats': self.stats_updater.get_software_components_stats(),
+            'elf_executable_stats': self.stats_updater.get_executable_stats(),
         }
         return stats_dict
