6c9a7072dcbe2704c75b7735b9a5aee6a6dbceb2
Jörg Stucke <joerg.stucke@fkie.fraunhofer.de>
2022-02-18 15:54:26 +0100

    fixed error in show_analysis caused by missing root_uid

--- src/web_interface/components/analysis_routes.py
+++ src/web_interface/components/analysis_routes.py
@@ -194,6 +194,9 @@ class AnalysisRoutes(ComponentBase):
     @AppRoute('/dependency-graph/<uid>/<root_uid>', GET)
     def show_elf_dependency_graph(self, uid, root_uid):
         with ConnectTo(FrontEndDbInterface, self._config) as db:
+            if root_uid in [None, 'None']:
+                fo = db.get_object(uid)
+                root_uid = list(fo.parent_firmware_uids)[0]
             data = db.get_data_for_dependency_graph(uid, root_uid)
 
             whitelist = ['application/x-executable', 'application/x-pie-executable', 'application/x-sharedlib', 'inode/symlink']
