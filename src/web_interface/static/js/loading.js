b88e1f5b58f5ac38c1d30bc94ae16962058e61a8
Jörg Stucke <joerg.stucke@fkie.fraunhofer.de>
2022-02-18 08:54:09 +0100

    Update src/web_interface/static/js/loading.js
    Co-authored-by: René Helmke <5111321+rhelmke@users.noreply.github.com>

--- src/web_interface/static/js/loading.js
+++ src/web_interface/static/js/loading.js
@@ -1,4 +1,4 @@
 function showImg() {
-    document.getElementById("form").style.display = "none";
-    document.getElementById("loading_img").style.display = "block";
+    $('#form').hide();
+    $('#loading_img').css({'display': 'block'});
 }
