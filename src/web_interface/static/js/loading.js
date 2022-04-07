f9c4204c56853596a134e205393133cb3628e428
Jörg Stucke <joerg.stucke@fkie.fraunhofer.de>
2022-04-07 10:25:32 +0200

    hide loading gif when going back in page history

--- src/web_interface/static/js/loading.js
+++ src/web_interface/static/js/loading.js
@@ -2,3 +2,8 @@ function showImg() {
     $('#form').hide();
     $('#loading_img').css({'display': 'block'});
 }
+
+function hideImg() {
+    $('#form').show();
+    $('#loading_img').css({'display': 'none'});
+}
