aef42a22cc39adb5c86dbedd7ed5f03e8a94d435
Marten Ringwelski <git@maringuu.de>
2022-04-20 12:55:48 +0200

    tests: Make web_frontend fixture usable for integration/acceptance tests

--- src/test/conftest.py
+++ src/test/conftest.py
@@ -1,7 +1,14 @@
 import gc
+from typing import List
 
 import pytest
 
+from storage.db_interface_admin import AdminDbInterface
+from storage.db_interface_backend import BackendDbInterface
+from storage.db_interface_common import DbInterfaceCommon
+from storage.db_interface_frontend import FrontEndDbInterface
+from storage.db_interface_frontend_editing import FrontendEditingDbInterface
+from storage.db_setup import DbSetup
 from test.common_helper import CommonDatabaseMock, CommonIntercomMock
 from web_interface.frontend_main import WebFrontEnd
 from web_interface.security.authentication import add_flask_security_to_app
@@ -32,30 +39,39 @@ class UserDbMock:
 @pytest.fixture
 def web_frontend(request, monkeypatch, cfg_tuple) -> WebFrontEnd:
     _, configparser_cfg = cfg_tuple
-
-    # TODO rename marker, find out why lambda has to be used
     db_mock_marker = request.node.get_closest_marker('db_mock')
-    db_mock = db_mock_marker.args[0]() if db_mock_marker else CommonDatabaseMock
-    db_mock_instance = db_mock()
-
-    # TODO rename marker, find out why lambda has to be used
     intercom_mock_marker = request.node.get_closest_marker('intercom_mock')
-    intercom_mock = intercom_mock_marker.args[0]() if intercom_mock_marker else CommonIntercomMock
 
-    def add_security_get_mocked(app):
-        add_flask_security_to_app(app)
-        return UserDbMock(), db_mock_instance
+    if 'test_real_database' not in request.fixturenames:
+        # TODO rename marker, find out why lambda has to be used
+        db_mock = db_mock_marker.args[0]() if db_mock_marker else CommonDatabaseMock
+        db_mock_instance = db_mock()
 
-    monkeypatch.setattr('web_interface.frontend_main.add_flask_security_to_app', add_security_get_mocked)
+        # TODO rename marker, find out why lambda has to be used
+        intercom_mock = intercom_mock_marker.args[0]() if intercom_mock_marker else CommonIntercomMock
 
-    frontend = WebFrontEnd(config=configparser_cfg, db=FrontendDbMock(db_mock_instance), intercom=intercom_mock)
+        def add_security_get_mocked(app):
+            add_flask_security_to_app(app)
+            return UserDbMock(), db_mock_instance
+
+        monkeypatch.setattr('web_interface.frontend_main.add_flask_security_to_app', add_security_get_mocked)
+
+        frontend = WebFrontEnd(config=configparser_cfg, db=FrontendDbMock(db_mock_instance), intercom=intercom_mock)
+    else:
+        assert db_mock_marker is None, "You can't mock the database if you use test_real_database"
+        assert intercom_mock_marker is None, "You can't mock the database if you use test_real_database"
+
+        frontend = WebFrontEnd(config=configparser_cfg)
+
     frontend.app.config['TESTING'] = True
 
     yield frontend
-    # TODO This should not have to be done here
-    # State should not be stored in class variables.
-    # Otherwise tests are not isolated.
-    intercom_mock.tasks = []
+
+    if 'test_real_database' not in request.fixturenames:
+        # TODO This should not have to be done here
+        # State should not be stored in class variables.
+        # Otherwise tests are not isolated.
+        intercom_mock.tasks = []
 
     gc.collect()
 
@@ -63,3 +79,64 @@ def web_frontend(request, monkeypatch, cfg_tuple) -> WebFrontEnd:
 @pytest.fixture
 def test_client(web_frontend):
     yield web_frontend.app.test_client()
+
+
+# TODO scope session
+@pytest.fixture
+def test_real_database(cfg_tuple):
+    # Keep function name in sync with web_frontend fixture
+    # TODO name
+    _, configparser_cfg = cfg_tuple
+    db_setup = DbSetup(configparser_cfg)
+    db_setup.create_tables()
+    db_setup.set_table_privileges()
+
+    yield
+
+    db_setup.base.metadata.drop_all(db_setup.engine)
+
+
+# See test/integration/conftest.py
+# TODO scope=function
+# TODO scope=session
+@pytest.fixture
+def real_database(cfg_tuple, test_real_database):
+    _, configparser_cfg = cfg_tuple
+    admin = AdminDbInterface(configparser_cfg, intercom=MockIntercom())
+    common = DbInterfaceCommon(configparser_cfg)
+    backend = BackendDbInterface(configparser_cfg)
+    frontend = FrontEndDbInterface(configparser_cfg)
+    frontend_ed = FrontendEditingDbInterface(configparser_cfg)
+
+    db_interface = DB(common, backend, frontend, frontend_ed, admin)
+
+    try:
+        yield db_interface
+    finally:
+        with db_interface.admin.get_read_write_session() as session:
+            # clear rows from test db between tests
+            for table in reversed(db_interface.admin.base.metadata.sorted_tables):
+                session.execute(table.delete())
+        # clean intercom mock
+        if hasattr(db_interface.admin.intercom, 'deleted_files'):
+            db_interface.admin.intercom.deleted_files.clear()
+
+
+class MockIntercom:
+    def __init__(self):
+        self.deleted_files = []
+
+    def delete_file(self, uid_list: List[str]):
+        self.deleted_files.extend(uid_list)
+
+
+class DB:
+    def __init__(
+        self, common: DbInterfaceCommon, backend: BackendDbInterface, frontend: FrontEndDbInterface,
+        frontend_editing: FrontendEditingDbInterface, admin: AdminDbInterface
+    ):
+        self.common = common
+        self.backend = backend
+        self.frontend = frontend
+        self.frontend_ed = frontend_editing
+        self.admin = admin
