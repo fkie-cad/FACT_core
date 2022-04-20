1cbe5d46f03a93578a78c07f682c1b14d284d823
Marten Ringwelski <git@maringuu.de>
2022-04-20 13:45:59 +0200

    WIP: test/integration/statistic: Use new fixtures

--- src/test/integration/statistic/test_work_load.py
+++ src/test/integration/statistic/test_work_load.py
@@ -4,26 +4,32 @@ from time import time
 
 import pytest
 
-from config import configparser_cfg
 from statistic.work_load import WorkLoadStatistic
 from storage.db_interface_stats import StatsDbViewer
 
 
-@pytest.mark.usefixtures('patch_cfg')
-class TestWorkloadStatistic:
+@pytest.fixture
+def workload_stat(cfg_tuple):
+    _, configparser_cfg = cfg_tuple
+    wls = WorkLoadStatistic(config=configparser_cfg, component='test')
 
-    def setup(self):
-        self.workload_stat = WorkLoadStatistic(config=configparser_cfg, component='test')
-        self.stats_db = StatsDbViewer(config=configparser_cfg)
+    yield wls
 
-    def teardown(self):
-        self.workload_stat.shutdown()
-        gc.collect()
+    wls.shutdown()
+    gc.collect()
 
-    def test_update_workload_statistic(self, db):
-        self.workload_stat.update()
-        result = self.stats_db.get_statistic('test')
-        assert result['name'] == 'test', 'name not set'
-        assert isclose(time(), result['last_update'], abs_tol=0.1), 'timestamp not valid'
-        assert isinstance(result['platform'], dict), 'platform is not a dict'
-        assert isinstance(result['system'], dict), 'system is not a dict'
+
+@pytest.fixture
+def stats_db(cfg_tuple):
+    _, configparser_cfg = cfg_tuple
+    StatsDbViewer(config=configparser_cfg)
+
+
+@pytest.mark.usefixtures('patch_cfg', 'test_real_database')
+def test_update_workload_statistic(workload_stat, stats_db):
+    workload_stat.update()
+    result = stats_db.get_statistic('test')
+    assert result['name'] == 'test', 'name not set'
+    assert isclose(time(), result['last_update'], abs_tol=0.1), 'timestamp not valid'
+    assert isinstance(result['platform'], dict), 'platform is not a dict'
+    assert isinstance(result['system'], dict), 'system is not a dict'
