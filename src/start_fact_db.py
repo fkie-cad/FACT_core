8f080206f4af79ed354d65095cac8a69edad613c
Marten Ringwelski <git@maringuu.de>
2022-06-22 14:40:16 +0200

    Simplify startup logic
    FactBase is removed since it was not really used anyways.
    The calls to `super()` sometimes were missing because the super method did not fit.
    Removing inheritance simplifies the code.
    
    Also the signal handeling is fixed and does not spam the log anymore.

--- src/start_fact_db.py
+++ src/start_fact_db.py
@@ -17,28 +17,48 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 '''
 import logging
+import signal
 import sys
-
-try:
-    from fact_base import FactBase
-except (ImportError, ModuleNotFoundError):
-    sys.exit(1)
 
 from sqlalchemy.exc import SQLAlchemyError
 
 from helperFunctions.program_setup import program_setup
+from statistic.work_load import WorkLoadStatistic
 from storage.db_interface_base import ReadOnlyDbInterface
 
 
-class FactDb(FactBase):
+class FactDb:
     PROGRAM_NAME = 'FACT DB-Service'
     PROGRAM_DESCRIPTION = 'Firmware Analysis and Compare Tool (FACT) DB-Service'
     COMPONENT = 'database'
 
     def __init__(self):
         _, config = program_setup(self.PROGRAM_NAME, self.PROGRAM_DESCRIPTION, self.COMPONENT)
+        self.run = False
         self._check_postgres_connection(config)
-        super().__init__()
+        self.work_load_stat = WorkLoadStatistic(config=config, component=self.COMPONENT)
+
+    def _shutdown_listener(self, signum, _):
+        logging.info(f'Received signal {signum}. Shutting down {self.PROGRAM_NAME}...')
+        self.shutdown()
+
+    def start(self):
+        self.run = True
+
+        signal.signal(signal.SIGINT, signal.SIG_IGN)
+        self.work_load_stat.start()
+
+        signal.signal(signal.SIGINT, self._shutdown_listener)
+        signal.signal(signal.SIGTERM, self._shutdown_listener)
+
+    def shutdown(self):
+        self.run = False
+        self.work_load_stat.shutdown()
+
+    def main(self):
+        self.start()
+        while self.run:
+            signal.pause()
 
     @staticmethod
     def _check_postgres_connection(config):
