1882cde372b90d99b319e16b906282bcb592ff2e
Marten Ringwelski <git@maringuu.de>
2022-06-22 14:16:13 +0200

    WorkLoadStatistic: Rework to be more like AnalysisScheduler etc.

--- src/statistic/work_load.py
+++ src/statistic/work_load.py
@@ -4,7 +4,8 @@ generate workload statistics
 import logging
 import os
 import sys
-from time import time
+from multiprocessing import Process, Value
+from time import sleep, time
 
 import distro
 import psutil
@@ -14,18 +15,36 @@ from version import __VERSION__
 
 
 class WorkLoadStatistic:
-    def __init__(self, config, component):
+
+    def __init__(self, config, component, analysis_workload_fn=None, comparison_workload_fn=None, unpacking_workload_fn=None):
         self.config = config
         self.component = component
         self.db = StatsUpdateDbInterface(config=self.config)
         self.platform_information = self._get_platform_information()
+        self._analysis_workload_fn = analysis_workload_fn if analysis_workload_fn else lambda: None
+        self._comparison_workload_fn = comparison_workload_fn if comparison_workload_fn else lambda: None
+        self._unpacking_workload_fn = unpacking_workload_fn if unpacking_workload_fn else lambda: None
+
+        self.process = Process(target=self._entrypoint)
+
+        self.stop_condition = Value('i', 1)
+
+    def start(self):
+        self.stop_condition.value = 0
+        self.process.start()
         logging.debug(f'{self.component}: Online')
 
     def shutdown(self):
         logging.debug(f'{self.component}: shutting down -> set offline message')
+        self.stop_condition.value = 1
         self.db.update_statistic(self.component, {'status': 'offline', 'last_update': time()})
 
-    def update(self, unpacking_workload=None, analysis_workload=None, compare_workload=None):
+    def _entrypoint(self):
+        while self.stop_condition.value == 0:
+            self.update()
+            sleep(5)
+
+    def update(self):
         stats = {
             'name': self.component,
             'status': 'online',
@@ -33,12 +52,16 @@ class WorkLoadStatistic:
             'system': self._get_system_information(),
             'platform': self.platform_information,
         }
-        if unpacking_workload:
-            stats['unpacking'] = unpacking_workload
-        if analysis_workload:
-            stats['analysis'] = analysis_workload
-        if compare_workload:
-            stats['compare'] = compare_workload
+        analyis_wl = self._analysis_workload_fn()
+        if analyis_wl:
+            stats['analysis'] = analyis_wl
+        comparison_wl = self._comparison_workload_fn()
+        if comparison_wl:
+            stats['compare'] = comparison_wl
+        unpacking_wl = self._unpacking_workload_fn()
+        if unpacking_wl:
+            stats['unpacking'] = unpacking_wl
+
         self.db.update_statistic(self.component, stats)
 
     def _get_system_information(self):
