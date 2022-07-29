5a38c2f56a3906808839904d1c6f6eed7bf278b8
Marten Ringwelski <git@maringuu.de>
2022-07-29 11:53:36 +0200

    test/unit/compare: Remove unittest

--- src/test/unit/compare/test_compare.py
+++ src/test/unit/compare/test_compare.py
@@ -1,7 +1,5 @@
+# pylint: disable=no-self-use
 # pylint: disable=wrong-import-order,protected-access,no-self-use,unused-argument
-import gc
-import unittest
-
 import pytest
 
 from compare.compare import Compare
@@ -40,33 +38,31 @@ class MockDbInterface:
         return {}
 
 
-class TestCompare(unittest.TestCase):
-    def setUp(self):
-        self.fw_one = create_test_firmware(device_name='dev_1', all_files_included_set=True)
-        self.fw_one.processed_analysis['file_hashes'] = {'ssdeep': get_ssdeep(self.fw_one.binary)}
-        self.fw_two = create_test_firmware(
-            device_name='dev_2', bin_path='container/test.7z', all_files_included_set=True
-        )
-        self.fw_two.processed_analysis['file_hashes'] = {'ssdeep': get_ssdeep(self.fw_two.binary)}
-        self.compare_system = Compare(db_interface=MockDbInterface())
+@pytest.fixture
+def compare_system():
+    return Compare(db_interface=MockDbInterface())
 
-    def tearDown(self):
-        gc.collect()
 
-    def test_compare_objects(self):
-        result = self.compare_system.compare_objects([self.fw_one, self.fw_two])
+class TestCompare:
+    fw_one = create_test_firmware(device_name='dev_1', all_files_included_set=True)
+    fw_one.processed_analysis['file_hashes'] = {'ssdeep': get_ssdeep(fw_one.binary)}
+    fw_two = create_test_firmware(device_name='dev_2', bin_path='container/test.7z', all_files_included_set=True)
+    fw_two.processed_analysis['file_hashes'] = {'ssdeep': get_ssdeep(fw_two.binary)}
+
+    def test_compare_objects(self, compare_system):
+        result = compare_system.compare_objects([self.fw_one, self.fw_two])
         assert isinstance(result, dict), 'Result is not a dict'
         assert 'general' in result, 'general part is missing'
         assert isinstance(result['general'], dict), 'general part is not a dict'
         assert 'plugins' in result, 'plugin part is missing'
         assert isinstance(result['plugins'], dict), 'plugins part is not a dict'
 
-    def test_compare_error_none_existing_fo(self):
+    def test_compare_error_none_existing_fo(self, compare_system):
         with pytest.raises(AttributeError):
-            self.compare_system.compare(['error'])
+            compare_system.compare(['error'])
 
-    def test_create_general_section_dict(self):
-        result = self.compare_system._create_general_section_dict([self.fw_one, self.fw_two])
+    def test_create_general_section_dict(self, compare_system):
+        result = compare_system._create_general_section_dict([self.fw_one, self.fw_two])
         assert isinstance(result, dict), 'result is not a dict'
         assert result['device_name'][self.fw_one.uid] == 'dev_1'
         assert result['device_name'][self.fw_two.uid] == 'dev_2'
@@ -77,6 +73,6 @@ class TestCompare(unittest.TestCase):
         assert result['size'][self.fw_one.uid] == len(self.fw_one.binary)
         assert result['virtual_file_path'][self.fw_one.uid] == [self.fw_one.uid]
 
-    def test_plugin_system(self):
-        assert len(self.compare_system.compare_plugins) > 0, 'no compare plugin found'
-        assert 'File_Coverage' in self.compare_system.compare_plugins, 'File Coverage module not found'
+    def test_plugin_system(self, compare_system):
+        assert len(compare_system.compare_plugins) > 0, 'no compare plugin found'
+        assert 'File_Coverage' in compare_system.compare_plugins, 'File Coverage module not found'
