69fd7b1b6300aed241623e31a764968be04024ca
Marten Ringwelski <git@maringuu.de>
2022-07-29 14:11:25 +0200

    WIP: This has to be before all commits in integration. Update integration/conftest.py

--- src/test/integration/conftest.py
+++ src/test/integration/conftest.py
@@ -1,70 +1,54 @@
-from typing import List
-
 import pytest
-
-from storage.db_connection import ReadOnlyConnection, ReadWriteConnection
-from storage.db_interface_admin import AdminDbInterface
-from storage.db_interface_backend import BackendDbInterface
-from storage.db_interface_common import DbInterfaceCommon
-from storage.db_interface_comparison import ComparisonDbInterface
-from storage.db_interface_frontend import FrontEndDbInterface
-from storage.db_interface_frontend_editing import FrontendEditingDbInterface
-from test.common_helper import clear_test_tables  # pylint: disable=wrong-import-order
-from test.common_helper import get_config_for_testing, setup_test_tables  # pylint: disable=wrong-import-order
+from scheduler.unpacking_scheduler import UnpackingScheduler
+from tempfile import TemporaryDirectory
+from multiprocessing import Queue
+from pathlib import Path
 
 
-class DB:
-    def __init__(
-        self, common: DbInterfaceCommon, backend: BackendDbInterface, frontend: FrontEndDbInterface,
-        frontend_editing: FrontendEditingDbInterface, admin: AdminDbInterface
-    ):
-        self.common = common
-        self.backend = backend
-        self.frontend = frontend
-        self.frontend_ed = frontend_editing
-        self.admin = admin
+class MockFSOrganizer:
+    def __init__(self, *_, **__):
+        self._data_folder = TemporaryDirectory()
+
+    def store_file(self, file_object):
+        Path(self._data_folder.name, file_object.uid).write_bytes(file_object.binary)
+
+    def delete_file(self, uid):
+        file_path = Path(self._data_folder.name, uid)
+        if file_path.is_file():
+            file_path.unlink()
+
+    def generate_path(self, uid):
+        return str(Path(self._data_folder.name, uid))
+
+    def __del__(self):
+        self._data_folder.cleanup()
 
 
-@pytest.fixture(scope='session')
-def db_interface():
-    config = get_config_for_testing()
-    admin = AdminDbInterface(config, intercom=MockIntercom())
-    setup_test_tables(config)
-    ro_connection = ReadOnlyConnection(config)
-    rw_connection = ReadWriteConnection(config)
-    common = DbInterfaceCommon(config, connection=ro_connection)
-    backend = BackendDbInterface(config, connection=rw_connection)
-    frontend = FrontEndDbInterface(config, connection=ro_connection)
-    frontend_ed = FrontendEditingDbInterface(config, connection=rw_connection)
-    try:
-        yield DB(common, backend, frontend, frontend_ed, admin)
-    finally:
-        clear_test_tables(config)
+# TODO rename
+@pytest.fixture
+def unpacking_queue():
+    """TODO
+    See also analysis_queue.
+    """
+    q = Queue()
+    yield q
+    q.close()
 
 
-@pytest.fixture(scope='function')
-def db(db_interface):  # pylint: disable=invalid-name,redefined-outer-name
-    try:
-        yield db_interface
-    finally:
-        with db_interface.admin.get_read_write_session() as session:
-            # clear rows from test db between tests
-            for table in reversed(db_interface.admin.connection.base.metadata.sorted_tables):
-                session.execute(table.delete())
-        # clean intercom mock
-        if hasattr(db_interface.admin.intercom, 'deleted_files'):
-            db_interface.admin.intercom.deleted_files.clear()
+@pytest.fixture
+def unpacking_scheduler(cfg_tuple, unpacking_lock_manager, analysis_scheduler, unpacking_queue) -> UnpackingScheduler:
+    def post_unpack_cb(fw):
+        unpacking_queue.put(fw)
+        return analysis_scheduler.start_analysis_of_object()
 
+    _, configparser_cfg = cfg_tuple
+    sched = UnpackingScheduler(
+        config=configparser_cfg,
+        fs_organizer=MockFSOrganizer(),
+        post_unpack=post_unpack_cb,
+        unpacking_locks=unpacking_lock_manager,
+    )
 
-class MockIntercom:
-    def __init__(self):
-        self.deleted_files = []
+    yield sched
 
-    def delete_file(self, uid_list: List[str]):
-        self.deleted_files.extend(uid_list)
-
-
-@pytest.fixture()
-def comp_db():
-    config = get_config_for_testing()
-    yield ComparisonDbInterface(config)
+    sched.shutdown()
