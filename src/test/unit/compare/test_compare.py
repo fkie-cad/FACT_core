a6bf10f117d589ce66983de98b423ee60b159cc7
Marten Ringwelski <git@maringuu.de>
2022-07-29 11:53:36 +0200

    test/unit/compare: Remove unittest

--- src/test/unit/compare/test_compare.py
+++ src/test/unit/compare/test_compare.py
@@ -1,7 +1,4 @@
 # pylint: disable=wrong-import-order,protected-access,no-self-use,unused-argument
-import gc
-import unittest
-
 import pytest
 
 from compare.compare import Compare
@@ -40,43 +37,44 @@ class MockDbInterface:
         return {}
 
 
-class TestCompare(unittest.TestCase):
-    def setUp(self):
-        self.fw_one = create_test_firmware(device_name='dev_1', all_files_included_set=True)
-        self.fw_one.processed_analysis['file_hashes'] = {'ssdeep': get_ssdeep(self.fw_one.binary)}
-        self.fw_two = create_test_firmware(
-            device_name='dev_2', bin_path='container/test.7z', all_files_included_set=True
-        )
-        self.fw_two.processed_analysis['file_hashes'] = {'ssdeep': get_ssdeep(self.fw_two.binary)}
-        self.compare_system = Compare(db_interface=MockDbInterface())
+@pytest.fixture
+def compare_system():
+    return Compare(db_interface=MockDbInterface())
 
-    def tearDown(self):
-        gc.collect()
 
-    def test_compare_objects(self):
-        result = self.compare_system.compare_objects([self.fw_one, self.fw_two])
-        assert isinstance(result, dict), 'Result is not a dict'
-        assert 'general' in result, 'general part is missing'
-        assert isinstance(result['general'], dict), 'general part is not a dict'
-        assert 'plugins' in result, 'plugin part is missing'
-        assert isinstance(result['plugins'], dict), 'plugins part is not a dict'
+fw_one = create_test_firmware(device_name='dev_1', all_files_included_set=True)
+fw_one.processed_analysis['file_hashes'] = {'ssdeep': get_ssdeep(fw_one.binary)}
+fw_two = create_test_firmware(device_name='dev_2', bin_path='container/test.7z', all_files_included_set=True)
+fw_two.processed_analysis['file_hashes'] = {'ssdeep': get_ssdeep(fw_two.binary)}
 
-    def test_compare_error_none_existing_fo(self):
-        with pytest.raises(AttributeError):
-            self.compare_system.compare(['error'])
 
-    def test_create_general_section_dict(self):
-        result = self.compare_system._create_general_section_dict([self.fw_one, self.fw_two])
-        assert isinstance(result, dict), 'result is not a dict'
-        assert result['device_name'][self.fw_one.uid] == 'dev_1'
-        assert result['device_name'][self.fw_two.uid] == 'dev_2'
-        assert result['device_class'][self.fw_one.uid] == 'Router'
-        assert result['vendor'][self.fw_one.uid] == 'test_vendor'
-        assert result['version'][self.fw_one.uid] == '0.1'
-        assert result['release_date'][self.fw_one.uid] == '1970-01-01'
-        assert result['size'][self.fw_one.uid] == len(self.fw_one.binary)
-        assert result['virtual_file_path'][self.fw_one.uid] == [self.fw_one.uid]
+def test_compare_objects(compare_system):
+    result = compare_system.compare_objects([fw_one, fw_two])
+    assert isinstance(result, dict), 'Result is not a dict'
+    assert 'general' in result, 'general part is missing'
+    assert isinstance(result['general'], dict), 'general part is not a dict'
+    assert 'plugins' in result, 'plugin part is missing'
+    assert isinstance(result['plugins'], dict), 'plugins part is not a dict'
 
-    def test_plugin_system(self):
-        assert len(self.compare_system.compare_plugins) > 0, 'no compare plugin found'
-        assert 'File_Coverage' in self.compare_system.compare_plugins, 'File Coverage module not found'
+
+def test_compare_error_none_existing_fo(compare_system):
+    with pytest.raises(AttributeError):
+        compare_system.compare(['error'])
+
+
+def test_create_general_section_dict(compare_system):
+    result = compare_system._create_general_section_dict([fw_one, fw_two])
+    assert isinstance(result, dict), 'result is not a dict'
+    assert result['device_name'][fw_one.uid] == 'dev_1'
+    assert result['device_name'][fw_two.uid] == 'dev_2'
+    assert result['device_class'][fw_one.uid] == 'Router'
+    assert result['vendor'][fw_one.uid] == 'test_vendor'
+    assert result['version'][fw_one.uid] == '0.1'
+    assert result['release_date'][fw_one.uid] == '1970-01-01'
+    assert result['size'][fw_one.uid] == len(fw_one.binary)
+    assert result['virtual_file_path'][fw_one.uid] == [fw_one.uid]
+
+
+def test_plugin_system(compare_system):
+    assert len(compare_system.compare_plugins) > 0, 'no compare plugin found'
+    assert 'File_Coverage' in compare_system.compare_plugins, 'File Coverage module not found'
