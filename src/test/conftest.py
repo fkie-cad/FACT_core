859ecd3332ae66543b4c74b1a9b2a4c693ad158b
Marten Ringwelski <git@maringuu.de>
2022-11-10 11:46:11 +0100

    test/conftest.py: Remove TODO

--- src/test/conftest.py
+++ src/test/conftest.py
@@ -4,10 +4,6 @@ from storage.db_setup import DbSetup
 from test.common_helper import clear_test_tables, setup_test_tables
 
 
-# TODO Consider changing this function to have a higher scope that function.
-# Currently this is not possible because the cfg_tuple fixture must have function
-# scope and this fixture needs it.
-# TODO This function should only be used by integration tests -> Move it to to integration/conftest.py
 @pytest.fixture
 def create_tables():
     """Creates the tables that backend needs.
