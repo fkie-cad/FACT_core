be389d1f082fdc6d3c15f608c16926aa1c6c2ee1
Marten Ringwelski <git@maringuu.de>
2022-11-10 11:40:40 +0100

    helperFunctions/config: Remove get_temp_dir_path

--- src/test/unit/helperFunctions/test_config.py
+++ src/test/unit/helperFunctions/test_config.py
@@ -1,12 +1,9 @@
-import logging
 import os
-from pathlib import Path
 
 import pytest
 
 from config import parse_comma_separated_list
-from helperFunctions.config_deprecated import get_config_dir, get_temp_dir_path
-from test.common_helper import get_test_data_dir
+from helperFunctions.config_deprecated import get_config_dir
 
 
 def test_get_config_dir():
@@ -34,12 +31,3 @@ class MockConfig:
 
     def get(self, *_, **__):
         return self.dir
-
-
-def test_get_temp_dir_path(caplog):
-    assert get_temp_dir_path() == '/tmp'
-    assert get_temp_dir_path(MockConfig(get_test_data_dir())) == get_test_data_dir()
-    not_a_dir = str(Path(get_test_data_dir()) / '__init__.py')
-    with caplog.at_level(logging.WARNING):
-        assert get_temp_dir_path(MockConfig(not_a_dir)) == '/tmp'
-        assert 'TempDir path does not exist and could not be created' in caplog.messages[0]
