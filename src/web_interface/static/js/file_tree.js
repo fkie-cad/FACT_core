92a8d6457aa5f9d81819a573d5f44bb7d2998eed
Jörg Stucke <joerg.stucke@fkie.fraunhofer.de>
2022-11-25 16:57:10 +0100

    file tree config search fix

--- src/web_interface/static/js/file_tree.js
+++ src/web_interface/static/js/file_tree.js
@@ -1,22 +1,27 @@
 function create_file_tree(div_element, search, url_function) {
     const config = {
         "core": {
-            'data': {
+            "data": {
                 'url': url_function,
             },
-            'themes': {
-                'name': 'proton',
-                'responsive': true,
+            "themes": {
+                "name": "proton",
+                "responsive": true,
             },
         },
-        "search": {
+        "plugins": ["sort", "themes"],
+    };
+    if (search) {
+        config.search = {
             "case_insensitive": true,
             "show_only_matches": true,
-        },
-        "plugins": search ? ["sort", "themes", "search"] : ["sort", "themes"],
-    };
+        };
+        config.plugins.push("search");
+    }
     // generate links to the analysis page
-    div_element.on("activate_node.jstree", (e,data) => { window.location.href = data.node.a_attr.href; });
+    div_element.on("activate_node.jstree", (e, data) => {
+        window.location.href = data.node.a_attr.href;
+    });
     // generate file tree
     div_element.jstree(config);
     return div_element;
