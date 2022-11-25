bf4a51cfe1fb140a00f026d83e5d865faaffa133
Jörg Stucke <joerg.stucke@fkie.fraunhofer.de>
2022-11-25 16:48:22 +0100

    moved common parts of analysis and comparison file trees into common module

--- src/web_interface/static/js/show_analysis_fileTreeAjax.js
+++ src/web_interface/static/js/show_analysis_fileTreeAjax.js
@@ -1,36 +1,3 @@
-$('#fileTreeAjax')
-    // generate links to the analysis page
-    .on("activate_node.jstree", function(e,data){window.location.href = data.node.a_attr.href;})
-    // generate file tree
-    .jstree({
-        "core" : {
-            'data' : {
-                'url' : function (node) {
-                    return node.id === '#' ?
-                        `/ajax_root/${uid}/${root_uid}` : "/ajax_tree/" + node.data.uid + `/${root_uid}`;
-                }
-            },
-            'themes': {
-                'name': 'proton',
-                'responsive': true,
-            },
-        },
-        "search": {
-            "case_insensitive": true,
-            "show_only_matches": true,
-        },
-        "plugins" : [ "sort", "themes", "search" ]
-    }).on('search.jstree', function (nodes, str, res) {
-        if (str.nodes.length === 0) {
-            $("#fileTreeAjax").jstree(true).hide_all();
-            $("#fileTreeSearch").addClass("is-invalid");
-            $("#fileTreeSearchFeedback").show();
-        } else {
-            $("#fileTreeSearch").removeClass("is-invalid");
-            $("#fileTreeSearchFeedback").hide();
-        }
-    });
-
 // source: https://stackoverflow.com/questions/1909441/how-to-delay-the-keyup-handler-until-the-user-stops-typing
 function delay(fn, ms) {
     let timer = 0;
@@ -41,10 +8,30 @@ function delay(fn, ms) {
 }
 
 $(document).ready(function () {
+    let fileTree = create_file_tree(
+        $('#fileTreeAjax'),
+        true,
+        (node) => node.id === '#' ?
+            // root node url
+            `/ajax_root/${uid}/${root_uid}` :
+            // inner node url
+            `/ajax_tree/${node.data.uid}/${root_uid}`
+    );
+    // start file tree search on keyup event
     $('#fileTreeSearch').keyup(delay(function () {
-        let fileTree = $('#fileTreeAjax');
         fileTree.jstree(true).show_all();
         fileTree.jstree('search', $(this).val());
         $('#fileTreeAjax .jstree-hidden').hide();
     }, 1000));
+    // display error if file tree search yielded no results
+    fileTree.on('search.jstree', (nodes, str, res) => {
+        if (str.nodes.length === 0) {
+            $("#fileTreeAjax").jstree(true).hide_all();
+            $("#fileTreeSearch").addClass("is-invalid");
+            $("#fileTreeSearchFeedback").show();
+        } else {
+            $("#fileTreeSearch").removeClass("is-invalid");
+            $("#fileTreeSearchFeedback").hide();
+        }
+    });
 });
