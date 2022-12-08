ff55f572c9bf1d4b3e734974513ae762d7ffeaf4
Marten Ringwelski <git@maringuu.de>
2022-12-08 12:58:48 +0100

    acceptance/test_binary_search: Use new fixtures

--- src/test/acceptance/test_binary_search.py
+++ src/test/acceptance/test_binary_search.py
@@ -1,41 +1,52 @@
-from test.acceptance.base_full_start import TestAcceptanceBaseFullStart
+import pytest
+from test.acceptance.conftest import SchedulerAcceptanceTestConfig, upload_test_firmware
 
 
-class TestAcceptanceBinarySearch(TestAcceptanceBaseFullStart):
+query = {
+    'file': None,
+    'textarea': 'rule test_file_string {strings: $a = "This is the second test file" condition: $a }',
+}
 
-    query = {
-        'file': None,
-        'textarea': 'rule test_file_string {strings: $a = "This is the second test file" condition: $a }',
-    }
 
-    def _query_page_get(self):
-        rv = self.test_client.get('/database/binary_search')
-        assert b'<h3 class="mb-3">Binary Pattern Search</h3>' in rv.data
+def _query_page_get(test_client):
+    rv = test_client.get('/database/binary_search')
+    assert b'<h3 class="mb-3">Binary Pattern Search</h3>' in rv.data
 
-    def _query_page_post_file_query(self):
-        rv = self.test_client.post(
-            '/database/binary_search', content_type='multipart/form-data', data=self.query, follow_redirects=True
-        )
-        assert b'testfile2' in rv.data
 
-    def _query_page_post_firmware_query(self):
-        rv = self.test_client.post(
-            '/database/binary_search',
-            content_type='multipart/form-data',
-            data={**self.query, 'only_firmware': 'True'},
-            follow_redirects=True,
-        )
-        assert self.test_fw_a.name.encode() in rv.data
-        assert b'testfile2' not in rv.data
+def _query_page_post_file_query(test_client):
+    rv = test_client.post(
+        '/database/binary_search', content_type='multipart/form-data', data=query, follow_redirects=True
+    )
+    assert b'testfile2' in rv.data
 
-    def _get_without_request_id(self):
-        rv = self.test_client.get('/database/binary_search_results')
-        assert b'No request ID found' in rv.data
 
-    def test_binary_search(self):
-        self._query_page_get()
-        self.upload_test_firmware(self.test_fw_a)
-        self.analysis_finished_event.wait(timeout=15)
-        self._query_page_post_file_query()
-        self._query_page_post_firmware_query()
-        self._get_without_request_id()
+def _query_page_post_firmware_query(test_client, test_fw_a):
+    rv = test_client.post(
+        '/database/binary_search',
+        content_type='multipart/form-data',
+        data={**query, 'only_firmware': 'True'},
+        follow_redirects=True,
+    )
+    assert test_fw_a.name.encode() in rv.data
+    assert b'testfile2' not in rv.data
+
+
+def _get_without_request_id(test_client):
+    rv = test_client.get('/database/binary_search_results')
+    assert b'No request ID found' in rv.data
+
+
+@pytest.mark.skip(reason="TODO should work")
+@pytest.mark.SchedulerTestConfig(
+    SchedulerAcceptanceTestConfig(
+        # 4 Files and 2 plugins
+        items_to_analyze=4*2,
+    ),
+)
+def test_binary_search(test_client, test_fw_a, analysis_finished_event):
+    _query_page_get(test_client)
+    upload_test_firmware(test_client, test_fw_a)
+    analysis_finished_event.wait(timeout=15)
+    _query_page_post_file_query(test_client)
+    _query_page_post_firmware_query(test_client)
+    _get_without_request_id(test_client)
