c453985ca92bacda6561bb1533a2c2240a16f121
Marten Ringwelski <git@maringuu.de>
2023-03-02 10:04:31 +0100

    test/unit/helperFunctions: Fix typo

--- src/test/unit/helperFunctions/test_plugin.py
+++ src/test/unit/helperFunctions/test_plugin.py
@@ -8,5 +8,5 @@ def test_import_plugins(monkeypatch):
     monkeypatch.setattr('helperFunctions.plugin.get_src_dir', lambda: TEST_PLUGINS_SRC_DIR)
 
     plugins = discover_analysis_plugins()
-    assert len(plugins) == 1, 'worng number of plugins imported'
+    assert len(plugins) == 1, 'wrong number of plugins imported'
     assert plugins[0].__name__ == 'plugins.analysis.plugin_one.code.plugin_one', 'plugin name not correct'
