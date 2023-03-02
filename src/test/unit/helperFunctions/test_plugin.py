e837c78e825aefc4c57fe4eaece26c24cb84ddcd
Marten Ringwelski <git@maringuu.de>
2023-03-02 10:27:35 +0100

    tests: Test plugin discovery with failing plugins

--- src/test/unit/helperFunctions/test_plugin.py
+++ src/test/unit/helperFunctions/test_plugin.py
@@ -7,6 +7,7 @@ TEST_PLUGINS_SRC_DIR = f'{get_src_dir()}/test/data/plugin_system'
 def test_import_plugins(monkeypatch):
     monkeypatch.setattr('helperFunctions.plugin.get_src_dir', lambda: TEST_PLUGINS_SRC_DIR)
 
-    plugins = discover_analysis_plugins()
-    assert len(plugins) == 1, 'wrong number of plugins imported'
-    assert plugins[0].__name__ == 'plugins.analysis.plugin_one.code.plugin_one', 'plugin name not correct'
+    plugins = sorted(discover_analysis_plugins(), key=lambda k: k.__name__)
+    # "plugin_one" and "crashes_during_instantiation"
+    assert len(plugins) == 2, 'wrong number of plugins imported'
+    assert plugins[1].__name__ == 'plugins.analysis.plugin_one.code.plugin_one', 'plugin name not correct'
