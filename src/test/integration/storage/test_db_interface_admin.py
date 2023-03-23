a0c059a647be56b7895304f0cf42b0e6c33f09bf
Jörg Stucke <joerg.stucke@fkie.fraunhofer.de>
2023-03-23 17:47:04 +0100

    added test for delete fw

--- src/test/integration/storage/test_db_interface_admin.py
+++ src/test/integration/storage/test_db_interface_admin.py
@@ -1,4 +1,4 @@
-from ...common_helper import create_test_firmware
+from ...common_helper import create_test_file_object, create_test_firmware
 from .helper import TEST_FW, create_fw_with_child_fo, create_fw_with_parent_and_child
 
 
@@ -23,51 +23,14 @@ def test_delete_cascade(db):
     assert db.common.exists(fo.uid) is False, 'deletion should be cascaded to child objects'
 
 
-def test_remove_vp_no_other_fw(db):
-    fo, fw = create_fw_with_child_fo()
-    db.backend.insert_object(fw)
-    db.backend.insert_object(fo)
-
-    with db.admin.get_read_write_session() as session:
-        removed_vps, deleted_uids = db.admin._remove_virtual_path_entries(
-            fw.uid, fo.uid, session
-        )  # pylint: disable=protected-access
-
-    assert removed_vps == 0
-    assert deleted_uids == {fo.uid}
-
-
-def test_remove_vp_other_fw(db):
-    fo, fw = create_fw_with_child_fo()
-    fo.virtual_file_path.update({'some_other_fw_uid': ['some_vfp']})
-    db.backend.insert_object(fw)
-    db.backend.insert_object(fo)
-
-    with db.admin.get_read_write_session() as session:
-        removed_vps, deleted_files = db.admin._remove_virtual_path_entries(
-            fw.uid, fo.uid, session
-        )  # pylint: disable=protected-access
-    fo_entry = db.common.get_object(fo.uid)
-
-    assert fo_entry is not None
-    assert removed_vps == 1
-    assert deleted_files == set()
-    assert fw.uid not in fo_entry.virtual_file_path
-
-
 def test_delete_firmware(db):
     fw, parent, child = create_fw_with_parent_and_child()
     db.backend.insert_object(fw)
     db.backend.insert_object(parent)
     db.backend.insert_object(child)
 
-    removed_vps, deleted_files = db.admin.delete_firmware(fw.uid)
+    db.admin.delete_firmware(fw.uid)
 
-    assert removed_vps == 0
-    assert deleted_files == 3
-    assert child.uid in db.admin.intercom.deleted_files
-    assert parent.uid in db.admin.intercom.deleted_files
-    assert fw.uid in db.admin.intercom.deleted_files
     assert db.common.exists(fw.uid) is False
     assert db.common.exists(parent.uid) is False, 'should have been deleted by cascade'
     assert db.common.exists(child.uid) is False, 'should have been deleted by cascade'
@@ -78,6 +41,7 @@ def test_delete_but_fo_is_in_fw(db):
     fw2 = create_test_firmware()
     fw2.uid = 'fw2_uid'
     fo.parents.append(fw2.uid)
+    fo.parent_firmware_uids.add(fw2.uid)
     fo.virtual_file_path.update({fw2.uid: [f'|{fw2.uid}|/some/path']})
     db.backend.insert_object(fw)
     db.backend.insert_object(fw2)
@@ -94,3 +58,26 @@ def test_delete_but_fo_is_in_fw(db):
     assert fw.uid in db.admin.intercom.deleted_files
     assert db.common.exists(fw.uid) is False
     assert db.common.exists(fo.uid) is True, 'should have been spared by cascade delete because it is in another FW'
+    fo_db = db.common.get_object(fo.uid)
+    assert fo_db.virtual_file_path == {'fw2_uid': ['|fw2_uid|/some/path']}, 'entry of fw should be deleted from vfp'
+
+
+def test_same_fw_multiple_parents(db):
+    fw, parent, child = create_fw_with_parent_and_child()
+    db.backend.insert_object(fw)
+    db.backend.insert_object(parent)
+    parent2 = create_test_file_object()
+    parent2.uid = 'parent2'
+    parent2.parents.append(fw.uid)
+    parent2.parent_firmware_uids.add(fw.uid)
+    db.backend.insert_object(parent2)
+    # child has multiple parents but all in the same FW -> should still be deleted by cascade
+    child.parents.append(parent2.uid)
+    db.backend.insert_object(child)
+
+    db.admin.delete_firmware(fw.uid)
+
+    assert db.common.exists(fw.uid) is False
+    assert db.common.exists(parent.uid) is False, 'should have been deleted by cascade'
+    assert db.common.exists(parent2.uid) is False, 'should have been deleted by cascade'
+    assert db.common.exists(child.uid) is False, 'should have been deleted by cascade'
