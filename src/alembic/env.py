bfe102ef672d657813a072d1cff6f0d1941aa235
Jörg Stucke <joerg.stucke@fkie.fraunhofer.de>
2023-03-27 10:36:02 +0200

    clean up alembic environment

--- src/alembic/env.py
+++ src/alembic/env.py
@@ -6,41 +6,18 @@ from config import load
 from storage.db_connection import AdminConnection
 from storage.schema import Base
 
-# this is the Alembic Config object, which provides
-# access to the values within the .ini file in use.
 config = context.config
-
-# Interpret the config file for Python logging.
-# This line sets up loggers basically.
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
-# add your model's MetaData object here
-# for 'autogenerate' support
-# from myapp import mymodel
-# target_metadata = mymodel.Base.metadata
-
-# other values from the config, defined by the needs of env.py,
-# can be acquired:
-# my_important_option = config.get_main_option("my_important_option")
-# ... etc.
-
 load()
-# connection = AdminConnection()
-# config.set_main_option('sqlalchemy.url', connection.engine_url.render_as_string(hide_password=False))
-# engine = connection.engine
 
 
 def run_migrations_online() -> None:
-    """Run migrations in 'online' mode.
-
-    In this scenario we need to create an Engine
-    and associate a connection with the context.
-
     """
-    connection = AdminConnection()
-
-    with connection.engine.connect() as connection:
+    Run migrations in 'online' mode: create an Engine and associate a connection with the context.
+    """
+    with AdminConnection().engine.connect() as connection:
         context.configure(
             connection=connection,
             target_metadata=Base.metadata,
@@ -50,4 +27,5 @@ def run_migrations_online() -> None:
             context.run_migrations()
 
 
-run_migrations_online()
+if __name__ == '__main__':
+    run_migrations_online()
