961d099934097befd0331e89ba34b2866edadcb9
Jörg Stucke <joerg.stucke@fkie.fraunhofer.de>
2023-04-04 16:44:19 +0200

    replaced advanced search input with text area and added validation
    - the validation also fixes trailing commas
    - also removed old unused angular code

--- src/web_interface/static/js/advanced_search.js
+++ src/web_interface/static/js/advanced_search.js
@@ -5,15 +5,35 @@ function hide_checkbox() {
         $("#inverted_div").hide();
     }
 }
-var app = angular.module('myApp', []);
 
-app.config(['$interpolateProvider', function($interpolateProvider) {
-    $interpolateProvider.startSymbol('{a');
-    $interpolateProvider.endSymbol('a}');
-}]);
+function validateQuery() {
+    const queryInput = document.getElementById('advanced_search');
+    const feedback = document.getElementById('advanced_search_feedback');
+    const trailing_comma_regex = /\,(?=\s*?[\}\]])/g;
+    try {
+        // try to parse advanced search query as JSON to see if it is valid
+        // also remove trailing commas which are not legal in JSON
+        const json = JSON.parse(queryInput.value.replace(trailing_comma_regex, ''));
+        // format JSON query
+        queryInput.value = JSON.stringify(json, undefined, 4);
+        queryInput.classList.remove("is-invalid");
+        queryInput.classList.add("is-valid");
+        feedback.innerText = "";
+        // adjust text area height to fit input
+        queryInput.style.height = 0;
+        queryInput.style.height = `${queryInput.scrollHeight + 1}px`;
+    } catch (err) {
+        queryInput.classList.remove("is-valid");
+        queryInput.classList.add("is-invalid");
+        // remove the start to only leave the error message
+        feedback.innerText = String(err).slice(25);
+        return false;
+    }
+    return true;
+}
 
-app.controller('formCtrl', function($scope) {
-    $scope.firstname = "complete";
-    $scope.data = database_structure;
-    $scope.plugins = Object.keys($scope.data);
-});
+function submitSearch(form) {
+    if (validateQuery()) {
+        form.submit();
+    }
+}
