6f3cd87b1e4aeace1b4a9f338de1ddb9729211fc
Marten Ringwelski <git@maringuu.de>
2023-04-17 16:18:44 +0200

    plugins/crypto_hints: Use analysis.PluginV0

--- src/plugins/analysis/crypto_hints/code/crypto_hints.py
+++ src/plugins/analysis/crypto_hints/code/crypto_hints.py
@@ -1,10 +1,33 @@
-from analysis.YaraPluginBase import YaraBasePlugin
+import io
+
+import pydantic
+
+import plugins.analysis.compat
+from plugins import analysis
+from plugins.analysis import addons
 
 
-class AnalysisPlugin(YaraBasePlugin):
+class AnalysisPlugin(analysis.PluginV0, analysis.compat.AnalysisBasePluginAdapterMixin):
+    class Schema(pydantic.BaseModel):
+        matches: list[dict]
 
-    NAME = 'crypto_hints'
-    DESCRIPTION = 'find indicators of specific crypto algorithms'
-    DEPENDENCIES = []
-    VERSION = '0.1.1'
-    FILE = __file__
+    def __init__(self):
+        metadata = analysis.PluginV0.MetaData(
+            name='crypto_hints',
+            description='find indicators of specific crypto algorithms',
+            version='0.2.0',
+            Schema=AnalysisPlugin.Schema,
+        )
+        super().__init__(metadata=metadata)
+
+        self._yara = addons.Yara(plugin=self)
+
+    def summarize(self, result):
+        del result
+        return []
+
+    def analyze(self, file_handle: io.FileIO, virtual_file_path: str, analyses: dict) -> Schema:
+        del virtual_file_path, analyses
+        return AnalysisPlugin.Schema(
+            matches=[analysis.compat.yara_match_to_dict(m) for m in self._yara.match(file_handle)],
+        )
