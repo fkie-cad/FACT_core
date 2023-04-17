a5baa8d32e82f5d380d808b7093dc544f0a139cb
Marten Ringwelski <git@maringuu.de>
2023-04-17 09:00:45 +0200

    Add example plugin for `analysis.PluginV0`

--- src/scheduler/analysis.py
+++ src/scheduler/analysis.py
@@ -599,7 +599,7 @@ class AnalysisScheduler:  # pylint: disable=too-many-instance-attributes
 
     @staticmethod
     def _remove_unwanted_plugins(list_of_plugins):
-        defaults = ['dummy_plugin_for_testing_only']
+        defaults = ['dummy_plugin_for_testing_only', 'ExamplePlugin']
         for plugin in defaults:
             list_of_plugins.remove(plugin)
         return list_of_plugins
