bf0c0df08deac98275c0b8298348ff3679764790
Jörg Stucke <joerg.stucke@fkie.fraunhofer.de>
2023-04-18 13:06:47 +0200

    added MIME type to file download html header
    so that firefox doesn't append .html to files without suffix when downloading a file

--- src/test/unit/web_interface/test_app_download.py
+++ src/test/unit/web_interface/test_app_download.py
@@ -1,4 +1,6 @@
-from test.common_helper import TEST_FW
+import pytest
+
+from test.common_helper import TEST_FW, CommonDatabaseMock  # pylint: disable=wrong-import-order
 
 
 def test_app_download_raw_invalid(test_client):
@@ -11,13 +13,29 @@ def test_app_download_raw_error(test_client):
     assert b'<strong>Error!</strong>  timeout' in rv.data
 
 
+class DbMock(CommonDatabaseMock):
+    def get_analysis(self, uid, plugin):  # pylint: disable=unused-argument
+        return {'mime': 'application/x-foobar'}
+
+
+@pytest.mark.WebInterfaceUnitTestConfig(database_mock_class=DbMock)
 def test_app_download_raw(test_client):
     rv = test_client.get(f'/download/{TEST_FW.uid}')
     assert TEST_FW.binary in rv.data
     assert 'attachment; filename=test.zip' in rv.headers['Content-Disposition']
+    assert rv.headers['Content-Type'] == 'application/x-foobar'
+
+
+@pytest.mark.WebInterfaceUnitTestConfig(database_mock_class=DbMock)
+def test_app_download_missing_mime(test_client, monkeypatch):
+    monkeypatch.setattr(DbMock, 'get_analysis', lambda *_: None)  # simulate missing file type analysis
+    rv = test_client.get(f'/download/{TEST_FW.uid}')
+    assert TEST_FW.binary in rv.data
+    assert rv.headers['Content-Type'] == 'application/zip', 'MIME data should be generated if the DB entry is missing'
 
 
 def test_app_tar_download(test_client):
     rv = test_client.get(f'/tar-download/{TEST_FW.uid}')
     assert TEST_FW.binary in rv.data
     assert 'attachment; filename=test.zip' in rv.headers['Content-Disposition']
+    assert rv.headers['Content-Type'] == 'application/gzip'
