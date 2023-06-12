0255d05a40f21cc88f8418ee690e2926eb620cd5
Jörg Stucke <joerg.stucke@fkie.fraunhofer.de>
2023-06-12 17:17:33 +0200

    users_and_passwords: updated image tag

--- src/plugins/analysis/users_and_passwords/code/password_file_analyzer.py
+++ src/plugins/analysis/users_and_passwords/code/password_file_analyzer.py
@@ -113,7 +113,7 @@ def crack_hash(passwd_entry: bytes, result_entry: dict, format_term: str = '') -
         fp.write(passwd_entry)
         fp.seek(0)
         john_process = run_docker_container(
-            'fact/john:alpine-3.14',
+            'fact/john:alpine-3.18',
             command=f'/work/input_file {format_term}',
             mounts=[
                 Mount('/work/input_file', fp.name, type='bind'),
--- src/plugins/analysis/users_and_passwords/install.py
+++ src/plugins/analysis/users_and_passwords/install.py
@@ -21,7 +21,7 @@ class UsersAndPasswordsInstaller(AbstractPluginInstaller):
     base_path = Path(__file__).resolve().parent
 
     def install_docker_images(self):
-        self._build_docker_image('fact/john:alpine-3.14')
+        self._build_docker_image('fact/john:alpine-3.18')
 
     def install_files(self):
         if not JOHN_POT.is_file():
