ef0edc36ce94be80f83b99a04867294be6bec542
lennart <lennart.henke@FKIE.fraunhofer.de>
2023-06-14 10:56:07 +0200

    formated with black

--- src/plugins/analysis/cve_lookup/internal/busybox_cve_filter.py
+++ src/plugins/analysis/cve_lookup/internal/busybox_cve_filter.py
@@ -8,7 +8,10 @@ from objects.file import FileObject
 GROUP_1 = None
 GROUP_2 = None
 
-def filter_busybox_cves(file_object: FileObject, cve_candidates: Dict[str, Dict[str, str]], version: str) -> Dict[str, Dict[str, str]]:
+
+def filter_busybox_cves(
+    file_object: FileObject, cve_candidates: Dict[str, Dict[str, str]], version: str
+) -> Dict[str, Dict[str, str]]:
     '''
     Filters the BusyBox CVEs based on the components present in the binary file and the specified version.
     '''
@@ -17,23 +20,25 @@ def filter_busybox_cves(file_object: FileObject, cve_candidates: Dict[str, Dict[
     filtered_cves = filter_cves_by_component(cves, components)
     return {cve_id: cve_data for cve_id, cve_data in cve_candidates.items() if cve_id in filtered_cves}
 
-def get_cves_for_busybox_version(version: str) -> Dict[str, str]:
-        '''
-        Gets the CVEs for the specified version of BusyBox.
-        '''
-        url = f'https://services.nvd.nist.gov/rest/json/cves/1.0?cpeMatchString=cpe:/a:busybox:busybox:{version}'
-        try:
-            response = requests.get(url)
-            response.raise_for_status()
-        except requests.exceptions.HTTPError as e:
-            logging.exception(f"Error fetching CVE data for BusyBox {version}")
-            return {}
 
-        cve_data = response.json()
-        return {
-            entry['cve']['CVE_data_meta']['ID']: entry['cve']['description']['description_data'][0]['value']
-            for entry in cve_data['result']['CVE_Items']
-        }
+def get_cves_for_busybox_version(version: str) -> Dict[str, str]:
+    '''
+    Gets the CVEs for the specified version of BusyBox.
+    '''
+    url = f'https://services.nvd.nist.gov/rest/json/cves/1.0?cpeMatchString=cpe:/a:busybox:busybox:{version}'
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+    except requests.exceptions.HTTPError:
+        logging.exception(f'Error fetching CVE data for BusyBox {version}')
+        return {}
+
+    cve_data = response.json()
+    return {
+        entry['cve']['CVE_data_meta']['ID']: entry['cve']['description']['description_data'][0]['value']
+        for entry in cve_data['result']['CVE_Items']
+    }
+
 
 def get_busybox_components(file_object: FileObject) -> list[str]:
     '''
@@ -45,10 +50,9 @@ def get_busybox_components(file_object: FileObject) -> list[str]:
 
     start_index = data.index(b'\x5b\x00\x5b\x5b\x00')
     end_index = data.index(b'\x00\x00', start_index + 5)
-    extracted_bytes = data[start_index:end_index+2]
+    extracted_bytes = data[start_index : end_index + 2]
     split_bytes = extracted_bytes.split(b'\x00')
-    components = [word.decode('ascii') for word in split_bytes if word]
-    return components
+    return [word.decode('ascii') for word in split_bytes if word]
 
 
 def filter_cves_by_component(cves: Dict[str, str], components: List[str]) -> Dict[str, List[str]]:
@@ -82,8 +86,9 @@ def get_matched_words(cve_data: str) -> List[str]:
     pattern_1 = r'(?:\")(?:\-|\")'
     pattern_2 = r'(?:\b|\(|\"|\_)(?:{})(?:\b|\)|-)'
 
-    return [word for word in GROUP_1 if re.search(pattern_1.format(word), cve_data)] + \
-                    [word for word in GROUP_2 if re.search(pattern_2.format(word), cve_data)]
+    return [word for word in GROUP_1 if re.search(pattern_1.format(word), cve_data)] + [
+        word for word in GROUP_2 if re.search(pattern_2.format(word), cve_data)
+    ]
 
 
 def remove_unrelated_cves(cves: Dict[str, List[str]], components: List[str]) -> Dict[str, List[str]]:
