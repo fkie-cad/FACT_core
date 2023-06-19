2a544650218889ca6859cd5a6b43fca039b5b8b9
Jörg Stucke <joerg.stucke@fkie.fraunhofer.de>
2023-06-19 11:13:20 +0200

    test_check_collision: adapt to new VFP structure

--- src/test/integration/scheduler/test_regression_virtual_file_path.py
+++ src/test/integration/scheduler/test_regression_virtual_file_path.py
@@ -1,17 +1,13 @@
-# pylint: disable=redefined-outer-name,wrong-import-order
 from multiprocessing import Event, Value
 from pathlib import Path
 
 import pytest
 
-from intercom.back_end_binding import InterComBackEndBinding
 from objects.firmware import Firmware
-from scheduler.analysis import AnalysisScheduler
 from scheduler.unpacking_scheduler import UnpackingScheduler
 from storage.db_interface_backend import BackendDbInterface
 from storage.unpacking_locks import UnpackingLockManager
 from test.common_helper import get_test_data_dir
-from web_interface.frontend_main import WebFrontEnd
 
 FIRST_ROOT_ID = '5fadb36c49961981f8d87cc21fc6df73a1b90aa1857621f2405d317afb994b64_68415'
 SECOND_ROOT_ID = '0383cac1dd8fbeb770559163edbd571c21696c435a4942bec6df151983719731_52143'
@@ -37,13 +33,6 @@ def intermediate_event():
 
 
 @pytest.fixture
-def test_app():
-    frontend = WebFrontEnd()
-    frontend.app.config['TESTING'] = True
-    return frontend.app.test_client()
-
-
-@pytest.fixture
 def test_scheduler(finished_event, intermediate_event):
     interface = BackendDbInterface()
     unpacking_lock_manager = UnpackingLockManager()
@@ -57,50 +46,37 @@ def test_scheduler(finished_event, intermediate_event):
         elif elements_finished.value == 4:
             intermediate_event.set()
 
-    analyzer = AnalysisScheduler(
-        # pre_analysis=count_pre_analysis, TODO
-        db_interface=interface,
+    unpacker = UnpackingScheduler(
+        post_unpack=count_pre_analysis,
         unpacking_locks=unpacking_lock_manager,
     )
-    analyzer.start()
-    unpacker = UnpackingScheduler(post_unpack=analyzer.start_analysis_of_object, unpacking_locks=unpacking_lock_manager)
     unpacker.start()
-    intercom = InterComBackEndBinding(
-        analysis_service=analyzer,
-        unpacking_service=unpacker,
-        compare_service=MockScheduler(),
-        unpacking_locks=unpacking_lock_manager,
-    )
-    intercom.start()
     try:
         yield unpacker
     finally:
-        intercom.shutdown()
         unpacker.shutdown()
         unpacking_lock_manager.shutdown()
-        analyzer.shutdown()
 
 
-def add_test_file(scheduler, path_in_test_dir):
+def add_test_file(scheduler: UnpackingScheduler, path_in_test_dir: str):
     firmware = Firmware(file_path=str(Path(get_test_data_dir(), path_in_test_dir)))
     firmware.release_date = '1990-01-16'
     firmware.version, firmware.vendor, firmware.device_name, firmware.device_class = ['foo'] * 4
     scheduler.add_task(firmware)
 
 
-def test_check_collision(
-    db, test_app, test_scheduler, finished_event, intermediate_event
-):  # pylint: disable=unused-argument
+def test_check_collision(db, test_scheduler, finished_event, intermediate_event):
     add_test_file(test_scheduler, 'regression_one')
 
-    assert intermediate_event.wait(timeout=30)
+    assert intermediate_event.wait(timeout=5)
 
     add_test_file(test_scheduler, 'regression_two')
 
     assert finished_event.wait(timeout=30)
 
-    first_response = test_app.get(f'/analysis/{TARGET_UID}/ro/{FIRST_ROOT_ID}')
-    assert b'insufficient information' not in first_response.data
-
-    second_response = test_app.get(f'/analysis/{TARGET_UID}/ro/{SECOND_ROOT_ID}')
-    assert b'insufficient information' not in second_response.data
+    fo_from_db = db.frontend.get_object(TARGET_UID)
+    assert len(fo_from_db.virtual_file_path) == 2, 'fo should have two parents'
+    assert FIRST_ROOT_ID in fo_from_db.virtual_file_path
+    assert fo_from_db.virtual_file_path[FIRST_ROOT_ID] == ['/test']
+    assert SECOND_ROOT_ID in fo_from_db.virtual_file_path
+    assert fo_from_db.virtual_file_path[SECOND_ROOT_ID] == ['/test']
