a0299b524232ad3ecde3f64e9c722905af2212b2
Jörg Stucke <joerg.stucke@fkie.fraunhofer.de>
2023-06-28 11:58:05 +0200

    fix static file test duplicates & naming + refactoring

--- src/test/unit/web_interface/test_app_static.py
+++ src/test/unit/web_interface/test_app_static.py
@@ -1,23 +1,28 @@
+from __future__ import annotations
+
 import re
 from pathlib import Path
+from typing import Iterable
 
 import pytest
 from flask import url_for
 
+from helperFunctions.fileSystem import get_template_dir, get_src_dir
+
 URL_FOR_REGEX = re.compile(r'url_for\([\'"]\.?static[\'"], filename ?= ?[\'"]([^\'"]+)[\'"]\)')
-TEMPLATE_DIR = Path(__file__).parent.parent.parent.parent / 'web_interface/templates'
+SRC_DIR = Path(get_src_dir())
 
 
-def _find_static_files():
-    return {
-        (template, static_url)
-        for template in TEMPLATE_DIR.glob('**/*.html')
-        for static_url in URL_FOR_REGEX.findall(template.read_text())
-    }
+def _find_static_files() -> Iterable[tuple[str, set[str]]]:
+    result = {}
+    for template in get_template_dir().glob('**/*.html'):
+        for static_url in URL_FOR_REGEX.findall(template.read_text()):
+            result.setdefault(static_url, set()).add(str(template.relative_to(SRC_DIR)))
+    return result.items()
 
 
-@pytest.mark.parametrize(('template', 'static_file'), list(_find_static_files()))
-def test_add_firmwares_to_compare__multiple(web_frontend, test_client, template, static_file):
+@pytest.mark.parametrize(('static_file', 'template'), _find_static_files())
+def test_static_web_files_are_found(web_frontend, test_client, template, static_file):
     with web_frontend.app.test_request_context():
         url = url_for('static', filename=static_file)
         rv = test_client.get(url, follow_redirects=True)
