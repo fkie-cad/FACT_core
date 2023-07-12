8fa25306ad195817e72c2699a59ce713090943cb
lennart <lennart.henke@FKIE.fraunhofer.de>
2023-07-12 15:20:11 +0200

    fixed test

--- src/plugins/analysis/cve_lookup/test/test_cve_lookup.py
+++ src/plugins/analysis/cve_lookup/test/test_cve_lookup.py
@@ -14,20 +14,18 @@ db_path = temp_dir.name
 cve_lookup.DB_PATH = f'{db_path}/test.db'
 
 SOFTWARE_COMPONENTS_ANALYSIS_RESULT = {
-    'result': {
-        'dnsmasq': {'meta': {'software_name': 'Dnsmasq', 'version': ['2.40']}},
-        'OpenSSL': {
-            'matches': True,
-            'meta': {
-                'description': 'SSL library',
-                'open_source': True,
-                'software_name': 'OpenSSL',
-                'version': [''],
-                'website': 'https://www.openssl.org',
-            },
-            'rule': 'OpenSSL',
-            'strings': [[7194, '$a', 'T1BFTlNTTA==']],
+    'dnsmasq': {'meta': {'software_name': 'Dnsmasq', 'version': ['2.40']}},
+    'OpenSSL': {
+        'matches': True,
+        'meta': {
+            'description': 'SSL library',
+            'open_source': True,
+            'software_name': 'OpenSSL',
+            'version': [''],
+            'website': 'https://www.openssl.org',
         },
+        'rule': 'OpenSSL',
+        'strings': [[7194, '$a', 'T1BFTlNTTA==']],
     },
     'analysis_date': 1563453634.37708,
     'plugin_version': '0.3.2',
