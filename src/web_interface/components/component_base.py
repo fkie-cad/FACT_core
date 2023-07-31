f0be0e2b87cce08d647eb14df7d707fd2831118e
Jörg Stucke <joerg.stucke@fkie.fraunhofer.de>
2023-07-31 10:20:59 +0200

    chore: Remove ConnectTo

--- src/web_interface/components/component_base.py
+++ src/web_interface/components/component_base.py
@@ -5,6 +5,7 @@ from typing import Any, NamedTuple, TYPE_CHECKING
 
 
 if TYPE_CHECKING:
+    from intercom.front_end_binding import InterComFrontEndBinding
     from storage.redis_status_interface import RedisStatusInterface
     from web_interface.frontend_database import FrontendDatabase
     from collections.abc import Callable
@@ -46,7 +47,14 @@ class AppRoute:
 
 
 class ComponentBase:
-    def __init__(self, app, db: FrontendDatabase, intercom, status: RedisStatusInterface, api=None):  # noqa: PLR0913
+    def __init__(  # noqa: PLR0913
+        self,
+        app,
+        db: FrontendDatabase,
+        intercom: InterComFrontEndBinding,
+        status: RedisStatusInterface,
+        api=None,
+    ):
         self._app = app
         self._api = api
         self.db = db
