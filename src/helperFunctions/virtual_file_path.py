0e292333b342c1e441b6513c5cb77743235a6866
Jörg Stucke <joerg.stucke@fkie.fraunhofer.de>
2023-08-04 14:48:53 +0200

    added type alias for vfp and vfp dict

--- src/helperFunctions/virtual_file_path.py
+++ src/helperFunctions/virtual_file_path.py
@@ -1,9 +1,13 @@
 from __future__ import annotations
 
 from contextlib import suppress
+from typing import Dict, List, TypeAlias
+
+VFP: TypeAlias = str
+VfpDict: TypeAlias = Dict[str, List[VFP]]
 
 
-def get_paths_for_all_parents(vfp_dict: dict[str, list[str]]) -> list[str]:
+def get_paths_for_all_parents(vfp_dict: VfpDict) -> list[VFP]:
     """
     Get a combined list of all paths in all parents (without duplicates)
     :param vfp_dict: A vfp dict (typically found in FileObject.virtual_file_path)
@@ -13,7 +17,7 @@ def get_paths_for_all_parents(vfp_dict: dict[str, list[str]]) -> list[str]:
     return list(set.union(*(set(vfp_list) for vfp_list in vfp_dict.values())))
 
 
-def get_some_vfp(vfp_dict: dict[str, list[str]]) -> str | None:
+def get_some_vfp(vfp_dict: VfpDict) -> VFP | None:
     """Just get some random virtual file path."""
     for vfp_list in vfp_dict.values():
         with suppress(KeyError):
