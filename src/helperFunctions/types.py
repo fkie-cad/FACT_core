b7144735e83568084632c9c7ef97e7249e454cfe
Jörg Stucke <joerg.stucke@fkie.fraunhofer.de>
2023-08-18 10:15:16 +0200

    fix typing and type hints for analysis scheduler

--- src/helperFunctions/types.py
+++ src/helperFunctions/types.py
@@ -1,9 +1,13 @@
 from __future__ import annotations
 
+from abc import ABCMeta
 from multiprocessing.sharedctypes import Synchronized, SynchronizedArray
 from tempfile import _TemporaryFileWrapper
 from typing import TypeVar, TypeAlias, NamedTuple
 from unittest.mock import _patch
+
+from analysis.plugin import AnalysisPluginV0
+from analysis.plugin.compat import AnalysisBasePluginAdapterMixin
 
 KT = TypeVar('KT', str, tuple, bytes)  # generic key type
 VT = TypeVar('VT')  # generic value type
@@ -26,6 +30,14 @@ UID: TypeAlias = str
 
 # unittest patch() is a function and returns a private _patch class
 Patch: TypeAlias = _patch
+
+
+class CompatPluginV0(AnalysisPluginV0, AnalysisBasePluginAdapterMixin, metaclass=ABCMeta):
+    """An AnalysisPluginV0 plugin that also inherits from AnalysisBasePluginAdapterMixin"""
+
+    # Fixme: it would be better if AnalysisPluginV0 inherited from AnalysisBasePluginAdapterMixin because it should
+    #        not concern the user that we need a mixin class for compatibility. On top of that, we plan to remove that
+    #        class when we remove the old class and would need to change all plugins again.
 
 
 class AnalysisPluginInfo(NamedTuple):
