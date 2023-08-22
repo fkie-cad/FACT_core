9f5b4cc119ee61375654857e7821c6d3434b3ab6
Jörg Stucke <joerg.stucke@fkie.fraunhofer.de>
2023-08-22 10:44:48 +0200

    chore(source_code_analysis): Add test cases

--- src/plugins/analysis/linter/test/test_source_code_analysis.py
+++ src/plugins/analysis/linter/test/test_source_code_analysis.py
@@ -4,14 +4,19 @@ import pytest
 
 from ..code.source_code_analysis import AnalysisPlugin
 
-PYLINT_TEST_FILE = Path(__file__).parent / 'data' / 'linter_test_file'
-NOT_A_SCRIPT_FILE = Path(__file__).parent / 'data' / 'file'
+TEST_DATA_DIR = Path(__file__).parent / 'data'
+PYLINT_TEST_FILE = TEST_DATA_DIR / 'hello_world.py'
+PHP_TEST_FILE = TEST_DATA_DIR / 'hello_world.php'
+JS_TEST_FILE = TEST_DATA_DIR / 'hello_world.js'
+RUBY_TEST_FILE = TEST_DATA_DIR / 'hello_world.ruby'
+BASH_TEST_FILE = TEST_DATA_DIR / 'hello_world.sh'
+NOT_A_SCRIPT_FILE = TEST_DATA_DIR / 'file'
 
 
 @pytest.mark.AnalysisPluginTestConfig(plugin_class=AnalysisPlugin)
 class TestSourceCodeAnalysis:
     def test_analyze_not_supported(self, analysis_plugin):
-        with Path(NOT_A_SCRIPT_FILE).open() as f:
+        with NOT_A_SCRIPT_FILE.open() as f:
             result = analysis_plugin.analyze(f, {}, {})
         summary = analysis_plugin.summarize(result)
 
@@ -19,13 +24,22 @@ class TestSourceCodeAnalysis:
         assert result.language is None
         assert result.issues is None
 
-    def test_analyze(self, analysis_plugin, monkeypatch):
-        with Path(PYLINT_TEST_FILE).open() as f:
+    @pytest.mark.parametrize(
+        ('file', 'language', 'symbol'),
+        [
+            (PYLINT_TEST_FILE, 'python', 'unused-import'),
+            (PHP_TEST_FILE, 'php', 'error'),
+            (JS_TEST_FILE, 'javascript', 'no-unused-vars'),
+            (RUBY_TEST_FILE, 'ruby', 'Style/FrozenStringLiteralComment'),
+            (BASH_TEST_FILE, 'shell', '2050'),
+        ],
+    )
+    def test_analyze(self, analysis_plugin, file, language, symbol):
+        with file.open() as f:
             result = analysis_plugin.analyze(f, {}, {})
         summary = analysis_plugin.summarize(result)
 
-        assert set(summary) == {'has-warnings', 'python'}
+        assert set(summary) == {'has-warnings', language}
         assert result is not None
         assert len(result.issues) > 0
-        assert result.issues[0].type == 'warning'
-        assert result.issues[0].symbol == 'unused-import'
+        assert result.issues[0].symbol == symbol
