55e6c5c7c90e49b7e038aa5b4704229185f3d6dc
Jörg Stucke <joerg.stucke@fkie.fraunhofer.de>
2023-08-31 17:55:37 +0200

    fix typing of remaining (non-plugin) tests

--- src/plugins/analysis/software_components/test/test_plugin_software_components_signatures.py
+++ src/plugins/analysis/software_components/test/test_plugin_software_components_signatures.py
@@ -7,28 +7,25 @@ SIGNATURE_PATH = Path(__file__).parent.parent / 'signatures/'
 TEST_SIGNATURE_PATH = Path(__file__).parent.parent / 'test/data/signatures/'
 
 
-class TestSoftwareSignatureMeta:
-    @classmethod
-    def setup_class(cls):
-        cls.sigTest = SignatureTestingMeta()
-
+class TestSoftwareSignatureMeta(SignatureTestingMeta):
     def test_check_meta_fields(self):
-        missing_fields = self.sigTest.check_meta_fields(SIGNATURE_PATH)
+        missing_fields = self.check_meta_fields(SIGNATURE_PATH)
         assert not missing_fields, f'Missing meta fields: {missing_fields.__str__()}'
 
     def test_check_meta_fields_missing(self):
-        missing_fields = self.sigTest.check_meta_fields(TEST_SIGNATURE_PATH)
+        missing_fields = self.check_meta_fields(TEST_SIGNATURE_PATH)
         assert len(missing_fields) == 3  # noqa: PLR2004
-        assert all(
-            entry in missing_fields
-            for entry in ['website in missing_meta_1', 'description in missing_meta_1', 'ALL in missing_meta_2']
-        )
+        assert set(missing_fields) == {
+            'ALL in missing_meta_2',
+            'description in missing_meta_1',
+            'website in missing_meta_1',
+        }
 
 
 class TestAllSoftwareSignaturesMatched:
     def setup_method(self):
-        self.sig_tester = SignatureTestingMatching()
+        self.resulter = SignatureTestingMatching()
 
     def test_all_signatures_matched(self):
-        diff = self.sig_tester.check(SIGNATURE_PATH, TEST_DATA_DIR / 'software_component_test_list.txt')
+        diff = self.resulter.check(SIGNATURE_PATH, TEST_DATA_DIR / 'software_component_test_list.txt')
         assert diff == set(), f'Missing signature for {diff}'
