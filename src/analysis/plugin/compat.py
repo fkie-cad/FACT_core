96ce0ccec275650d4d0b33bfc209fc0143f08672
Jörg Stucke <joerg.stucke@fkie.fraunhofer.de>
2023-12-20 16:14:38 +0100

    plugin compat: updated for new yara version + added test

--- src/analysis/plugin/compat.py
+++ src/analysis/plugin/compat.py
@@ -58,19 +58,17 @@ class AnalysisBasePluginAdapterMixin:
 
 def yara_match_to_dict(match: yara.Match) -> dict:
     """Converts a ``yara.Match`` to the format that :py:class:`analysis.YaraPluginBase` would return."""
-    # FIXME (yara): Use this when we upgrade to yara-python 4.3.0
-    # for string_match in match.strings:
-    #    for string_instance in string_match.instances:
-
-    strings = [(offset, identifier, data.hex()) for offset, identifier, data in match.strings]
+    # see YARA docs: https://yara.readthedocs.io/en/latest/yarapython.html#yara.StringMatchInstance
+    strings = [
+        (string_instance.offset, string_match.identifier, string_instance.matched_data.decode(errors='replace'))
+        for string_match in match.strings  # type: yara.StringMatch
+        for string_instance in string_match.instances  # type: yara.StringMatchInstance
+    ]
 
     return {
         'meta': {
-            # Optional
-            'date': match.meta.get('date'),
-            # Optional
-            'author': match.meta.get('author'),
-            'description': match.meta['description'],
+            key: match.meta.get(key)
+            for key in ('open_source', 'software_name', 'website', 'date', 'author', 'description')
         },
         'rule': match.rule,
         'strings': strings,
