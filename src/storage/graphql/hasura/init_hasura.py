09c4f633cf3c8cfa384f9376ed36ba1014d6d9db
Jörg Stucke <joerg.stucke@fkie.fraunhofer.de>
2024-06-24 09:45:23 +0200

    graphql init: refactoring to make testing possible

--- src/storage/graphql/hasura/init_hasura.py
+++ src/storage/graphql/hasura/init_hasura.py
@@ -4,7 +4,6 @@ import logging
 import sys
 from pathlib import Path
 
-import docker
 import requests
 from requests import Response
 from requests.adapters import HTTPAdapter, Retry
@@ -16,18 +15,8 @@ except ImportError:
     sys.path.append(str(SRC_DIR))
     import config
 
-config.load()
 HTML_OK = 200
 HTML_BAD_REQUEST = 400
-DB_NAME = 'fact_db'
-URL = f'http://localhost:{config.frontend.hasura.port}/v1/metadata'
-HEADERS = {
-    'Content-Type': 'application/json',
-    'X-Hasura-Role': 'admin',
-    'X-Hasura-Admin-Secret': config.frontend.hasura.admin_secret,
-}
-client = docker.from_env()
-
 TRACKED_TABLES = ('analysis', 'file_object', 'firmware', 'fw_files', 'included_files', 'virtual_file_path')
 RELATIONSHIPS = {
     'pg_create_object_relationship': [
@@ -38,125 +27,171 @@ RELATIONSHIPS = {
         ('fw_files', 'file_object', 'file_uid'),
         ('included_files', 'parent', 'parent_uid'),
         ('included_files', 'child', 'child_uid'),
-        ('file_object', 'firmware', {'column': 'uid', 'table': {'name': 'firmware', 'schema': 'public'}}),
+        ('file_object', 'firmware', 'uid', 'firmware'),
     ],
     'pg_create_array_relationship': [
-        ('file_object', 'analysis', {'column': 'uid', 'table': {'name': 'analysis', 'schema': 'public'}}),
-        (
-            'file_object',
-            'firmwareFilesByFirmware',
-            {'column': 'root_uid', 'table': {'name': 'fw_files', 'schema': 'public'}},
-        ),
-        (
-            'file_object',
-            'firmwareFilesByFile',
-            {'column': 'file_uid', 'table': {'name': 'fw_files', 'schema': 'public'}},
-        ),
-        (
-            'file_object',
-            'includedFilesByParent',
-            {'column': 'parent_uid', 'table': {'name': 'included_files', 'schema': 'public'}},
-        ),
-        (
-            'file_object',
-            'includedFilesByChild',
-            {'column': 'child_uid', 'table': {'name': 'included_files', 'schema': 'public'}},
-        ),
-        (
-            'file_object',
-            'FilePathsByParent',
-            {'column': 'parent_uid', 'table': {'name': 'virtual_file_path', 'schema': 'public'}},
-        ),
-        (
-            'file_object',
-            'FilePathsByFile',
-            {'column': 'file_uid', 'table': {'name': 'virtual_file_path', 'schema': 'public'}},
-        ),
+        ('file_object', 'analysis', 'uid', 'analysis'),
+        ('file_object', 'firmwareFilesByFirmware', 'root_uid', 'fw_files'),
+        ('file_object', 'firmwareFilesByFile', 'file_uid', 'fw_files'),
+        ('file_object', 'includedFilesByParent', 'parent_uid', 'included_files'),
+        ('file_object', 'includedFilesByChild', 'child_uid', 'included_files'),
+        ('file_object', 'FilePathsByParent', 'parent_uid', 'virtual_file_path'),
+        ('file_object', 'FilePathsByFile', 'file_uid', 'virtual_file_path'),
     ],
 }
 
 
-def init_hasura():
-    logging.info('Waiting for Hasura...')
-    _wait_for_hasura()
-    logging.info('Initializing Hasura...')
-    if not _db_was_already_added():
-        _add_database()
-    _track_tables()
-    _add_relationships()
-    _add_ro_user_role_to_tables()
-    logging.info('Hasura initialization successful')
+class HasuraInitError(Exception):
+    pass
 
 
-def _wait_for_hasura():
-    # Hasura is not ready for connections directly after starting the container so we may need to wait a bit
-    # hasura will return code 200 OK or 500 ERROR on this health check endpoint
-    healthcheck_url = f'http://localhost:{config.frontend.hasura.port}/healthz'
-    session = requests.Session()
-    # retry 5 times with a total of 30 seconds
-    retries = Retry(total=5, backoff_factor=1, status_forcelist=[500])
-    session.mount('http://', HTTPAdapter(max_retries=retries))
-    try:
-        session.get(healthcheck_url)
-    except (ConnectionRefusedError, ConnectionResetError):
-        logging.exception('Could not reach Hasura')
-        sys.exit(1)
+class HasuraSetup:
+    def __init__(self, db_name: str | None = None, testing: bool = False):
+        self.db_name = db_name or config.common.postgres.database
+        self.url = f'http://localhost:{config.frontend.hasura.port}/v1/metadata'
+        self.headers = {
+            'Content-Type': 'application/json',
+            'X-Hasura-Role': 'admin',
+            'X-Hasura-Admin-Secret': config.frontend.hasura.admin_secret,
+        }
+        self.testing = testing
 
+    def init_hasura(self, db_args: dict | None = None):
+        logging.info('Waiting for Hasura...')
+        self._wait_for_hasura()
+        logging.info('Initializing Hasura...')
+        if not self._db_was_already_added():
+            self._add_database(db_args)
+        self._track_tables()
+        self._add_relationships()
+        self._add_ro_user_role_to_tables()
+        logging.info('Hasura initialization successful')
 
-def _add_database():
-    query = {
-        'type': 'pg_add_source',
-        'args': {
-            'name': DB_NAME,
-            'configuration': {
-                'connection_info': {
-                    'database_url': {'from_env': 'FACT_DB_URL'},
-                    'pool_settings': {'retries': 1, 'idle_timeout': 180, 'max_connections': 50},
-                },
-            },
-        },
-    }
-    response = requests.post(URL, headers=HEADERS, json=query)
-    if response.status_code != HTML_OK:
-        logging.error(f'Failed to add database: {response.text}')
-        sys.exit(5)
+    def _wait_for_hasura(self):
+        # Hasura is not ready for connections directly after starting the container, so we may need to wait a bit
+        # hasura will return code 200 OK or 500 ERROR on this health check endpoint
+        healthcheck_url = self.url.replace('/v1/metadata', '/healthz')
+        session = requests.Session()
+        # retry 5 times with a total of 30 seconds
+        retries = Retry(total=5, backoff_factor=1, status_forcelist=[500])
+        session.mount('http://', HTTPAdapter(max_retries=retries))
+        try:
+            session.get(healthcheck_url)
+        except (ConnectionRefusedError, ConnectionResetError) as _error:
+            raise HasuraInitError('Could not reach Hasura') from _error
 
-
-def _track_tables():
-    for table in TRACKED_TABLES:
+    def _add_database(self, additional_args: dict | None = None):
         query = {
-            'type': 'pg_track_table',
+            'type': 'pg_add_source',
             'args': {
-                'source': DB_NAME,
-                'table': table,
+                'name': self.db_name,
+                'configuration': {
+                    'connection_info': {
+                        'database_url': self._get_db_url(),
+                        'pool_settings': {'retries': 1, 'idle_timeout': 180, 'max_connections': 50},
+                    },
+                },
+                **(additional_args or {}),
             },
         }
-        response = requests.post(URL, headers=HEADERS, json=query)
+        response = requests.post(self.url, headers=self.headers, json=query)
         if response.status_code != HTML_OK:
-            if _was_already_added(response):
-                continue
-            logging.error(f'Failed to track table {table}: {response.text}')
-            sys.exit(6)
+            raise HasuraInitError(f'Failed to add database: {response.json().get("error")}')
 
+    def _get_db_url(self):
+        if not self.testing:
+            return {'from_env': 'FACT_DB_URL'}
+        user = config.common.postgres.ro_user
+        pw = config.common.postgres.ro_pw
+        return f'postgresql://{user}:{pw}@/fact_test?host=/var/run/postgresql'
 
-def _add_relationships():
-    for action, relation_list in RELATIONSHIPS.items():
-        for table, name, constraint in relation_list:
+    def drop_database(self):
+        query = {
+            'type': 'pg_drop_source',
+            'args': {
+                'name': self.db_name,
+                'cascade': True,
+            },
+        }
+        response = requests.post(self.url, headers=self.headers, json=query)
+        if response.status_code != HTML_OK:
+            raise HasuraInitError(f'Failed to drop database: {response.json().get("error")}')
+
+    def _track_tables(self):
+        for table in TRACKED_TABLES:
             query = {
-                'type': action,
+                'type': 'pg_track_table',
                 'args': {
+                    'source': self.db_name,
                     'table': table,
-                    'name': name,
-                    'source': DB_NAME,
-                    'using': {'foreign_key_constraint_on': constraint},
                 },
             }
-            response = requests.post(URL, headers=HEADERS, json=query)
+            if self.testing:
+                # we need to use another name during testing to avoid naming conflicts
+                query['args']['configuration'] = {'custom_name': self._prefix(table)}
+            response = requests.post(self.url, headers=self.headers, json=query)
             if response.status_code != HTML_OK:
                 if _was_already_added(response):
                     continue
-                logging.error(f'Failed to add constraint {name} on table {table}: {response.text}')
-                sys.exit(7)
+                raise HasuraInitError(f'Failed to track table {table}: {response.json().get("error")}')
+
+    def _add_relationships(self):
+        for action, relation_list in RELATIONSHIPS.items():
+            for table, name, *constraints in relation_list:
+                if len(constraints) == 1:
+                    constraint = constraints[0]
+                else:
+                    column, other_table = constraints
+                    constraint = {
+                        'column': column,
+                        'table': {'name': other_table, 'schema': 'public'},
+                    }
+                query = {
+                    'type': action,
+                    'args': {
+                        'table': table,
+                        'name': self._prefix(name),
+                        'source': self.db_name,
+                        'using': {'foreign_key_constraint_on': constraint},
+                    },
+                }
+                response = requests.post(self.url, headers=self.headers, json=query)
+                if response.status_code != HTML_OK:
+                    if _was_already_added(response):
+                        continue
+                    raise HasuraInitError(
+                        f'Failed to add constraint {name} on table {table}: {response.json().get("error")}'
+                    )
+
+    def _db_was_already_added(self) -> bool:
+        query = {'type': 'pg_get_source_tables', 'args': {'source': self.db_name}}
+        response = requests.post(self.url, headers=self.headers, json=query)
+        if response.status_code not in {HTML_OK, HTML_BAD_REQUEST}:
+            raise HasuraInitError('No connection to Hasura API. Is it running?')
+        data = response.json()
+        if isinstance(data, dict) and 'error' in data:
+            return False
+        return True
+
+    def _add_ro_user_role_to_tables(self):
+        for table in TRACKED_TABLES:
+            query = {
+                'type': 'pg_create_select_permission',
+                'args': {
+                    'source': self.db_name,
+                    'table': table,
+                    'role': 'ro_user',
+                    'permission': {'columns': '*', 'filter': {}},
+                },
+            }
+            response = requests.post(self.url, headers=self.headers, json=query)
+            if response.status_code != HTML_OK:
+                if _was_already_added(response):
+                    continue
+                raise HasuraInitError(f'Failed to role to table {table}: {response.json().get("error")}')
+
+    def _prefix(self, attribute: str) -> str:
+        return f'test_{attribute}' if self.testing else attribute
 
 
 def _was_already_added(response: Response) -> bool:
@@ -164,37 +199,11 @@ def _was_already_added(response: Response) -> bool:
     return isinstance(data, dict) and 'error' in data and data.get('code') in ('already-exists', 'already-tracked')
 
 
-def _db_was_already_added() -> bool:
-    query = {'type': 'pg_get_source_tables', 'args': {'source': DB_NAME}}
-    response = requests.post(URL, headers=HEADERS, json=query)
-    if response.status_code not in {HTML_OK, HTML_BAD_REQUEST}:
-        logging.error('No connection to Hasura API. Is it running?')
-        sys.exit(4)
-    data = response.json()
-    if isinstance(data, dict) and 'error' in data:
-        return False
-    return True
-
-
-def _add_ro_user_role_to_tables():
-    for table in TRACKED_TABLES:
-        query = {
-            'type': 'pg_create_select_permission',
-            'args': {
-                'source': DB_NAME,
-                'table': table,
-                'role': 'ro_user',
-                'permission': {'columns': '*', 'filter': {}},
-            },
-        }
-        response = requests.post(URL, headers=HEADERS, json=query)
-        if response.status_code != HTML_OK:
-            if _was_already_added(response):
-                continue
-            logging.error(f'Failed to role to table {table}: {response.text}')
-            sys.exit(8)
-
-
 if __name__ == '__main__':
-    init_hasura()
+    config.load()
+    try:
+        HasuraSetup().init_hasura()
+    except HasuraInitError as error:
+        logging.exception(f'Error during Hasura init: {error}')
+        sys.exit(1)
     sys.exit(0)
