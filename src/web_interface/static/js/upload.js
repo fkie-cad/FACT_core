d42c5be6a99cde627bcc4d8e202719568330275d
Jörg Stucke <joerg.stucke@fkie.fraunhofer.de>
2024-09-09 15:40:27 +0200

    feat: added custom autocomplete to upload inputs

--- src/web_interface/static/js/upload.js
+++ src/web_interface/static/js/upload.js
@@ -5,39 +5,30 @@ $(() => {
     });
 });
 
-function add_device_class_options(selected_device_class, selected_vendor, data) {
-    const deviceClassButton = document.getElementById("device_class_select_button");
-    let deviceNameList = $('#device_name_list');
-    deviceNameList.empty();
-    if (data.hasOwnProperty(selected_device_class)) {
-        if (data[selected_device_class].hasOwnProperty(selected_vendor)) {
-            let device_classes = data[selected_device_class][selected_vendor];
-            // remove duplicates
-            device_classes = [...new Set(device_classes)];
-            device_classes.sort();
-            if (device_classes.length > 0) {
-                for (let index in device_classes) {
-                    deviceNameList.append(`
-                        <a class="dropdown-item" href="#" onClick="updateInput('device_name', this)">
-                            ${device_classes[index]}
-                        </a>
-                    `);
-                }
-                deviceClassButton.disabled = false;
+function addDeviceNameOptions(selected_device_class, selected_vendor) {
+    const dropdownButton = document.getElementById("device_name-button");
+    if (allDeviceNames.hasOwnProperty(selected_device_class)) {
+        if (allDeviceNames[selected_device_class].hasOwnProperty(selected_vendor)) {
+            // update the global variable but without overwriting it
+            deviceNames.length = 0; // empty existing array
+            deviceNames.push(...new Set(allDeviceNames[selected_device_class][selected_vendor]));  // remove duplicates
+            if (deviceNames.length > 0) {
+                deviceNames.sort();
+                dropdownButton.disabled = false;
                 return;
             }
         }
     }
-    deviceClassButton.disabled = true;
+    dropdownButton.disabled = true;
 }
 
-function update_device_names() {
+function updateDeviceNames() {
     const deviceClassInput = document.getElementById("device_class");
     const vendorInput = document.getElementById("vendor");
     const vendor = vendorInput.value.trim();
     const device_class = deviceClassInput.value.trim();
     if (vendor.length > 0 && device_class.length > 0) {
-        add_device_class_options(device_class, vendor, device_names);
+        addDeviceNameOptions(device_class, vendor);
     }
 }
 
@@ -52,21 +43,113 @@ function change_selected_plugins(preset_name) {
     }
 }
 
-function updateInput(input_id, element, do_update = false) {
-    const input = document.getElementById(input_id);
-    input.value = element.innerText;
-    if (do_update) {
-        update_device_names();
+function autocompleteInput(inputId, options) {
+    const input = document.getElementById(inputId);
+    const dropdownButton = document.getElementById(`${inputId}-button`);
+    let currentFocus;
+
+    input.addEventListener("input", populateAutocomplete);
+    dropdownButton.addEventListener("click", () => {
+        input.value = "";
+        populateAutocomplete();
+    });
+
+    input.addEventListener("keydown", (event) => {
+        if (event.ctrlKey && event.code === "Space") {
+            populateAutocomplete();
+            return;
+        } else if (event.code === "Tab") {
+            closeAll();
+            return;
+        }
+        let list = document.getElementById(input.id + "-autocomplete-list");
+        if (!list) return;
+        let listElements = list.getElementsByTagName("li");
+        if (event.code === "ArrowDown") {
+            currentFocus++;
+            setActive(listElements);
+        } else if (event.code === "ArrowUp") {
+            currentFocus--;
+            setActive(listElements);
+        } else if (event.code === "Enter") {
+            event.preventDefault();  // prevent the form from being submitted
+            if (currentFocus > -1) {
+                listElements[currentFocus].click();  // simulate a click on the item
+            }
+        }
+    });
+
+    function populateAutocomplete() {
+        closeAll();
+        currentFocus = -1;
+        const list = document.createElement("ul");
+        list.setAttribute("id", input.id + "-autocomplete-list");
+        list.setAttribute("class", "autocomplete-items list-group border");
+        input.parentNode.appendChild(list);
+
+        let listItem, target;
+        options.forEach(option => {
+            let index = option.toLowerCase().indexOf(input.value.toLowerCase());
+            if (!input.value || index !== -1) {
+                listItem = document.createElement("li");
+                listItem.setAttribute("class", "list-group-item list-group-item-action");
+                listItem.innerHTML = (
+                    option.slice(0, index) +
+                    // display the matched part in bold (with the class click-through, the event.target in the
+                    // eventListener below will not be the <strong/> element)
+                    `<strong class="click-through">${option.slice(index, index + input.value.length)}</strong>` +
+                    option.slice(index + input.value.length)
+                );
+                listItem.__value = option;
+                listItem.__inputId = inputId;
+                listItem.addEventListener("click", (event) => {
+                    input.value = event.target.__value;
+                    closeAll();
+                    if (["device_class", "vendor"].includes(event.target.__inputId)) {
+                        updateDeviceNames();
+                    }
+                });
+                list.appendChild(listItem);
+            }
+        });
+    }
+
+    function setActive(elements) {
+        if (elements === null || elements.length === 0) return false;
+        Array.from(elements).forEach(element => {
+            element.classList.remove("active");
+        });
+        if (currentFocus >= elements.length) {
+            currentFocus = 0;
+        } else if (currentFocus < 0) {
+            currentFocus = (elements.length - 1);
+        }
+        elements[currentFocus].classList.add("active");
     }
 }
 
-function filterFunction(input) {
-    let filter = input.value.toLowerCase();
-    input.parentElement.querySelectorAll(".dropdown-item").forEach(element => {
-        if (!element.innerText.toLowerCase().includes(filter)) {
-            element.style.display = "none";
-        } else {
-            element.style.display = "";
+function closeAll(currentElement) {
+    if (currentElement && currentElement.id.endsWith("-button")) {
+        // if we clicked on the dropdown button we don't want to close the autocomplete list (we just opened)
+        currentElement = getAutocompleteElement(currentElement.parentNode.parentNode);
+    } else if (currentElement && currentElement.tagName.toLowerCase() === "input") {
+        // also don't close the autocomplete that belongs to an input we just clicked
+        currentElement = getAutocompleteElement(currentElement.parentNode);
+    }
+    const elements = document.getElementsByClassName("autocomplete-items");
+    Array.from(elements).forEach((element) => {
+        if (currentElement !== element) {
+            element.parentNode.removeChild(element);
         }
     });
 }
+
+function getAutocompleteElement(parentElement) {
+    const autocompletes = parentElement.querySelectorAll("ul");
+    if (autocompletes.length === 1) return autocompletes[0];
+    return null;
+}
+
+document.addEventListener("click", (event) => {
+    closeAll(event.target);
+});
