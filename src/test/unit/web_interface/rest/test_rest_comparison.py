2e3c1921180d1f84288138b289888593987d9a40
Jörg Stucke <joerg.stucke@fkie.fraunhofer.de>
2024-09-10 15:29:29 +0200

    chore: fix 'compare' typos

--- src/test/unit/web_interface/rest/test_rest_comparison.py
+++ src/test/unit/web_interface/rest/test_rest_comparison.py
@@ -19,21 +19,21 @@ def test_empty_data(test_client):
     assert 'The method is not allowed for the requested URL' in result['message']
 
 
-def test_get_unknown_compare(test_client):
-    compare_id = f'{UID_1};{UID_2}'
-    result = test_client.get(f'/rest/compare/{compare_id}').json
-    assert 'Compare not found in database' in result['error_message']
+def test_get_unknown_comparison(test_client):
+    comparison_id = f'{UID_1};{UID_2}'
+    result = test_client.get(f'/rest/compare/{comparison_id}').json
+    assert 'Comparison not found in database' in result['error_message']
 
 
-def test_get_invalid_compare_id(test_client):
-    compare_id = f'invalid_uid;{UID_2}'
-    result = test_client.get(f'/rest/compare/{compare_id}').json
+def test_get_invalid_comparison_id(test_client):
+    comparison_id = f'invalid_uid;{UID_2}'
+    result = test_client.get(f'/rest/compare/{comparison_id}').json
     assert 'contains invalid chars' in result['error_message']
 
 
-def test_get_invalid_compare_id_2(test_client):
-    compare_id = f'deadbeef_1;{UID_2}'
-    result = test_client.get(f'/rest/compare/{compare_id}').json
+def test_get_invalid_comparison_id_2(test_client):
+    comparison_id = f'deadbeef_1;{UID_2}'
+    result = test_client.get(f'/rest/compare/{comparison_id}').json
     assert 'contains invalid UIDs' in result['error_message']
 
 
@@ -54,11 +54,11 @@ def test_put_pre_existing(test_client):
     data = {'uid_list': [TEST_FW.uid, TEST_FW_2.uid], 'redo': False}
     result = test_client.put('/rest/compare', json=data).json
     assert result['status'] == 1
-    assert 'Compare already exists' in result['error_message']
+    assert 'Comparison already exists' in result['error_message']
 
 
 def test_put_success(test_client):
     data = {'uid_list': [TEST_FW.uid, TEST_FW_2.uid], 'redo': True}
     result = test_client.put('/rest/compare', json=data).json
     assert result['status'] == 0
-    assert 'Compare started' in result['message']
+    assert 'Comparison started' in result['message']
