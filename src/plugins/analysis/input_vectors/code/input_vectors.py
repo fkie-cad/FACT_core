d51c8fa5af340a68da4c56e6a5eba98ef15ac221
Jörg Stucke <joerg.stucke@fkie.fraunhofer.de>
2024-09-27 13:38:20 +0200

    fix: improved memory efficiency of input vectors plugin

--- src/plugins/analysis/input_vectors/code/input_vectors.py
+++ src/plugins/analysis/input_vectors/code/input_vectors.py
@@ -1,13 +1,10 @@
 import logging
 from json import JSONDecodeError, loads
-from pathlib import Path
-from tempfile import TemporaryDirectory
 
 from docker.errors import DockerException
 from docker.types import Mount
 from requests.exceptions import ReadTimeout
 
-import config
 from analysis.PluginBase import AnalysisBasePlugin
 from helperFunctions.docker import run_docker_container
 from objects.file import FileObject
@@ -29,32 +26,29 @@ class AnalysisPlugin(AnalysisBasePlugin):
     NAME = 'input_vectors'
     DESCRIPTION = 'Determines possible input vectors of an ELF executable like stdin, network, or syscalls.'
     DEPENDENCIES = ['file_type']  # noqa: RUF012
-    VERSION = '0.1.2'
+    VERSION = '0.1.3'
     MIME_WHITELIST = ['application/x-executable', 'application/x-object', 'application/x-sharedlib']  # noqa: RUF012
     FILE = __file__
 
     def process_object(self, file_object: FileObject):
-        with TemporaryDirectory(prefix=self.NAME, dir=config.backend.docker_mount_base_dir) as tmp_dir:
-            file_path = Path(tmp_dir) / file_object.file_name
-            file_path.write_bytes(file_object.binary)
-            try:
-                result = run_docker_container(
-                    DOCKER_IMAGE,
-                    # We explicitly don't want stderr to ignore "Cannot analyse at [...]"
-                    combine_stderr_stdout=False,
-                    logging_label=self.NAME,
-                    timeout=TIMEOUT_IN_SECONDS,
-                    command=CONTAINER_TARGET_PATH,
-                    mounts=[
-                        Mount(CONTAINER_TARGET_PATH, str(file_path), type='bind'),
-                    ],
-                )
-                file_object.processed_analysis[self.NAME] = loads(result.stdout)
-            except ReadTimeout:
-                file_object.processed_analysis[self.NAME]['failed'] = 'Analysis timed out. It might not be complete.'
-            except (DockerException, OSError):
-                file_object.processed_analysis[self.NAME]['failed'] = 'Analysis issues. It might not be complete.'
-            except JSONDecodeError:
-                logging.error('[input_vectors]: Could not decode JSON output:', exc_info=True)
+        try:
+            result = run_docker_container(
+                DOCKER_IMAGE,
+                # We explicitly don't want stderr to ignore "Cannot analyse at [...]"
+                combine_stderr_stdout=False,
+                logging_label=self.NAME,
+                timeout=TIMEOUT_IN_SECONDS,
+                command=CONTAINER_TARGET_PATH,
+                mounts=[
+                    Mount(CONTAINER_TARGET_PATH, file_object.file_path, type='bind', read_only=True),
+                ],
+            )
+            file_object.processed_analysis[self.NAME] = loads(result.stdout)
+        except ReadTimeout:
+            file_object.processed_analysis[self.NAME]['failed'] = 'Analysis timed out. It might not be complete.'
+        except (DockerException, OSError):
+            file_object.processed_analysis[self.NAME]['failed'] = 'Analysis issues. It might not be complete.'
+        except JSONDecodeError:
+            logging.error('[input_vectors]: Could not decode JSON output:', exc_info=True)
 
-            return file_object
+        return file_object
