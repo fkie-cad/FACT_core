3c93be65b0982182b77bb92283a304ec2ab8674e
Jörg Stucke <joerg.stucke@fkie.fraunhofer.de>
2024-11-13 16:17:44 +0100

    chore: replaced test config files with default config
    this fixes the problem that these config files always need to be updated as well if the default config changes
    
    and there is one additional advantage: the default config is tested and the tests make sure that it is valid

--- src/test/unit/test_config.py
+++ src/test/unit/test_config.py
@@ -1,3 +1,6 @@
+from pathlib import Path
+from tempfile import TemporaryDirectory
+
 import pydantic
 import pytest
 
@@ -6,14 +9,15 @@ import config
 # We explicitly don't want the patch_cfg fixture to be able to patch this function
 # This is why we import it here
 from config import load
-from test.common_helper import get_test_data_dir
+from helperFunctions.fileSystem import get_config_dir
+
+CONFIG_PATH = Path(get_config_dir()) / 'fact-core-config.toml'
 
 
 def test_load(monkeypatch):
     # Undo all monkeypatching which includes what `patch_config` patched.
     monkeypatch.undo()
-    cfg_path = f'{get_test_data_dir()}/fact-core-config.toml'
-    load(path=cfg_path)
+    load(path=CONFIG_PATH)
 
     assert config.common is not None, 'common global was not set'
     assert config.backend is not None, 'backend global was not set'
@@ -23,6 +27,12 @@ def test_load(monkeypatch):
 
 
 def test_load_missing_entries():
-    cfg_path = get_test_data_dir() + '/fact-core-config.toml-missing-entrys'
-    with pytest.raises(pydantic.ValidationError, match='server'):
-        load(path=cfg_path)
+    cfg_contents = CONFIG_PATH.read_text()
+    assert '[common.postgres]\nserver =' in cfg_contents
+    # comment out server
+    cfg_contents = cfg_contents.replace('[common.postgres]\nserver =', '[common.postgres]\n# server =')
+    with TemporaryDirectory() as tmp_dir:
+        cfg_path = Path(tmp_dir) / 'config.toml'
+        cfg_path.write_text(cfg_contents)
+        with pytest.raises(pydantic.ValidationError, match='server'):
+            load(path=cfg_path)
