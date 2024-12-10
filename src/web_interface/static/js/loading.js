1c76ee93f90a449e8986753e055f0b49159b360f
Jörg Stucke <joerg.stucke@fkie.fraunhofer.de>
2024-12-10 17:33:07 +0100

    feat: new custom loading animation

--- src/web_interface/static/js/loading.js
+++ src/web_interface/static/js/loading.js
@@ -1,9 +1,20 @@
 function showImg() {
     $('#form').hide();
-    $('#loading_img').css({'display': 'block'});
+    $('#loading-img').css({'display': 'block'});
 }
 
 function hideImg() {
     $('#form').show();
-    $('#loading_img').css({'display': 'none'});
+    $('#loading-img').css({'display': 'none'});
+}
+
+function loadLoadingAnimation(elementId) {
+    fetch("/static/fact_loading.svg")
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById(elementId).innerHTML = data;
+    })
+    .catch(error => {
+        console.error('Error loading loading animation:', error);
+    });
 }
