086281f1a2756b3d296caeb5f74387992aaec244
Jörg Stucke <joerg.stucke@fkie.fraunhofer.de>
2025-03-19 14:17:14 +0100

    fix: fix for redirect loop in case of user w/o roles

--- src/web_interface/security/decorator.py
+++ src/web_interface/security/decorator.py
@@ -5,11 +5,11 @@ from flask_security import roles_accepted as original_decorator
 import config
 
 
-def roles_accepted(*roles):
+def roles_accepted(*roles, no_role_needed: bool = False):
     def wrapper(fn):
         @wraps(fn)
         def decorated_view(*args, **kwargs):
-            if not config.frontend.authentication.enabled:
+            if not config.frontend.authentication.enabled or no_role_needed:
                 return fn(*args, **kwargs)
             return original_decorator(*roles)(fn)(*args, **kwargs)
 
