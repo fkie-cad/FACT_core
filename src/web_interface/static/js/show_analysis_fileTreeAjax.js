29dba08747a9efdddffd7515e2df65e6c7aca419
Jörg Stucke <joerg.stucke@fkie.fraunhofer.de>
2025-05-05 14:00:17 +0200

    feat: added switch for hiding symlinks in file tree

--- src/web_interface/static/js/show_analysis_fileTreeAjax.js
+++ src/web_interface/static/js/show_analysis_fileTreeAjax.js
@@ -17,6 +17,7 @@ $(document).ready(function () {
             // inner node url
             `/ajax_tree/${node.data.uid}/${root_uid}`
     );
+    Document.fileTree = fileTree;
     // start file tree search on keyup event
     $('#fileTreeSearch').keyup(delay(function () {
         fileTree.jstree(true).show_all();
@@ -34,4 +35,25 @@ $(document).ready(function () {
             $("#fileTreeSearchFeedback").hide();
         }
     });
+    fileTree.on('open_node.jstree', function(e, data) {
+        if (!data.node.icon.includes("folder")) {
+            toggleSymlinkNodes();
+        }
+    });
 });
+
+const symlinkSwitch = document.getElementById("symlinkSwitch");
+symlinkSwitch.checked = true;
+
+function toggleSymlinkNodes() {
+    let filetree = Document.fileTree;
+    filetree.jstree(true).get_json('#', { flat: true }).forEach(function(node) {
+        if (node.icon.includes("inode-symlink")) {
+            if (symlinkSwitch.checked === true) {
+                filetree.jstree('show_node', node.id);
+            } else {
+                filetree.jstree('hide_node', node.id);
+            }
+        }
+    });
+}
