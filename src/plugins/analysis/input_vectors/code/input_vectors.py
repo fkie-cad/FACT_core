aed824ade6e22e5bee73ff193727c8b9e950ee31
Jörg Stucke <joerg.stucke@fkie.fraunhofer.de>
2025-05-23 13:25:58 +0200

    feat: converted input_vectors plugin to new base class

--- src/plugins/analysis/input_vectors/code/input_vectors.py
+++ src/plugins/analysis/input_vectors/code/input_vectors.py
@@ -1,36 +1,82 @@
-import logging
+from __future__ import annotations
+
 from json import JSONDecodeError, loads
+from typing import TYPE_CHECKING
 
 from docker.errors import DockerException
 from docker.types import Mount
+from pydantic import BaseModel
 from requests.exceptions import ReadTimeout
+from semver import Version
 
-from analysis.PluginBase import AnalysisBasePlugin
+from analysis.plugin import AnalysisPluginV0
 from helperFunctions.docker import run_docker_container
-from objects.file import FileObject
+
+if TYPE_CHECKING:
+    from io import FileIO
 
 DOCKER_IMAGE = 'input-vectors:latest'
 TIMEOUT_IN_SECONDS = 120
 CONTAINER_TARGET_PATH = '/tmp/input'
 
 
-class AnalysisPlugin(AnalysisBasePlugin):
-    """
-    This plugin determines possible input vectors of Linux ELF executables.
-    Examples are:
-    - network
-    - stdin
-    - kernel via syscalls
-    """
+class InputVectorsAnalysisError(Exception):
+    pass
 
-    NAME = 'input_vectors'
-    DESCRIPTION = 'Determines possible input vectors of an ELF executable like stdin, network, or syscalls.'
-    DEPENDENCIES = ['file_type']  # noqa: RUF012
-    VERSION = '0.1.3'
-    MIME_WHITELIST = ['application/x-executable', 'application/x-object', 'application/x-sharedlib']  # noqa: RUF012
-    FILE = __file__
 
-    def process_object(self, file_object: FileObject):
+class InputVector(BaseModel):
+    name: str
+    xrefs: list[str]
+    count: int | None = None
+
+
+class InputVectors(BaseModel):
+    bus_usb: list[InputVector] | None = None
+    environment: list[InputVector] | None = None
+    file: list[InputVector] | None = None
+    ipc: list[InputVector] | None = None
+    network: list[InputVector] | None = None
+    random: list[InputVector] | None = None
+    shell: list[InputVector] | None = None
+    signal: list[InputVector] | None = None
+    stdin: list[InputVector] | None = None
+    time: list[InputVector] | None = None
+
+
+class AnalysisPlugin(AnalysisPluginV0):
+    class Schema(BaseModel):
+        inputs: InputVectors
+        configs: list[str]
+        url_paths: list[str]
+        domains: list[str]
+
+    def __init__(self):
+        super().__init__(
+            metadata=self.MetaData(
+                name='input_vectors',
+                description='Determines possible input vectors of an ELF executable like stdin, network, or syscalls.',
+                version=Version(1, 0, 0),
+                Schema=self.Schema,
+                mime_whitelist=[
+                    'application/x-executable',
+                    'application/x-object',
+                    'application/x-sharedlib',
+                    'application/x-pie-executable',
+                ],
+            ),
+        )
+
+    def analyze(self, file_handle: FileIO, virtual_file_path: dict[str, list[str]], analyses: dict) -> Schema:
+        del virtual_file_path, analyses
+        analysis_data = self._run_docker(file_handle)
+        return self.Schema(
+            inputs=analysis_data['inputs'],
+            configs=analysis_data['configs'],
+            url_paths=analysis_data['url_paths'],
+            domains=analysis_data['domains'],
+        )
+
+    def _run_docker(self, file_handle: FileIO) -> dict:
         try:
             result = run_docker_container(
                 DOCKER_IMAGE,
@@ -40,15 +86,25 @@ class AnalysisPlugin(AnalysisBasePlugin):
                 timeout=TIMEOUT_IN_SECONDS,
                 command=CONTAINER_TARGET_PATH,
                 mounts=[
-                    Mount(CONTAINER_TARGET_PATH, file_object.file_path, type='bind', read_only=True),
+                    Mount(CONTAINER_TARGET_PATH, file_handle.name, type='bind', read_only=True),
                 ],
             )
-            file_object.processed_analysis[self.NAME] = loads(result.stdout)
-        except ReadTimeout:
-            file_object.processed_analysis[self.NAME]['failed'] = 'Analysis timed out. It might not be complete.'
-        except (DockerException, OSError):
-            file_object.processed_analysis[self.NAME]['failed'] = 'Analysis issues. It might not be complete.'
-        except JSONDecodeError:
-            logging.error('[input_vectors]: Could not decode JSON output:', exc_info=True)
+            analysis_data = loads(result.stdout)['full']
+        except ReadTimeout as err:
+            raise InputVectorsAnalysisError('Analysis timed out. It might not be complete.') from err
+        except (DockerException, OSError, KeyError) as err:
+            raise InputVectorsAnalysisError('Analysis issues. It might not be complete.') from err
+        except JSONDecodeError as err:
+            raise InputVectorsAnalysisError('Could not decode JSON output') from err
+        return analysis_data
 
-        return file_object
+    def summarize(self, result: Schema) -> list:
+        summary = []
+        for key, value in result.inputs.model_dump().items():
+            if key == 'inputs':
+                for input_class, input_vector in value.items():
+                    if input_vector:
+                        summary.append(input_class)
+            elif value:
+                summary.append(key)
+        return summary
