420b21c1af26a3395ee8a07ac31d518289c4e4b7
Jörg Stucke <joerg.stucke@fkie.fraunhofer.de>
2026-01-08 10:57:29 +0100

    feat: improved pre-install script
    * added CLI help
    * added option to skip docker installation

--- src/install/pre_install.sh
+++ src/install/pre_install.sh
@@ -2,16 +2,70 @@
 
 set -e
 
+# === CLI arguments and help ===
+# defaults:
+SKIP_DOCKER=false
+SILENT=false
+
+show_help() {
+    cat << EOF
+$(basename "$0") is a script for installing the installation requirements of
+FACT and should be run before running "python3 install.py".
+Usage: $(basename "$0") [OPTIONS]
+
+Options:
+    -D, --skip-docker  Skip Docker installation
+    -d, --debug        Show debugging output
+    -s, --silent       No CLI output
+    -h, --help         Show this help
+EOF
+}
+
+log() {
+  if [[ "$SILENT" == true ]]; then
+    echo "$@"
+  fi
+}
+
+# parse CLI arguments
+while [[ $# -gt 0 ]]; do
+    case $1 in
+        -D|--skip-docker)
+            SKIP_DOCKER=true
+            shift
+            ;;
+        -s|--silent)
+            SILENT=true
+            shift
+            ;;
+        -d|--debug)
+            set -x
+            shift
+            ;;
+        -h|--help)
+            show_help
+            exit 0
+            ;;
+        *)
+            echo "Unknown option: $1" >&2
+            show_help
+            exit 1
+            ;;
+    esac
+done
+
 # cd in this files directory for relative paths to work
 cd "$( dirname "${BASH_SOURCE[0]}" )" || exit 1
 
 FACTUSER=$(whoami)
 
-echo "Installing pre-install requirements..."
-sudo apt-get update
-sudo apt-get -y install python3-pip git libffi-dev lsb-release
+log "Installing pre-install requirements..."
 
-# distro and codename detection
+# === step 1: apt packages ===
+sudo apt-get update
+grep -vE '^\s*(#|$)' apt-pkgs-pre_install.txt | sudo xargs apt-get install -y
+
+# === step 2: distro and codename detection ===
 DISTRO=$(lsb_release -is)
 if [ "${DISTRO}" = "Linuxmint" ] || [ "${DISTRO}" = "Ubuntu" ]; then
     DISTRO=ubuntu
@@ -29,69 +83,67 @@ elif [ "${CODENAME}" = "ulyana" ] || [ "${CODENAME}" = "ulyssa" ] || [ "${CODENA
 elif  [ "${CODENAME}" = "kali-rolling" ]; then
     CODENAME=bookworm
 elif [ -z "${CODENAME}" ]; then
-	echo "Could not get distribution codename. Please make sure that your distribution is compatible to ubuntu/debian."
+	echo "Could not get distribution codename. Please make sure that your distribution is compatible to ubuntu/debian." >&2
 	exit 1
 fi
 
-echo "detected distro ${DISTRO} and codename ${CODENAME}"
+log "detected distro ${DISTRO} and codename ${CODENAME}"
 
 if [ "${CODENAME}" = "bionic" ] || [ "${CODENAME}" = "focal" ] || [ "${CODENAME}" = "buster" ] || [ "${CODENAME}" = "bullseye" ]; then
-  echo "Warning: your distribution is outdated and the installation may not work as expected. Please upgrade your OS."
+  log "Warning: your distribution is outdated and the installation may not work as expected. Please upgrade your OS."
 fi
 
-# docker installation (source: https://docs.docker.com/engine/install/{ubuntu|debian})
-echo "Installing Docker"
+# === step 3: docker installation ===
+if [[ "$SKIP_DOCKER" == false ]]; then
+  # docker installation (source: https://docs.docker.com/engine/install/{ubuntu|debian})
+  log "Installing Docker"
 
-# uninstall old docker versions
-for i in docker.io docker-doc docker-compose podman-docker containerd runc; do
-  sudo apt-get remove -y $i || true
-done
+  # uninstall old docker versions
+  sudo apt-get remove -y "$(dpkg --get-selections docker.io docker-compose docker-compose-v2 docker-doc podman-docker containerd runc | cut -f1)"
 
-# install prerequisites
-sudo apt-get install -y \
-    ca-certificates \
-    curl \
-    gnupg
+  # add Docker's GPG key
+  sudo install -m 0755 -d /etc/apt/keyrings
+  echo "curl -fsSL \"https://download.docker.com/linux/${DISTRO}/gpg\""
+  curl -fsSL "https://download.docker.com/linux/${DISTRO}/gpg" | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg --batch --yes
+  sudo chmod a+r /etc/apt/keyrings/docker.gpg
 
-# add Docker's GPG key
-sudo install -m 0755 -d /etc/apt/keyrings
-echo "curl -fsSL \"https://download.docker.com/linux/${DISTRO}/gpg\""
-curl -fsSL "https://download.docker.com/linux/${DISTRO}/gpg" | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg --batch --yes
-sudo chmod a+r /etc/apt/keyrings/docker.gpg
+  # set up repository
+  echo \
+    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/${DISTRO} \
+    ${CODENAME} stable" | sudo tee /etc/apt/sources.list.d/docker.list
 
-# set up repository
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/${DISTRO} \
-  ${CODENAME} stable" | sudo tee /etc/apt/sources.list.d/docker.list
+  # Install Docker Engine
+  sudo apt-get update
+  sudo apt-get -o Dpkg::Options::="--force-confnew" install -y \
+    docker-ce \
+    docker-ce-cli \
+    containerd.io \
+    docker-buildx-plugin \
+    docker-compose-plugin
 
-# Install Docker Engine
-sudo apt-get update
-sudo apt-get -o Dpkg::Options::="--force-confnew" install -y \
-  docker-ce \
-  docker-ce-cli \
-  containerd.io \
-  docker-buildx-plugin \
-  docker-compose-plugin
+  sudo systemctl enable docker
 
-sudo systemctl enable docker
-
-# add fact-user to docker group
-if [ ! "$(getent group docker)" ]
-then
-    sudo groupadd docker
+  # add fact-user to docker group
+  if [ ! "$(getent group docker)" ]
+  then
+      sudo groupadd docker
+  fi
+  sudo usermod -aG docker "$FACTUSER"
+else
+    log "Skipping Docker installation"
 fi
-sudo usermod -aG docker "$FACTUSER"
 
+# === step 4: python packages ===
 IS_VENV=$(python3 -c 'import sys; print(sys.exec_prefix!=sys.base_prefix)')
 PREFIX=""
 if [[ $IS_VENV == "False" ]]
 then
-  echo -e "\\033[31mWarning: It is highly discouraged to install FACT without a virtual environment because of the risk of conflicts with system Python packages!\\033[0m"
+  log -e "\\033[31mWarning: It is highly discouraged to install FACT without a virtual environment because of the risk of conflicts with system Python packages!\\033[0m"
   PREFIX="sudo -EH python3 -m"
 fi
 $PREFIX pip install -U pip setuptools wheel
 $PREFIX pip install -r ./requirements_pre_install.txt --prefer-binary
 
-echo -e "Pre-Install-Routine complete! \\033[31mPlease reboot before running install.py\\033[0m"
+log -e "Pre-Install-Routine complete! \\033[31mPlease make sure you can run docker containers (e.g. reboot) before running install.py\\033[0m"
 
 exit 0
