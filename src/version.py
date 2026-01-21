114cb39bbd895de222d3a883d19e9259329e48dd
Jörg Stucke <joerg.stucke@fkie.fraunhofer.de>
2026-01-21 10:48:33 +0100

    WIP: uv install

--- src/version.py
+++ src/version.py
@@ -1,19 +1,29 @@
 """
-    Firmware Analysis and Comparison Tool (FACT)
-    Copyright (C) 2015-2026  Fraunhofer FKIE
+Firmware Analysis and Comparison Tool (FACT)
+Copyright (C) 2015-2026  Fraunhofer FKIE
 
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
-__VERSION__ = '4.4-dev'
+from importlib.metadata import PackageNotFoundError, version
+
+try:
+    __VERSION__ = version('fact-core')
+except PackageNotFoundError:
+    from pathlib import Path
+
+    import toml
+
+    project = toml.load(Path(__file__).parent.parent / 'pyproject.toml')
+    __VERSION__ = project['project']['version']
