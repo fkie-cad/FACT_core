e9b4b729d245c0c393c7485d9d2f3283abf7d40d
Jörg Stucke <joerg.stucke@fkie.fraunhofer.de>
2026-07-02 14:37:26 +0200

    chore: version bump to 4.5-dev (#1497)

--- src/version.py
+++ src/version.py
@@ -16,4 +16,4 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
-__VERSION__ = '4.4'
+__VERSION__ = '4.5-dev'
