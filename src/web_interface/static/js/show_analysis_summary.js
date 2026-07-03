86ac85047710515a6b3e98f0cdea12518c1e8db5
Johannes vom Dorp <dorp@cs.uni-bonn.de>
2026-07-03 11:45:04 +0200

    feat: optional inverted summary (#1496)
    * Implemented inverted summary read-out along with adaptations to the js and html templates with Checkbox
    * moved conversion logic to helper function
    * refactored summary generation to drop duplicated code
    * made loading animation appear on every reload
    * fixed overfull tds with word-wrap

--- src/web_interface/static/js/show_analysis_summary.js
+++ src/web_interface/static/js/show_analysis_summary.js
@@ -1,14 +1,11 @@
-function load_summary(uid, selected_analysis, focus=false){
+function load_summary(uid, selected_analysis, inverted){
     $("#summary-button").css("display", "none");
     let summary_gif = $("#loading-summary-gif");
     summary_gif.css("display", "block");
     $("#summary-div").load(
-        `/ajax_get_summary/${uid}/${selected_analysis}`,
+        `/ajax_get_summary/${uid}/${selected_analysis}/${inverted}`,
         () => {
             summary_gif.css("display", "none");
-            if (focus === true) {
-                location.href = "#summary-heading";
-            }
         }
     );
 }
