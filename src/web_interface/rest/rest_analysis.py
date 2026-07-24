8cb45bae1490b2b890c6a92607d799bc66d0c21b
Jörg Stucke <joerg.stucke@fkie.fraunhofer.de>
2026-07-24 10:47:48 +0200

    feat: add PUT endpoint to /rest/analysis (#1486)
    Co-authored-by: Johannes vom Dorp <johannes.vom.dorp@fkie.fraunhofer.de>

--- src/web_interface/rest/rest_analysis.py
+++ src/web_interface/rest/rest_analysis.py
@@ -2,9 +2,10 @@ from __future__ import annotations
 
 from http import HTTPStatus
 
+from flask import request
 from flask_restx import Namespace
 
-from web_interface.rest.helper import error_message, success_message
+from web_interface.rest.helper import error_message, get_boolean_from_request, success_message
 from web_interface.rest.rest_resource_base import RestResourceBase
 from web_interface.security.decorator import roles_accepted
 from web_interface.security.privileges import PRIVILEGES
@@ -19,6 +20,12 @@ api = Namespace('rest/analysis', description='Request the analysis results of a 
         'params': {
             'uid': 'File UID',
             'plugin': 'The name of the analysis plugin (lower case with underscores, e.g. file_type)',
+            'force': {
+                'description': 'Force re-analysis of already analyzed files',
+                'in': 'query',
+                'type': 'boolean',
+                'default': 'false',
+            },
         },
     },
 )
@@ -50,3 +57,36 @@ class RestAnalysis(RestResourceBase):
             return error_message(message, self.URL, request_data, return_code=return_code)
 
         return success_message({'analysis': analysis}, self.URL, request_data)
+
+    @roles_accepted(*PRIVILEGES['submit_analysis'])
+    @api.doc(
+        responses={
+            HTTPStatus.OK: 'Success',
+            HTTPStatus.BAD_REQUEST: 'Unknown file object or plugin',
+        }
+    )
+    def put(self, uid: str, plugin: str) -> tuple[dict, int]:
+        """
+        Run a specific analysis on a specific file.
+        """
+        file_object = self.db.frontend.get_object(uid)
+        request_data = {'uid': uid, 'plugin': plugin}
+
+        if not file_object:
+            message = f'No file object with UID "{uid}" found'
+            return error_message(message, self.URL, request_data, return_code=HTTPStatus.BAD_REQUEST)
+
+        available_plugins = self.intercom.get_available_analysis_plugins()
+        if plugin not in available_plugins:
+            message = f'Analysis plugin "{plugin}" not found'
+            return error_message(message, self.URL, request_data, return_code=HTTPStatus.BAD_REQUEST)
+
+        file_object.scheduled_analysis = [plugin]
+        if 'force' in request.args:
+            force_flag = get_boolean_from_request(request.args, 'force')
+            file_object.force_update = force_flag
+        success = self.intercom.add_single_file_task(file_object)
+
+        if success:
+            return success_message({'success': True}, self.URL, request_data)
+        return error_message('Failed to schedule analysis', self.URL, request_data, return_code=HTTPStatus.BAD_REQUEST)
