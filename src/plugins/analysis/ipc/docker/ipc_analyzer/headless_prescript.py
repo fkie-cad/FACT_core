2b93d977967710073b44593fec4126c7cacf2bf4
Jörg Stucke <joerg.stucke@fkie.fraunhofer.de>
2026-08-03 16:17:17 +0200

    feat: IPC: deactivate more unnecessary ghidra analyses
    * deactivates expensive analyzers that are not needed

--- src/plugins/analysis/ipc/docker/ipc_analyzer/headless_prescript.py
+++ src/plugins/analysis/ipc/docker/ipc_analyzer/headless_prescript.py
@@ -14,27 +14,26 @@ def main():
     """
 
     turn_off = [
-        'Aggressive Instruction Finder.Create Analysis Bookmarks',
-        'Apply Data Archives.Create Analysis Bookmarks',
+        'Aggressive Instruction Finder',
+        'Apply Data Archives',
         'Call Convention ID',
         'Call-Fixup Installer',
-        'Create Address Tables.Create Analysis Bookmarks',
-        'Create Address Tables.Relocation Table Guide',
+        'CFStrings',
+        'Condense Filler Bytes',
+        'Create Address Tables',
         'Data Reference.Relocation Table Guide',
         'Data Reference.Respect Execute Flag',
         'Data Reference.Subroutine References',
         'Data Reference.Unicode String References',
-        'Decompiler Parameter ID.Commit Data Types',
+        'Decompiler Parameter ID',
+        'Decompiler Switch Analysis',
         'Demangler GNU',
-        'Demangler GNU.Apply Function Calling Conventions',
-        'Demangler GNU.Apply Function Signatures',
-        'Disassemble Entry Points.Respect Execute Flag',
+        'Demangler Rust',
         'DWARF',
         'DWARF.Create Function Signatures',
         'DWARF.Import Data Types',
         'DWARF.Import Functions',
         'DWARF.Try To Pack Structs',
-        'ELF Scalar Operand References',
         'ELF Scalar Operand References.Relocation Table Guide',
         'Embedded Media',
         'Embedded Media.Create Analysis Bookmarks',
@@ -45,12 +44,9 @@ def main():
         'Function Start Search After Code',
         'GCC Exception Handlers',
         'GCC Exception Handlers.Create Try Catch Comments',
+        'Instruction Merge',
         'Non-Returning Functions - Discovered',
-        'Non-Returning Functions - Discovered.Create Analysis Bookmarks',
-        'Non-Returning Functions - Discovered.Repair Flow Damage',
         'Non-Returning Functions - Known.Create Analysis Bookmarks',
-        'Reference.Ascii String References',
-        'Reference.References to Pointers',
         'Reference.Relocation Table Guide',
         'Reference.Unicode String References',
         'Shared Return Calls.Assume Contiguous Functions Only',
@@ -59,12 +55,17 @@ def main():
         'Stack.Create Local Variables',
         'Stack.Create Param Variables',
         'Subroutine References.Create Thunks Early',
+        'Variadic Function Signature Override',
+        'Windows x86 PE Exception Handling',
+        'Windows x86 PE RTTI Analyzer',
     ]
     options = getCurrentAnalysisOptionsAndValues(currentProgram)
     for option in turn_off:
         if options.containsKey(option):
             print('turning off {}'.format(option))
             setAnalysisOption(currentProgram, option, 'false')
+        else:
+            print('WARNING: unknown analysis option: {}'.format(option))
     return 0
 
 
